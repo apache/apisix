28a085ab92fc0b02689e3eb6a22a8a7aa0bd1e34
Yuansheng Wang <membphis@gmail.com>
2019-04-11 16:53:21 +0800

    feature: supported to load config from YAML config.

---
+++ lua/apimeta/comm/config.lua
@@ -0,0 +1,32 @@
+-- Copyright (C) Yuansheng Wang
+
+local ngx = ngx
+local pcall = pcall
+local yaml = require("apimeta.comm.yaml")
+local io_open = io.open
+
+local config_path = ngx.config.prefix() .. "conf/config.yaml"
+
+local _M = {}
+
+local function read_file(path)
+    local file = io_open(path, "rb")   -- r read mode and b binary mode
+    if not file then
+        return nil
+    end
+
+    local content = file:read("*a") -- *a or *all reads the whole file
+    file:close()
+    return content
+end
+
+function _M.read()
+    local yaml_config = read_file(config_path)
+    if type(yaml_config) ~= "string" then
+        return nil, "failed to read config file"
+    end
+
+    return yaml.parse(yaml_config)
+end
+
+return _M
--- t/sanity.t
+++ t/sanity.t
@@ -1,29 +1,9 @@
-# vim:set ft= ts=4 sw=4 et fdm=marker:
-
-use Test::Nginx::Socket::Lua;
-use Cwd qw(cwd);
+use t::APIMeta 'no_plan';
 
 repeat_each(1);
-log_level('warn');
-
-my $pwd = cwd();
-
-add_block_preprocessor(sub {
-    my ($block) = @_;
-
-    my $http_config = $block->http_config // '';
-    $http_config .= <<_EOC_;
-    lua_package_path "$pwd/lua/?.lua;;";
-
-    init_by_lua_block {
-        require "resty.core"
-    }
-_EOC_
-
-    $block->set_value("http_config", $http_config);
-});
-
-plan tests => blocks() * repeat_each() * 2;
+no_long_string();
+no_shuffle();
+log_level('info');
 
 run_tests();
 
