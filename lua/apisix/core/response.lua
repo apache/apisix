f454328605673adc1393b5408b9d69d1162962b7
Yuansheng Wang <membphis@gmail.com>
2019-05-23 16:35:47 +0800

    feature: get the consumer id by auth plugin like `key-auth`.

--- lua/apisix/core/response.lua
+++ lua/apisix/core/response.lua
@@ -2,30 +2,64 @@
 
 local ngx = ngx
 local ngx_say = ngx.say
-local ngx_exit = ngx.exit
 local ngx_header = ngx.header
 local encode_json = require("cjson.safe").encode
+local error = error
+local select = select
+local type = type
+local ngx_exit = ngx.exit
+local insert_tab = table.insert
+local clear_tab = table.clear
+local concat_tab = table.concat
 
-local _M = {}
+
+local _M = {version = 0.1}
 
 
-function _M.say(code, body)
-    code = code or 200
+local resp_exit
+do
+    local t = {}
 
-    if not body then
+function resp_exit(code, ...)
+    clear_tab(t)
+
+    if type(code) ~= "number" then
+        insert_tab(t, code)
+        code = nil
+    end
+
+    if code then
+        ngx.status = code
+    end
+
+    for i = 1, select('#', ...) do
+        local v = select(i, ...)
+        if type(v) == "table" then
+            local body, err = encode_json(v)
+            if err then
+                error("failed to encode data: " .. err, -2)
+            else
+                insert_tab(t, body)
+            end
+
+        else
+            insert_tab(t, v)
+        end
+    end
+
+    ngx_say(concat_tab(t))
+
+    if code then
         ngx_exit(code)
-        return
     end
+end
 
-    local err
-    body, err = encode_json(body)
-    if err then
-        error("failed to encode body: " .. err)
-    end
+end -- do
+_M.exit = resp_exit
 
-    ngx.status = code
-    ngx_say(body)
-    ngx_exit(code)
+
+function _M.say(...)
+    resp_exit(nil, ...)
 end
 
 
@@ -34,9 +68,8 @@ function _M.set_header(name, value)
       error("headers have already been sent", 2)
     end
 
-
     ngx_header[name] = value
-  end
+end
 
 
 return _M
--- lua/apisix/plugins/key-auth.lua
+++ lua/apisix/plugins/key-auth.lua
@@ -1,5 +1,6 @@
 local core = require("apisix.core")
 local plugin_name = "key-auth"
+local consumers
 
 
 local _M = {
@@ -10,7 +11,30 @@ local _M = {
 
 
 function _M.init(conf)
+    if consumers then
+        consumers.close()
+    end
+
+    consumers = core.config.new("/plugins/key-auth/consumers",
+                                {automatic = true})
 end
+
+
+local create_consume_cache
+do
+    local consumer_ids = {}
+
+    function create_consume_cache()
+        core.table.clear(consumer_ids)
+
+        for _, consumer in ipairs(consumers.values) do
+            consumer_ids[consumer.value.key] = consumer.value.id
+        end
+
+        return consumer_ids
+    end
+
+end -- do
 
 
 function _M.check_args(conf)
@@ -21,11 +45,16 @@ end
 function _M.access(conf, ctx)
     local key = core.request.header(ctx, "apikey")
     if not key then
-        core.response.say(401, {message = "No API key found in request"})
-        return
+        return 401, {message = "Missing API key found in request"}
     end
 
-    core.log.warn("apikey: ", key)
+    local consumers_hash = core.lrucache.plugin(plugin_name, "consumers_key",
+                                consumers.version, create_consume_cache)
+
+    local consumer_id = consumers_hash[key]
+    if not consumer_id then
+        return 401, {message = "Invalid API key in request"}
+    end
 
     core.log.warn("hit key-auth access")
 end
