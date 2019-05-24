697a7eff1ece7528723d8ef0816d4ca9fb108b87
Yuansheng Wang <membphis@gmail.com>
2019-05-24 11:50:10 +0800

    change: used different parameter for plugin `balancer`.

--- lua/apisix/core/lrucache.lua
+++ lua/apisix/core/lrucache.lua
@@ -1,61 +1,72 @@
+-- Copyright (C) Yuansheng Wang
+-- only support to cache lua table object
+
 local lru_new = require("resty.lrucache").new
 -- todo: support to config it in YAML.
 local GLOBAL_TTL = 60 * 60          -- 60 min
 local GLOBAL_ITEMS_COUNT = 1024
 local PLUGIN_TTL = 5 * 60           -- 5 min
-local PLUGIN_ITEMS_COUNT = 512
+local PLUGIN_ITEMS_COUNT = 8
 local global_lrus = lru_new(GLOBAL_ITEMS_COUNT)
+
+
+-- here is a hack way
+local plugins_conf = {
+    ["balancer"] = {
+        count = 512,
+        ttl = 10 * 60,
+    }
+}
 
 
 local _M = {version = 0.1}
 
 
-local function _global_lrus(key, version, create_obj_fun, ...)
-    local lru, stale_lru = global_lrus:get(key)
-    if lru and lru._version == version then
-        return lru
+local function global_lru(key, version, create_obj_fun, ...)
+    local obj, stale_obj = global_lrus:get(key)
+    if obj and obj._cache_ver == version then
+        return obj
     end
 
-    if stale_lru and stale_lru._version == version then
-        global_lrus:set(key, lru, GLOBAL_TTL)
-        return stale_lru
+    if stale_obj and stale_obj._cache_ver == version then
+        global_lrus:set(key, obj, GLOBAL_TTL)
+        return stale_obj
     end
 
-    lru = create_obj_fun(...)
-    lru._version = version
-    global_lrus:set(key, lru, GLOBAL_TTL)
-    return lru
+    obj = create_obj_fun(...)
+    obj._cache_ver = version
+    global_lrus:set(key, obj, GLOBAL_TTL)
+    return obj
 end
-_M.global = _global_lrus
+_M.global = global_lru
 
 
 local function _plugin(plugin_name, key, version, create_obj_fun, ...)
-    -- todo: support to config in yaml.
-    local lru_global = _global_lrus("/plugin/" .. plugin_name, nil,
-                                    lru_new, PLUGIN_ITEMS_COUNT)
+    local conf = plugins_conf[plugin_name]
+    local lru_global = global_lru("/plugin/" .. plugin_name, nil,
+            lru_new, conf and conf.count or PLUGIN_ITEMS_COUNT)
 
-    local lru, stale_lru = lru_global:get(key)
-    if lru and lru._version == version then
-        return lru
+    local obj, stale_obj = lru_global:get(key)
+    if obj and obj._cache_ver == version then
+        return obj
     end
 
-    if stale_lru and stale_lru._version == version then
-        lru_global:set(key, stale_lru, PLUGIN_TTL)
-        return stale_lru
+    if stale_obj and stale_obj._cache_ver == version then
+        lru_global:set(key, stale_obj, conf and conf.ttl or PLUGIN_TTL)
+        return stale_obj
     end
 
-    lru = create_obj_fun(...)
-    lru._version = version
-    lru_global:set(key, lru, PLUGIN_TTL)
+    obj = create_obj_fun(...)
+    obj._cache_ver = version
+    lru_global:set(key, obj, PLUGIN_TTL)
 
-    return lru
+    return obj
 end
 _M.plugin = _plugin
 
 
 function _M.plugin_ctx(plugin_name, api_ctx, create_obj_fun, ...)
     local key = api_ctx.conf_type .. "#" .. api_ctx.conf_id
-
     return _plugin(plugin_name, key, api_ctx.conf_version, create_obj_fun, ...)
 end
 
