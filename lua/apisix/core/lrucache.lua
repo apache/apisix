a135665d4b580a1c65023ced9208e08a3abdba7d
Yuansheng Wang <membphis@gmail.com>
2019-05-24 13:37:51 +0800

    feature: allowed custom plugin lrucache parameters.

--- lua/apisix/core/lrucache.lua
+++ lua/apisix/core/lrucache.lua
@@ -10,16 +10,8 @@ local PLUGIN_ITEMS_COUNT = 8
 local global_lrus = lru_new(GLOBAL_ITEMS_COUNT)
 
 
--- here is a hack way
-local plugins_conf = {
-    ["balancer"] = {
-        count = 512,
-        ttl = 10 * 60,
-    }
-}
-
-
 local _M = {version = 0.1}
+local mt = { __index = _M }
 
 
 local function global_lru(key, version, create_obj_fun, ...)
@@ -41,10 +33,9 @@ end
 _M.global = global_lru
 
 
-local function _plugin(plugin_name, key, version, create_obj_fun, ...)
-    local conf = plugins_conf[plugin_name]
+local function _plugin(count, ttl, plugin_name, key, version, create_obj_fun, ...)
     local lru_global = global_lru("/plugin/" .. plugin_name, nil,
-            lru_new, conf and conf.count or PLUGIN_ITEMS_COUNT)
+            lru_new, count)
 
     local obj, stale_obj = lru_global:get(key)
     if obj and obj._cache_ver == version then
@@ -52,22 +43,54 @@ local function _plugin(plugin_name, key, version, create_obj_fun, ...)
     end
 
     if stale_obj and stale_obj._cache_ver == version then
-        lru_global:set(key, stale_obj, conf and conf.ttl or PLUGIN_TTL)
+        lru_global:set(key, stale_obj, ttl)
         return stale_obj
     end
 
     obj = create_obj_fun(...)
     obj._cache_ver = version
-    lru_global:set(key, obj, PLUGIN_TTL)
+    lru_global:set(key, obj, ttl)
 
     return obj
 end
-_M.plugin = _plugin
+
+
+function _M.plugin(plugin_name, key, version, create_obj_fun, ...)
+    return _plugin(PLUGIN_ITEMS_COUNT, PLUGIN_TTL, plugin_name, key,
+                   version, create_obj_fun, ...)
+end
 
 
 function _M.plugin_ctx(plugin_name, api_ctx, create_obj_fun, ...)
     local key = api_ctx.conf_type .. "#" .. api_ctx.conf_id
-    return _plugin(plugin_name, key, api_ctx.conf_version, create_obj_fun, ...)
+    return _plugin(PLUGIN_ITEMS_COUNT, PLUGIN_TTL, plugin_name, key,
+                   api_ctx.conf_version, create_obj_fun, ...)
+end
+
+
+local function _obj_plugin_ctx(self, plugin_name, api_ctx, create_obj_fun, ...)
+    local key = api_ctx.conf_type .. "#" .. api_ctx.conf_id
+    return _plugin(self.plugin_count, self.plugin_ttl, plugin_name, key,
+                   api_ctx.conf_version, create_obj_fun, ...)
+end
+
+
+local function _obj_plugin(self, plugin_name, key, version, create_obj_fun, ...)
+    return _plugin(self.plugin_count, self.plugin_ttl, plugin_name, key,
+                   version, create_obj_fun, ...)
+end
+
+
+function _M.new(opts)
+    local plugin_count = opts and opts.plugin_count or PLUGIN_ITEMS_COUNT
+    local plugin_ttl = opts and opts.plugin_ttl or PLUGIN_TTL
+
+    return setmetatable({
+        plugin_count = plugin_count,
+        plugin_ttl = plugin_ttl,
+        plugin_ctx = _obj_plugin_ctx,
+        plugin = _obj_plugin,
+    }, mt)
 end
 
 
