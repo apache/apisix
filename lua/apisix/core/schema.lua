463e5601b3e7ffacafdd62f49c75da1ba0c36b8a
YuanSheng Wang <membphis@gmail.com>
2019-06-11 23:47:00 +0800

    optimize: used `lru` way to cache the validator object for schema. (#77)
    * optimize: used `lru` way to cache the validator object for schema.
    
    Fix #76.

--- lua/apisix/core/schema.lua
+++ lua/apisix/core/schema.lua
@@ -1,24 +1,32 @@
-local rapidjson_schema_validator = require('rapidjson').SchemaValidator
-local rapidjson_schema_doc = require('rapidjson').SchemaDocument
-local rapidjson_doc = require('rapidjson').Document
+local rapidjson = require('rapidjson')
+local rapidjson_schema_validator = rapidjson.SchemaValidator
+local rapidjson_schema_doc = rapidjson.SchemaDocument
+local rapidjson_doc = rapidjson.Document
+local cached_sd = require("apisix.core.lrucache").new({count = 1000, ttl = 0})
 
 
 local _M = {version = 0.1}
 
-local schema_sd = {}
+
+local function create_validator(schema)
+    local sd = rapidjson_schema_doc(schema)
+    local validator = rapidjson_schema_validator(sd)
+    -- log.info("type: ", type(validator))
+
+    -- only support to cache lua table object
+    return {validator}
+end
+
 
 -- You can follow this document to write schema:
 -- https://github.com/Tencent/rapidjson/blob/master/bin/draft-04/schema
 -- rapidjson not supported `format` in draft-04 yet
-function _M.check_args(schema, json)
-    local sd = schema_sd[schema]
-    if not sd then
-        sd = rapidjson_schema_doc(schema)
-        schema_sd[schema] = sd
-    end
-    local validator = rapidjson_schema_validator(sd)
+function _M.check(schema, json)
+    local validator = cached_sd(schema, nil, create_validator, schema)[1]
+
     local d = rapidjson_doc(json)
     return validator:validate(d)
 end
+
 
 return _M
