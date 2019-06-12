0c71fe8d96e31db1dbfaf79d8e8d4a06c488363e
Yuansheng <membphis@gmail.com>
2019-06-12 09:16:23 +0800

    feature: supported to cache non-table Lua object by `lrucache` library.

--- lua/apisix/core/schema.lua
+++ lua/apisix/core/schema.lua
@@ -10,11 +10,7 @@ local _M = {version = 0.1}
 
 local function create_validator(schema)
     local sd = rapidjson_schema_doc(schema)
-    local validator = rapidjson_schema_validator(sd)
-    -- log.info("type: ", type(validator))
-
-    -- only support to cache lua table object
-    return {validator}
+    return rapidjson_schema_validator(sd)
 end
 
 
@@ -22,7 +18,7 @@ end
 -- https://github.com/Tencent/rapidjson/blob/master/bin/draft-04/schema
 -- rapidjson not supported `format` in draft-04 yet
 function _M.check(schema, json)
-    local validator = cached_sd(schema, nil, create_validator, schema)[1]
+    local validator = cached_sd(schema, nil, create_validator, schema)
 
     local d = rapidjson_doc(json)
     return validator:validate(d)
