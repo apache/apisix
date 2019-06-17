fb914f5f640bcde2aea255c36180089fd6f00d94
Yuansheng <membphis@gmail.com>
2019-06-17 17:04:19 +0800

    bug: only cached schema doc object.

--- lua/apisix/core/schema.lua
+++ lua/apisix/core/schema.lua
@@ -4,30 +4,15 @@ local schema_doc = json.SchemaDocument
 local json_doc = json.Document
 
 
-local cached_sd = require("apisix.core.lrucache").new({count = 1000, ttl = 0})
-
-
 local _M = {version = 0.1}
-
-
-local function create_validator(schema)
-    local sd = schema_doc(schema)
-    local validator = schema_validator(sd)
-
-    -- bug: we have to use a table to store the `validator` first,
-    --   if we returned the `validator` directyly, we will get
-    --   some error like this:
-    --
-    --   attempt to call method 'validate' (a nil value)
-    return {validator}
-end
 
 
 -- You can follow this document to write schema:
 -- https://github.com/Tencent/rapidjson/blob/master/bin/draft-04/schema
 -- rapidjson not supported `format` in draft-04 yet
 function _M.check(schema, json)
-    local validator = cached_sd(schema, nil, create_validator, schema)[1]
+    local sd = schema_doc(schema)
+    local validator = schema_validator(sd)
 
     local d = json_doc(json)
     return validator:validate(d)
