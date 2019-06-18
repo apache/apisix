9ef6a9714911ddbc1900460dd88ac2467092525c
Yuansheng <membphis@gmail.com>
2019-06-18 11:13:48 +0800

    bugfix: supported to LRU table to cache the JSON schema validator.

--- lua/apisix/core/schema.lua
+++ lua/apisix/core/schema.lua
@@ -4,15 +4,26 @@ local schema_doc = json.SchemaDocument
 local json_doc = json.Document
 
 
+local cached_sd = require("apisix.core.lrucache").new({count = 1000, ttl = 0})
+
+
 local _M = {version = 0.1}
+
+
+local function create_validator(schema)
+    local sd = schema_doc(schema)
+    local validator = schema_validator(sd)
+
+    -- need to cache `validator` and `sd` object at same time
+    return {validator, sd}
+end
 
 
 -- You can follow this document to write schema:
 -- https://github.com/Tencent/rapidjson/blob/master/bin/draft-04/schema
 -- rapidjson not supported `format` in draft-04 yet
 function _M.check(schema, json)
-    local sd = schema_doc(schema)
-    local validator = schema_validator(sd)
+    local validator = cached_sd(schema, nil, create_validator, schema)[1]
 
     local d = json_doc(json)
     return validator:validate(d)
