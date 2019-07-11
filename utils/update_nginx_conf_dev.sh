3730e6ade1da23a9996b194ae2c75b06a9435de3
chnliyong <chnliyong@users.noreply.github.com>
2019-07-11 14:41:13 +0800

    bugfix: failed early when lua isn't installed on `make dev` (#233)

--- utils/update_nginx_conf_dev.sh
+++ utils/update_nginx_conf_dev.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-lua_version=`lua -e "print(_VERSION)" | grep -o -E "(5.[0-9])"`
+lua_version=`lua -e "print(_VERSION)" 2>/dev/null | grep -o -E "(5.[0-9])"`
 
-if [ $lua_version = "5.1" ];then
-    echo "current Lua version is 5.1, skip to update conf/nginx.conf"
+if [ -z "$lua_version" ]; then
+    echo "Lua 5.x environment (luarocks included) should be installed in advance."
+    exit 1
+elif [ $lua_version = "5.1" ];then
+    echo "Current Lua version is 5.1, skip to update conf/nginx.conf."
     exit
 fi
 
