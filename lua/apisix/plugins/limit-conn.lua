0608f0658b954b132a62d3d737dd4eb8c87b31f1
WenMing <moonbingbing@gmail.com>
2019-07-12 11:25:17 +0800

    feature: supported real_ip configure in nginx.conf and added functions to get ip and remote ip. (#236)
    * feature: supported real_ip configure in nginx.conf and added functions to get ip and remote ip.
    
    * fixed code style.

--- lua/apisix/plugins/limit-conn.lua
+++ lua/apisix/plugins/limit-conn.lua
@@ -49,7 +49,8 @@ function _M.access(conf, ctx)
         return 500
     end
 
-    local key = (ctx.var[conf.key] or "") .. ctx.conf_version
+    local ip = core.request.get_remote_client_ip(ctx)
+    local key = ip .. ctx.conf_version
     local rejected_code = conf.rejected_code
 
     local delay, err = lim:incoming(key, true)
