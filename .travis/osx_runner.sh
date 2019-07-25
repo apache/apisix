d899f9bfab8984ce5c852303e15e1e63acd5524b
WenMing <moonbingbing@gmail.com>
2019-07-25 10:32:30 +0800

    bugfix: fixed doc and travis about osx.

--- .travis/osx_runner.sh
+++ .travis/osx_runner.sh
@@ -17,7 +17,7 @@ export_or_prefix() {
 }
 
 before_install() {
-    HOMEBREW_NO_AUTO_UPDATE=1 brew install perl cpanminus etcd luarocks openresty/brew/openresty-debug tree
+    HOMEBREW_NO_AUTO_UPDATE=1 brew install perl cpanminus etcd luarocks openresty/brew/openresty-debug
     sudo cpanm --notest Test::Nginx >build.log 2>&1 || (cat build.log && exit 1)
     export_or_prefix
     luarocks install --lua-dir=${OPENRESTY_PREFIX}/luajit luacov-coveralls --local --tree=deps
