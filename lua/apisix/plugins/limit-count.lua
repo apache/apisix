877b72e21d1a1e1d009ea8db7f5297d86b533c2c
WenMing <moonbingbing@gmail.com>
2019-07-25 10:50:44 +0800

    bugfix: added conf_type to the key of limit plugins.

--- lua/apisix/plugins/limit-count.lua
+++ lua/apisix/plugins/limit-count.lua
@@ -45,7 +45,7 @@ function _M.access(conf, ctx)
     end
 
     local ip = core.request.get_remote_client_ip(ctx)
-    local key = ip .. ctx.conf_version
+    local key = ip .. ctx.conf_type .. ctx.conf_version
 
     local delay, remaining = lim:incoming(key, true)
     if not delay then
