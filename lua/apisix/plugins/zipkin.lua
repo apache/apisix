54a55b1301149cd8d4eda9f6c08f0cb822a943ea
WenMing <moonbingbing@gmail.com>
2019-07-26 08:38:05 +0800

    fixed code style.

--- lua/apisix/plugins/zipkin.lua
+++ lua/apisix/plugins/zipkin.lua
@@ -44,20 +44,20 @@ end
 
 local function create_tracer(conf)
     local tracer = new_tracer(new_reporter(conf), new_random_sampler(conf))
-	tracer:register_injector("http_headers", zipkin_codec.new_injector())
-	tracer:register_extractor("http_headers", zipkin_codec.new_extractor())
-	return tracer
+    tracer:register_injector("http_headers", zipkin_codec.new_injector())
+    tracer:register_extractor("http_headers", zipkin_codec.new_extractor())
+    return tracer
 end
 
 local function report2endpoint(premature, reporter)
-	if premature then
-		return
-	end
+    if premature then
+        return
+    end
 
-	local ok, err = reporter:flush()
-	if not ok then
-		core.log.error("reporter flush ", err)
-		return
+    local ok, err = reporter:flush()
+    if not ok then
+        core.log.error("reporter flush ", err)
+        return
     end
 
     core.log.info("report2endpoint ok")
@@ -162,11 +162,11 @@ function _M.log(conf, ctx)
     opentracing.proxy_span:finish(log_end_time)
     opentracing.request_span:finish(log_end_time)
 
-	local reporter = opentracing.tracer.reporter
-	local ok, err = ngx.timer.at(0, report2endpoint, reporter)
-	if not ok then
-		core.log.error("failed to create timer: ", err)
-	end
+    local reporter = opentracing.tracer.reporter
+    local ok, err = ngx.timer.at(0, report2endpoint, reporter)
+    if not ok then
+        core.log.error("failed to create timer: ", err)
+    end
 end
 
 return _M
