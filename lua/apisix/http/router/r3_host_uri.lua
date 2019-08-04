364f72a93fd34bfd3366cb93db28cc39d8b6a399
Yuansheng <membphis@gmail.com>
2019-08-04 09:54:30 +0800

    doc(architecture): added new doc about `router`.

--- lua/apisix/http/router/r3_host_uri.lua
+++ lua/apisix/http/router/r3_host_uri.lua
@@ -13,20 +13,17 @@ local routes
 
 local _M = {version = 0.1}
 
-
-    local empty_tab = {}
-    local route_items
-local function create_r3_router(routes)
-    routes = routes or empty_tab
-
+    local api_routes = {}
+    local api_router
+local function create_api_router()
     local api_routes = plugin.api_routes()
-    route_items = core.table.new(#api_routes + #routes, 0)
-    local idx = 0
+    core.table.clear(api_routes)
 
+    local idx = 0
     for _, route in ipairs(api_routes) do
         if type(route) == "table" then
             idx = idx + 1
-            route_items[idx] = {
+            api_routes[idx] = {
                 path = route.uri,
                 handler = route.handler,
                 method = route.methods,
@@ -34,35 +31,57 @@ local function create_r3_router(routes)
         end
     end
 
-    for _, route in ipairs(routes) do
-        if type(route) == "table" then
-            idx = idx + 1
+    api_router = r3router.new(api_routes)
+    api_router:compile()
+    return true
+end
 
-            local host = route.value.host
-            if not host then
-                host = [=[{domain:[^/]+}]=]
 
-            else
-                host = str_reverse(host)
-                if host:sub(#host) == "*" then
-                    host = host:sub(1, #host - 1) .. "{prefix:.*}"
-                end
-            end
-
-            core.log.info("route rule: ", host .. route.value.uri)
-            route_items[idx] = {
-                path = host .. route.value.uri,
-                method = route.value.methods,
-                handler = function (params, api_ctx)
-                    api_ctx.matched_params = params
-                    api_ctx.matched_route = route
-                end
-            }
-        end
+    local req_routes = {}
+    local req_routes_idx = 0
+local function push_valid_route(route)
+    if type(route) ~= "table" then
+        return
     end
 
-    core.log.info("route items: ", core.json.delay_encode(route_items, true))
-    local r3 = r3router.new(route_items)
+    local host = route.value.host
+    if not host then
+        core.log.error("missing `host` field in route: ",
+                        core.json.delay_encode(route))
+        return
+    end
+
+    host = str_reverse(host)
+    if host:sub(#host) == "*" then
+        host = host:sub(1, #host - 1) .. "{host_prefix}"
+    end
+
+    core.log.info("route rule: ", host .. route.value.uri)
+    req_routes_idx = req_routes_idx + 1
+    req_routes[req_routes_idx] = {
+        path = "/" .. host .. route.value.uri,
+        method = route.value.methods,
+        handler = function (params, api_ctx)
+            api_ctx.matched_params = params
+            api_ctx.matched_route = route
+        end
+    }
+
+    return
+end
+
+local function create_r3_router(routes)
+    create_api_router()
+
+    core.table.clear(req_routes)
+    req_routes_idx = 0
+
+    for _, route in ipairs(routes or {}) do
+        push_valid_route(route)
+    end
+
+    core.log.info("route items: ", core.json.delay_encode(req_routes, true))
+    local r3 = r3router.new(req_routes)
     r3:compile()
     return r3
 end
@@ -80,18 +99,19 @@ function _M.match(api_ctx)
     core.table.clear(match_opts)
     match_opts.method = api_ctx.var.method
 
-    local host = api_ctx.var.host
-    host = host and str_reverse(host) or "[^/]+"
-    host = host .. api_ctx.var.uri
-    core.log.info("match string: ", host)
-
-    local ok = router:dispatch2(nil, host, match_opts, api_ctx)
-    if not ok then
-        core.log.info("not find any matched route")
-        return core.response.exit(404)
+    local host_uri = "/" .. str_reverse(api_ctx.var.host) .. api_ctx.var.uri
+    local ok = router:dispatch2(nil, host_uri, match_opts, api_ctx)
+    if ok then
+        return true
     end
 
-    return true
+    ok = router:dispatch2(nil, api_ctx.var.uri, match_opts, api_ctx)
+    if ok then
+        return true
+    end
+
+    core.log.info("not find any matched route")
+    return core.response.exit(404)
 end
 
 
