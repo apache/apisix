19e09b539fdb6566096f143235cc38d92218b9bb
Yuansheng <membphis@gmail.com>
2019-08-04 12:48:23 +0800

    feature: supported `PATCH` method for admin `routes` API.

--- lua/apisix/admin/routes.lua
+++ lua/apisix/admin/routes.lua
@@ -4,7 +4,7 @@ local tostring = tostring
 
 
 local _M = {
-    version = 0.1,
+    version = 0.2,
 }
 
 
@@ -139,6 +139,77 @@ function _M.delete(id)
     local res, err = core.etcd.delete(key)
     if not res then
         core.log.error("failed to delete route[", key, "]: ", err)
+        return 500, {error_msg = err}
+    end
+
+    return res.status, res.body
+end
+
+
+function _M.patch(id, conf, sub_path)
+    if not id then
+        return 400, {error_msg = "missing route id"}
+    end
+
+    if not sub_path then
+        return 400, {error_msg = "missing sub-path"}
+    end
+
+    if not conf then
+        return 400, {error_msg = "missing new configuration"}
+    end
+
+    local key = "/routes"
+    if id then
+        key = key .. "/" .. id
+    end
+
+    local res_old, err = core.etcd.get(key)
+    if not res_old then
+        core.log.error("failed to delete route[", key, "]: ", err)
+        return 500, {error_msg = err}
+    end
+
+    if res_old.status ~= 200 then
+        return res_old.status, res_old.body
+    end
+    core.log.info("key: ", key, " old value: ",
+                  core.json.delay_encode(res_old, true))
+
+    local node_value = res_old.body.node.value
+    local sub_value = node_value
+    local sub_paths = core.utils.split_uri(sub_path)
+    for i = 1, #sub_paths - 1 do
+        local sub_name = sub_paths[i]
+        if sub_value[sub_name] == nil then
+            sub_value[sub_name] = {}
+        end
+
+        sub_value = sub_value[sub_name]
+
+        if type(sub_value) ~= "table" then
+            return 400, "invalid sub-path: /"
+                        .. core.table.concat(sub_paths, 1, i)
+        end
+    end
+
+    if type(sub_value) ~= "table" then
+        return 400, "invalid sub-path: /" .. sub_path
+    end
+
+    local sub_name = sub_paths[#sub_paths]
+    sub_value[sub_name] = conf
+    core.log.info("new conf: ", core.json.delay_encode(node_value, true))
+
+    local id, err = check_conf(id, node_value, true)
+    if not id then
+        return 400, err
+    end
+
+    -- TODO: this is not safe, we need to use compare-set
+    local res, err = core.etcd.set(key, node_value)
+    if not res then
+        core.log.error("failed to set new route[", key, "]: ", err)
         return 500, {error_msg = err}
     end
 
