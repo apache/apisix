3be5fe714b5f3048ddba79f6099d960c6f43a88b
Yuansheng <membphis@gmail.com>
2019-08-05 13:08:15 +0800

    feature: supported `PATCH` method for `upstream` API.

--- lua/apisix/admin/upstreams.lua
+++ lua/apisix/admin/upstreams.lua
@@ -147,4 +147,75 @@ function _M.delete(id)
 end
 
 
+function _M.patch(id, conf, sub_path)
+    if not id then
+        return 400, {error_msg = "missing upstream id"}
+    end
+
+    if not sub_path then
+        return 400, {error_msg = "missing sub-path"}
+    end
+
+    if not conf then
+        return 400, {error_msg = "missing new configuration"}
+    end
+
+    local key = "/upstreams" .. "/" .. id
+    local res_old, err = core.etcd.get(key)
+    if not res_old then
+        core.log.error("failed to delete upstream[", key, "]: ", err)
+        return 500, {error_msg = err}
+    end
+
+    if res_old.status ~= 200 then
+        return res_old.status, res_old.body
+    end
+    core.log.info("key: ", key, " old value: ",
+                  core.json.delay_encode(res_old, true))
+
+    local new_value = res_old.body.node.value
+    local sub_value = new_value
+    local sub_paths = core.utils.split_uri(sub_path)
+    for i = 1, #sub_paths - 1 do
+        local sub_name = sub_paths[i]
+        if sub_value[sub_name] == nil then
+            sub_value[sub_name] = {}
+        end
+
+        sub_value = sub_value[sub_name]
+
+        if type(sub_value) ~= "table" then
+            return 400, "invalid sub-path: /"
+                        .. core.table.concat(sub_paths, 1, i)
+        end
+    end
+
+    if type(sub_value) ~= "table" then
+        return 400, "invalid sub-path: /" .. sub_path
+    end
+
+    local sub_name = sub_paths[#sub_paths]
+    if sub_name and sub_name ~= "" then
+        sub_value[sub_name] = conf
+    else
+        new_value = conf
+    end
+    core.log.info("new value ", core.json.delay_encode(new_value, true))
+
+    local id, err = check_conf(id, new_value, true)
+    if not id then
+        return 400, err
+    end
+
+    -- TODO: this is not safe, we need to use compare-set
+    local res, err = core.etcd.set(key, new_value)
+    if not res then
+        core.log.error("failed to set new upstream[", key, "]: ", err)
+        return 500, {error_msg = err}
+    end
+
+    return res.status, res.body
+end
+
+
 return _M
