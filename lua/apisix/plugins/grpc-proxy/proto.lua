19a880c736eb8b99a0675afba532f93f1c2aa10a
nic-chen <johz@163.com>
2019-08-15 18:12:29 +0800

    feat fetch proto

--- lua/apisix/plugins/grpc-proxy/proto.lua
+++ lua/apisix/plugins/grpc-proxy/proto.lua
@@ -2,30 +2,36 @@ local core = require("apisix.core")
 local protoc = require("protoc")
 local util = require("apisix.plugins.grpc-proxy.util")
 
+
 local _M = {}
 
 _M.new = function(proto_id)
   local _p = protoc.new()
 
-  local err
-  proto_etcd, err = core.config.new("/proto", {
-                              automatic = true,
-                              item_schema = core.schema.proto
-                          })
-  if not proto_etcd then
-      ngx.log(ngx.ERR, "failed to create etcd instance for fetching proto:" .. err)
-      return
-  end
+    local key = "/proto/" .. proto_id
+    local res, err = core.etcd.get(key)
 
-  local proto_obj = proto_etcd:get(tostring(proto_id))
+    local proto_obj = res.body.node.value
+
+  -- local err
+  -- proto_etcd, err = core.config.new("/proto", {
+  --                             automatic = true,
+  --                             item_schema = core.schema.proto
+  --                         })
+  -- if not proto_etcd then
+  --     ngx.log(ngx.ERR, "failed to create etcd instance for fetching proto:" .. err)
+  --     return
+  -- end
+
+  --local proto_obj = proto_etcd:get(tostring(proto_id))
   if not proto_obj then
       ngx.log(ngx.ERR, "failed to find proto by id: " .. proto_id)
       return 
   end
 
-  ngx.log(ngx.ERR, "proto content:" .. proto_obj.value.content)
+  ngx.log(ngx.ERR, "proto content:" .. proto_obj.content)
 
-  _p:load(proto_obj.value.content)
+  _p:load(proto_obj.content)
 
   local instance = {}
   instance.get_loaded_proto = function()
