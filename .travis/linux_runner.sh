360dc43c5db1b7fdcc1a8089f80ee0ae380f5fae
nic-chen <johz@163.com>
2019-08-18 09:45:16 +0800

    fix Hash Sum mismatch

--- .travis/linux_runner.sh
+++ .travis/linux_runner.sh
@@ -23,6 +23,7 @@ before_install() {
 
 do_install() {
     wget -qO - https://openresty.org/package/pubkey.gpg | sudo apt-key add -
+    sudo apt-get -y update --fix-missing
     sudo apt-get -y install software-properties-common
     sudo add-apt-repository -y "deb http://openresty.org/package/ubuntu $(lsb_release -sc) main"
     sudo apt-get update
