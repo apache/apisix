d084d9d75ed53164451f115b01708e1ce19e707c
Yuansheng <membphis@gmail.com>
2019-08-20 17:34:13 +0800

    change: added more meaning message.

--- lua/apisix.lua
+++ lua/apisix.lua
@@ -151,7 +151,7 @@ function _M.http_access_phase()
 
     router.router_http.match(api_ctx)
 
-    core.log.info("route: ",
+    core.log.info("matched route: ",
                   core.json.delay_encode(api_ctx.matched_route, true))
 
     local route = api_ctx.matched_route
