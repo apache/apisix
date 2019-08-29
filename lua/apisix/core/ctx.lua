5f051fcc03ae85c15de2744d9e84d259c260ef27
Yuansheng <membphis@gmail.com>
2019-08-29 03:32:40 -0400

    feature: supported to fetch cookie value from ctx.var which is a easy way.

--- lua/apisix/core/ctx.lua
+++ lua/apisix/core/ctx.lua
@@ -1,15 +1,26 @@
-local tablepool = require("tablepool")
-local get_var = require("resty.ngxvar").fetch
-local get_request = require("resty.ngxvar").request
+local log          = require("apisix.core.log")
+local tablepool    = require("tablepool")
+local get_var      = require("resty.ngxvar").fetch
+local get_request  = require("resty.ngxvar").request
+local ck           = require "resty.cookie"
 local setmetatable = setmetatable
+local ffi          = require("ffi")
+local C            = ffi.C
+local sub_str      = string.sub
 
 
-local _M = {version = 0.1}
+ffi.cdef[[
+int memcmp(const void *s1, const void *s2, size_t n);
+]]
+
+
+local _M = {version = 0.2}
 
 
 do
     local var_methods = {
-        ["method"] = ngx.req.get_method
+        ["method"] = ngx.req.get_method,
+        ["cookie"] = function () return ck:new() end
     }
 
     local mt = {
@@ -18,6 +29,17 @@ do
             local method = var_methods[name]
             if method then
                 val = method()
+
+            elseif C.memcmp(name, "cookie_", 7) == 0 then
+                local cookie = t["cookie"]
+                if cookie then
+                    local err
+                    val, err = cookie:get(sub_str(name, 8))
+                    if not val then
+                        log.warn("failed to fetch cookie value by name: ",
+                                 name, " error: ", err)
+                    end
+                end
 
             else
                 val = get_var(name, t._request)
