742d94612fbc6e204cdfba26a7e9bca66918a0d2
bobopan <273719650@qq.com>
2019-10-11 09:23:14 +0800

    Error file submitted

--- lua/apisix/http/router/radixtree_host_uri.lua
+++ lua/apisix/http/router/radixtree_host_uri.lua
@@ -1,4 +1,4 @@
--- Copyright (C) Yuansheng Wang
+-- Copyright (C) bobo pan
 
 local require = require
 local router = require("resty.radixtree")
@@ -6,51 +6,115 @@ local core = require("apisix.core")
 local plugin = require("apisix.plugin")
 local ipairs = ipairs
 local type = type
-local error = error
 local str_reverse = string.reverse
+local error = error
 local user_routes
 local cached_version
+local only_uri_routes = {}
+local only_uri_router
+local host_uri_routes = {}
+local host_uri_router
 
 
 local _M = {version = 0.1}
 
 
-    local uri_routes = {}
-    local uri_router
-local function create_radixtree_router(routes)
-    routes = routes or {}
-
-    local api_routes = plugin.api_routes()
-    core.table.clear(uri_routes)
-
-    for _, route in ipairs(api_routes) do
+local function create_only_uri_router()
+    local routes = plugin.api_routes()
+    core.log.error("routes", core.json.encode(routes))
+    for _, route in ipairs(routes) do
         if type(route) == "table" then
-            core.table.insert(uri_routes, {
-                path = route.uri,
+            core.table.insert(only_uri_routes, {
+                paths = route.uris or route.uri,
                 handler = route.handler,
                 method = route.methods,
             })
         end
     end
+    only_uri_router = router.new(only_uri_routes)
+    return true
+end
 
-    for _, route in ipairs(routes) do
-        if type(route) == "table" then
-            core.table.insert(uri_routes, {
-                path = route.value.uri,
-                method = route.value.methods,
-                host = route.value.host,
-                remote_addr = route.value.remote_addr,
-                vars = route.value.vars,
-                handler = function (api_ctx)
-                    api_ctx.matched_params = nil
-                    api_ctx.matched_route = route
+
+local function add_host_uri_routes(paths, hosts, route)
+    core.log.info("route rule: ", paths)
+    core.table.insert(host_uri_routes, {
+        paths = paths,
+        methods = route.value.methods,
+        hosts = hosts,
+        remote_addrs = route.value.remote_addrs or route.value.remote_addr,
+        vars = route.value.vars,
+        handler = function (api_ctx)
+            api_ctx.matched_params = nil
+            api_ctx.matched_route = route
+        end
+    })
+end
+
+
+local function push_radixtree_host_router(route)
+    if type(route) ~= "table" then
+        return
+    end
+
+    local host = route.value.host
+    local hosts = route.value.hosts
+    local uri = route.value.uris or route.value.uri
+    if (not host or type(host) ~= "string") and (not hosts or type(hosts) ~= "table") then
+        core.table.insert(only_uri_routes, {
+            paths = route.value.uris or route.value.uri,
+            method = route.value.methods,
+            hosts = hosts or host,
+            remote_addrs = route.value.remote_addrs or route.value.remote_addr,
+            vars = route.value.vars,
+            handler = function (api_ctx)
+                api_ctx.matched_params = nil
+                api_ctx.matched_route = route
+            end
+        })
+        return
+    end
+    --more hosts
+    if hosts and type(hosts) == "table" then
+        for _, host_path in ipairs(hosts) do
+            local paths = "/" .. str_reverse(host_path)
+            if type(uri) == 'table' then
+                for _, uri_path in ipairs(uri) do
+                    add_host_uri_routes(paths .. uri_path, host_path, route)
                 end
-            })
+            else
+                add_host_uri_routes(paths .. uri, host_path, route)
+            end
         end
     end
 
-    core.log.info("route items: ", core.json.delay_encode(uri_routes, true))
-    uri_router = router.new(uri_routes)
+    -- only one host
+    if host and type(host) == "string" then
+        local paths = "/" .. str_reverse(host)
+        if type(uri) == 'table' then
+            for _, uri_path in ipairs(uri) do
+                add_host_uri_routes(paths .. uri_path, host, route)
+            end
+        else
+            add_host_uri_routes(paths .. uri, host, route)
+        end
+    end
+    return
+end
+
+
+local function create_radixtree_router(routes)
+    core.table.clear(host_uri_routes)
+    core.table.clear(only_uri_routes)
+    for _, route in ipairs(routes or {}) do
+        push_radixtree_host_router(route)
+    end
+
+    create_only_uri_router()
+
+    core.log.info("route items: ",
+                  core.json.delay_encode(host_uri_routes, true))
+    host_uri_router = router.new(host_uri_routes)
 end
 
 
@@ -61,7 +125,7 @@ function _M.match(api_ctx)
         cached_version = user_routes.conf_version
     end
 
-    if not uri_router then
+    if not host_uri_router then
         core.log.error("failed to fetch valid `uri` router: ")
         return core.response.exit(404)
     end
@@ -72,13 +136,19 @@ function _M.match(api_ctx)
     match_opts.remote_addr = api_ctx.var.remote_addr
     match_opts.vars = api_ctx.var
 
-    local ok = uri_router:dispatch(api_ctx.var.uri, match_opts, api_ctx)
-    if not ok then
-        core.log.info("not find any matched route")
-        return core.response.exit(404)
+    local host_uri = "/" .. str_reverse(api_ctx.var.host) .. api_ctx.var.uri
+    local ok = host_uri_router:dispatch(host_uri, match_opts, api_ctx)
+    if ok then
+        return true
     end
 
-    return true
+    ok = only_uri_router:dispatch(api_ctx.var.uri, match_opts, api_ctx)
+    if ok then
+        return true
+    end
+
+    core.log.info("not find any matched route")
+    return core.response.exit(404)
 end
 
 
@@ -91,11 +161,12 @@ function _M.routes()
 end
 
 
-function _M.init_worker()
+function _M.init_worker(filter)
     local err
     user_routes, err = core.config.new("/routes", {
             automatic = true,
-            item_schema = core.schema.route
+            item_schema = core.schema.route,
+            filter = filter,
         })
     if not user_routes then
         error("failed to create etcd instance for fetching /routes : " .. err)
