32052203202bba89b268867f54bb4f5f671836c9
YuanSheng Wang <membphis@gmail.com>
2019-10-14 15:21:13 +0800

    luarocks: support to install apisix without dashboard. (#686)
    * luarocks: support to install apisix without dashboard.

--- .travis/linux_runner.sh
+++ .travis/linux_runner.sh
@@ -17,7 +17,7 @@ export_or_prefix() {
 }
 
 create_lua_deps() {
-    sudo luarocks make --lua-dir=${OPENRESTY_PREFIX}/luajit rockspec/apisix-dev-1.0-0.rockspec --tree=deps --only-deps --local
+    WITHOUT_DASHBOARD=1 sudo luarocks make --lua-dir=${OPENRESTY_PREFIX}/luajit rockspec/apisix-dev-1.0-0.rockspec --tree=deps --only-deps --local
     sudo luarocks install --lua-dir=${OPENRESTY_PREFIX}/luajit lua-resty-libr3 --tree=deps --local
     echo "Create lua deps cache"
     sudo rm -rf build-cache/deps
