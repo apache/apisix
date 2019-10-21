57c61006b23572204b0f09e75f22278802a4f4f0
Lien <lilien1010@users.noreply.github.com>
2019-10-21 16:11:02 +0800

    Revert "bugfix: support more Nginx-built-in Paramters when set balancer type as consistent_hash"
    This reverts commit acc1f7cfec49f22aee0dff3612899c45275a2e80.

--- lua/apisix/schema_def.lua
+++ lua/apisix/schema_def.lua
@@ -236,7 +236,7 @@ local upstream_schema = {
         key = {
             description = "the key of chash for dynamic load balancing",
             type = "string",
-            pattern = [[^((uri|server_name|server_addr|request_uri|remote_port|remote_addr|query_string|host|hostname)|arg_[0-9a-zA-z_-]+)$]]
+            enum = {"remote_addr"},
         },
         desc = {type = "string", maxLength = 256},
         id = id_schema
