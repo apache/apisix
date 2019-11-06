bf2fb9387c3e347775044814c36f071d6ece3f96
spacewander <spacewanderlzx@gmail.com>
2019-11-06 11:34:12 +0800

    fix: core.utils.parse_addr didn't parse IPv6 address correctly

--- lua/apisix/core/utils.lua
+++ lua/apisix/core/utils.lua
@@ -21,7 +21,7 @@ local ipmatcher= require("resty.ipmatcher")
 local open     = io.open
 local math     = math
 local sub_str  = string.sub
-local find_str = string.find
+local str_byte = string.byte
 local tonumber = tonumber
 
 
@@ -83,15 +83,54 @@ function _M.dns_parse(resolvers, domain)
 end
 
 
-function _M.parse_addr(addr)
-    local pos = find_str(addr, ":", 1, true)
-    if not pos then
-        return addr, 80
+local function rfind_char(s, ch, idx)
+    local b = str_byte(ch)
+    for i = idx or #s, 1, -1 do
+        if str_byte(s, i, i) == b then
+            return i
+        end
     end
+    return nil
+end
 
-    local host = sub_str(addr, 1, pos - 1)
-    local port = sub_str(addr, pos + 1)
-    return host, tonumber(port)
+
+-- parse_addr parses 'addr' into the host and the port parts. If the 'addr'
+-- doesn't have a port, 80 is used to return. For malformed 'addr', the entire
+-- 'addr' is returned as the host part. For IPv6 literal host, like [::1], the
+-- square brackets will be removed.
+function _M.parse_addr(addr)
+    local default_port = 80
+    if str_byte(addr, 1) == str_byte("[") then
+        -- IPv6 format
+        local right_bracket = str_byte("]")
+        local len = #addr
+        if str_byte(addr, len) == right_bracket then
+            -- addr in [ip:v6] format
+            return sub_str(addr, 2, len-1), default_port
+        else
+            local pos = rfind_char(addr, ":", #addr - 1)
+            if not pos or str_byte(addr, pos - 1) ~= right_bracket then
+                -- malformed addr
+                return addr, default_port
+            end
+
+            -- addr in [ip:v6]:port format
+            local host = sub_str(addr, 2, pos - 2)
+            local port = sub_str(addr, pos + 1)
+            return host, tonumber(port)
+        end
+
+    else
+        -- IPv4 format
+        local pos = rfind_char(addr, ":", #addr - 1)
+        if not pos then
+            return addr, default_port
+        end
+
+        local host = sub_str(addr, 1, pos - 1)
+        local port = sub_str(addr, pos + 1)
+        return host, tonumber(port)
+    end
 end
 
 
