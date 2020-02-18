45c26d3949513c022685336c9c9b1bf414af56ce
Yuansheng <membphis@gmail.com>
2020-02-18 16:29:11 +0800

    no change

--- lua/apisix/core/profile.lua
+++ lua/apisix/core/profile.lua
@@ -14,6 +14,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 --
-local profile = os.getenv("APISIX_PROFILE")
 
-return {profile = profile}
+return {
+    profile = os.getenv("APISIX_PROFILE")
+}
