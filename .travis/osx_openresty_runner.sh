531fc71a7686ca3bea5eb90c1bb5d28fe0865dd9
Akayeshmantha <akayeshmantha@apache.org>
2020-03-21 17:42:06 +0100

    Kafka Logger.

--- .travis/osx_openresty_runner.sh
+++ .travis/osx_openresty_runner.sh
@@ -33,6 +33,26 @@ before_install() {
     sudo cpanm --notest Test::Nginx >build.log 2>&1 || (cat build.log && exit 1)
     export_or_prefix
     luarocks install --lua-dir=${OPENRESTY_PREFIX}/luajit luacov-coveralls --local --tree=deps
+
+    # spin up kafka cluster for tests (1 zookeper and 1 kafka instance)
+    export ZK_VER=3.5.7
+    export SCALA_VER=2.11
+    export KAFKA_VER=2.4.0
+
+    if [ ! -f download-cache/kafka_$SCALA_VER-$KAFKA_VER.tgz ]; then wget -P download-cache http://mirrors.tuna.tsinghua.edu.cn/apache/kafka/$KAFKA_VER/kafka_$SCALA_VER-$KAFKA_VER.tgz;fi
+    if [ ! -f download-cache/apache-zookeeper-$ZK_VER-bin.tar.gz ]; then wget -P download-cache https://mirrors.tuna.tsinghua.edu.cn/apache/zookeeper/zookeeper-$ZK_VER/apache-zookeeper-$ZK_VER-bin.tar.gz;fi
+
+    sudo tar -xzf download-cache/apache-zookeeper-$ZK_VER-bin.tar.gz -C /usr/local/
+    sudo tar -xzf download-cache/kafka_$SCALA_VER-$KAFKA_VER.tgz -C /usr/local/
+    sudo mv /usr/local/kafka_$SCALA_VER-$KAFKA_VER /usr/local/kafka
+    sudo mv /usr/local/apache-zookeeper-$ZK_VER-bin /usr/local/zookeeper
+    sudo cp /usr/local/zookeeper/conf/zoo_sample.cfg /usr/local/zookeeper/conf/zoo.cfg
+    sudo sed -i '' '$a\
+    host\.name=127.0.0.1' /usr/local/kafka/config/server.properties
+    sudo /usr/local/zookeeper/bin/zkServer.sh start
+    sudo /usr/local/kafka/bin/kafka-server-start.sh  -daemon /usr/local/kafka/config/server.properties
+    sleep 1
+    /usr/local/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181  --replication-factor 1 --partitions 2 --topic test2
 }
 
 do_install() {
--- t/debug/debug-mode.t
+++ t/debug/debug-mode.t
@@ -75,6 +75,7 @@ loaded plugin and sort by priority: 899 name: response-rewrite
 loaded plugin and sort by priority: 506 name: grpc-transcode
 loaded plugin and sort by priority: 500 name: prometheus
 loaded plugin and sort by priority: 405 name: tcp-logger
+loaded plugin and sort by priority: 403 name: kafka-logger
 loaded plugin and sort by priority: 400 name: udp-logger
 loaded plugin and sort by priority: 0 name: example-plugin
 loaded plugin and sort by priority: -1000 name: zipkin
