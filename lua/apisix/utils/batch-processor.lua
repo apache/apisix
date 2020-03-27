0785551bbe687b9876443c3cea457b8eaada09be
sshniro <sshniro@gmail.com>
2020-03-27 10:55:33 +0100

    Fix for max_retry_count is 0 bug

--- lua/apisix/utils/batch-processor.lua
+++ lua/apisix/utils/batch-processor.lua
@@ -62,7 +62,7 @@ function execute_func(premature, batch_processor, batch)
         core.log.error(fmt("Batch Processor[%s] failed to process entries: ",
             batch_processor.name), err)
         batch.retry_count = batch.retry_count + 1
-        if batch.retry_count < batch_processor.max_retry_count then
+        if batch.retry_count <= batch_processor.max_retry_count then
             schedule_func_exec(batch_processor, batch_processor.retry_delay, batch)
         else
             core.log.error(fmt(("Batch Processor[%s] exceeded the max_retry_count[%d] "
