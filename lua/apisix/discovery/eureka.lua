158a4440f20f503191327c8491de256da0cf8b11
qiujiayu <153163285@qq.com>
2020-03-31 21:34:05 +0800

    test

--- lua/apisix/discovery/eureka.lua
+++ lua/apisix/discovery/eureka.lua
@@ -14,6 +14,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 --
+
 local local_conf         = require("apisix.core.config_local").local_conf()
 local http               = require("resty.http")
 local core               = require("apisix.core")
--- lua/apisix/discovery/init.lua
+++ lua/apisix/discovery/init.lua
@@ -14,6 +14,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 --
+
 local log          = require("apisix.core.log")
 local local_conf   = require("apisix.core.config_local").local_conf()
 
