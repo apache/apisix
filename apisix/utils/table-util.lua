1263b6ccd070d03cb20380f29797f2910b288a74
nic-chen <johz@163.com>
2020-05-25 14:51:08 +0800

    fix: lint error

--- apisix/utils/table-util.lua
+++ apisix/utils/table-util.lua
@@ -14,6 +14,9 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 --
+local ngx = ngx
+local pairs = pairs
+local type = type
 
 local _M = {}
 
