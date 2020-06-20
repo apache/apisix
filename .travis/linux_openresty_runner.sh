844118cbe2f90f136ed2ce48c67efa01679c192c
nic-chen <johz@163.com>
2020-06-20 16:48:47 +0800

    fix: admin port

--- .travis/linux_openresty_runner.sh
+++ .travis/linux_openresty_runner.sh
@@ -144,7 +144,7 @@ script() {
     sleep 1
 
     echo '127.0.0.1 nginx.test.com' > /etc/hosts
-    curl --cacert ./conf/cert/apisix_admin_ca.crt --key ./conf/cert/apisix_admin_client.key --cert ./conf/cert/apisix_admin_client.crt  https://nginx.test.com/apisix/admin/ssls
+    curl --cacert ./conf/cert/apisix_admin_ca.crt --key ./conf/cert/apisix_admin_client.key --cert ./conf/cert/apisix_admin_client.crt  https://nginx.test.com:9180/apisix/admin/ssls
 
     ./bin/apisix stop
     sleep 1
