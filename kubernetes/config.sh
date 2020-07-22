414a327e160cbea856a64a4c53dde5a4faf5283e
gxthrj <gxthrj@163.com>
2020-07-22 23:48:05 +0800

    style：modify kubernetes doc,make config.yaml from the latest version and remove some config not necessary

---
+++ kubernetes/config.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+export etcd_url='http://here_is_you_etcd_IP:2379'
+
+wget https://raw.githubusercontent.com/apache/incubator-apisix/master/conf/config.yaml
+
+sed -i -e ':a' -e 'N' -e '$!ba' -e "s/allow_admin[a-z: #\/._]*\n\( *- [0-9a-zA-Z: #\/._',]*\n*\)*//g" config.yaml
+
+sed -i -e "s%http://[0-9.]*:2379%`echo $etcd_url`%g" config.yaml
+
+sed -i -e '/#CONFIG_YAML#/{r config.yaml' -e 'd}' apisix-gw-config-cm.yaml
+
