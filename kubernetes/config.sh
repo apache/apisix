f37a1ddc0097c88ab396586c40c07efe1ac38b2a
kv <gxthrj@163.com>
2020-07-31 12:16:29 +0800

    doc(k8s): removed unnecessary configurations. (#1891)

--- kubernetes/config.sh
+++ kubernetes/config.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 #
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
@@ -15,11 +16,14 @@
 # limitations under the License.
 #
 
-apiVersion: v1
-data:
-  config.yaml: |
-    #CONFIG_YAML#
-kind: ConfigMap
-metadata:
-  name: apisix-gw-config.yaml
-  # namespace: default
+
+export etcd_url='http://$ETCD_IP_ADDRESS:2379'
+
+wget https://raw.githubusercontent.com/apache/incubator-apisix/master/conf/config.yaml
+
+sed -i -e ':a' -e 'N' -e '$!ba' -e "s/allow_admin[a-z: #\/._]*\n\( *- [0-9a-zA-Z: #\/._',]*\n*\)*//g" config.yaml
+
+sed -i -e "s%http://[0-9.]*:2379%`echo $etcd_url`%g" config.yaml
+
+sed -i -e '/#CONFIG_YAML#/{r config.yaml' -e 'd}' apisix-gw-config-cm.yaml
+
