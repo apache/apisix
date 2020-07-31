b198d2c1c1fd1dd32c0fb8ac6ddfde8b163badd0
taotao2014 <20034838@qq.com>
2020-07-31 11:49:55 +0800

    Revert "add core.request.get_scheme/host/port/http_version"
    This reverts commit 17290e106ab938f1f904d1dc62d8366256b4cc50.

--- apisix/core/request.lua
+++ apisix/core/request.lua
@@ -94,35 +94,7 @@ function _M.get_remote_client_port(ctx)
         ctx = ngx.ctx.api_ctx
     end
     return tonumber(ctx.var.remote_port)
-end
-
-function _M.get_scheme(ctx)
-    if not ctx then
-        ctx = ngx.ctx.api_ctx
-    end
-    return ngx.var.scheme or ''
-end
-
-function _M.get_host(ctx)
-    if not ctx then
-        ctx = ngx.ctx.api_ctx
-    end
-    return ngx.var.host or ''
-end
-
-function _M.get_port(ctx)
-    if not ctx then
-        ctx = ngx.ctx.api_ctx
-    end
-    return tonumber(ngx.var.server_port)
-end
-
-function _M.get_http_version(ctx)
-    if not ctx then
-        ctx = ngx.ctx.api_ctx
-    end
-    return ngx.req.http_version()
-end
+  end
 
 
 return _M
