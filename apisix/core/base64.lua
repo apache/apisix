74cd62f290f5bf6be13db2b12c9cfbf4443b07a8
yiyiyimu <wosoyoung@gmail.com>
2020-09-04 17:16:59 +0800

    fix lint

--- apisix/core/base64.lua
+++ apisix/core/base64.lua
@@ -1,4 +1,21 @@
+--
+-- Licensed to the Apache Software Foundation (ASF) under one or more
+-- contributor license agreements.  See the NOTICE file distributed with
+-- this work for additional information regarding copyright ownership.
+-- The ASF licenses this file to You under the Apache License, Version 2.0
+-- (the "License"); you may not use this file except in compliance with
+-- the License.  You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
 local bit = bit
+local table = table
 local band, lshift, rshift = bit.band, bit.lshift, bit.rshift
 
 local _M = {}
@@ -28,11 +45,13 @@ function _M.encode(str)
     while pos <= len do
         res[#res+1] = base64_tab[rand(str:byte(pos+0), 0xfc, 2)]
         if pos+1 <= len then
-            res[#res+1] = base64_tab[land(str:byte(pos+0), 0x03, 4) + rand(str:byte(pos+1), 0xf0, 4)]
+            res[#res+1] = base64_tab[land(str:byte(pos+0), 0x03, 4)
+                                   + rand(str:byte(pos+1), 0xf0, 4)]
             if pos+2 <= len then
-                res[#res+1] = base64_tab[land(str:byte(pos+1), 0x0f, 2) + rand(str:byte(pos+2), 0xc0, 6)]
+                res[#res+1] = base64_tab[land(str:byte(pos+1), 0x0f, 2)
+                                       + rand(str:byte(pos+2), 0xc0, 6)]
                 res[#res+1] = base64_tab[band(str:byte(pos+2), 0x3f)]
-            else 
+            else
                 res[#res+1] = base64_tab[land(str:byte(pos+1), 0x0f, 2)]
                 res[#res+1] = base64_pad
             end
