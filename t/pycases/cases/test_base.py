e643c4647547e4f1adbf7d6f87e3b4276349fced
idbeta <idbeta@gmail.com>
2020-09-23 15:24:28 +0800

    modified:   t/pycases/cases/test_base.py

--- t/pycases/cases/test_base.py
+++ t/pycases/cases/test_base.py
@@ -25,37 +25,41 @@ def setup_module():
     global headers,nginx_pid,apisixhost
     apisixhost = "http://127.0.0.1:9080"
     headers = {"X-API-KEY": "edd1c9f034335f136f87ad84b625c8f1"}
-    casepath = cur_file_dir()
-    confpath = casepath + "/nginx.conf"
-    p = subprocess.Popen(['openresty', '-p',casepath,'-c',confpath], stderr = subprocess.PIPE, stdout = subprocess.PIPE, shell = False) 
-    p.wait()
-    nginx_pid = p.pid+1
+    # casepath = cur_file_dir()
+    # confpath = casepath + "/nginx.conf"
+    # p = subprocess.Popen(['openresty', '-p',casepath,'-c',confpath], stderr = subprocess.PIPE, stdout = subprocess.PIPE, shell = False) 
+    # p.wait()
+    # nginx_pid = p.pid+1
 
 def teardown_module():
-    killprocesstree(nginx_pid)
+    pass
+    #killprocesstree(nginx_pid)
 
 def test_01():
     cfgdata = {
-    "uri": "/hello",
+    "uri": "/apisix/status",
     "upstream": {
         "type": "roundrobin",
         "nodes": {
-            "127.0.0.1:9000": 1
+            "39.97.63.215:80": 1
         }
     }
 }
     r = requests.put("%s/apisix/admin/routes/1"%apisixhost, json=cfgdata,headers=headers )
     r = json.loads(r.content)
     assert r["action"] == "set"
-    r = requests.get("%s/hello"%apisixhost)
-    assert r.status_code == 200 and "Hello, World!" in r.content
-    r = geturl("%s/hello"%apisixhost,10)
+    r = requests.get("%s/apisix/status"%apisixhost)
+    assert r.status_code == 200
+    r = geturl("%s/apisix/status"%apisixhost,1)
     print(len(r))
     assert all(i == 200 for i in r)
 
     r = requests.delete("%s/apisix/admin/routes/1"%apisixhost, headers=headers )
     r = requests.get("%s/hello"%apisixhost)
     assert r.status_code == 404
-    r = geturl("%s/hello"%apisixhost,10)
+    r = geturl("%s/hello"%apisixhost,1)
     assert all(i == 404 for i in r)
 
+setup_module()
+test_01()
+teardown_module()
