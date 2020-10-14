15536524f1ce62d941c103d369798658db494ab8
spacewander <spacewanderlzx@gmail.com>
2020-10-14 15:44:21 +0800

    localize

--- apisix/admin/plugin_metadata.lua
+++ apisix/admin/plugin_metadata.lua
@@ -14,6 +14,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 --
+local error   = error
 local pcall   = pcall
 local require = require
 local core    = require("apisix.core")
