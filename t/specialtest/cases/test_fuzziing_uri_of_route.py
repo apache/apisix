155302485262c2514be606d9a6832e3eddf7258f
idbeta <idbeta@gmail.com>
2020-10-14 13:58:49 +0800

    modified code conforms to pep8 specification remove useless code

--- t/specialtest/cases/test_fuzziing_uri_of_route.py
+++ t/specialtest/cases/test_fuzziing_uri_of_route.py
@@ -15,37 +15,46 @@
 # limitations under the License.
 #
 
-#!/usr/bin/env python
-#-*- coding: utf-8 -*-
-import sys, os, time, json, subprocess, signal
-import random, string, urllib, re
-import requests, psutil, grequests
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import os
+import time
+import json
+import subprocess
+import signal
+import random
+import string
+import urllib
+import re
+import requests
+import psutil
+import grequests
 
-def kill_processtree(id):
-    parent = psutil.Process(pid)
-    children = parent.children(recursive=True)
-    for p in children:
-        psutil.Process(p.pid).terminate()
-    psutil.Process(id).terminate()
 
 def get_pid_byname():
     name = "apisix"
-    cmd = "ps -ef | grep %s/conf | grep master | grep -v grep| awk '{print $2}'"%name
-    p = subprocess.Popen(cmd, stderr = subprocess.PIPE, stdout = subprocess.PIPE, shell = True)
+    cmd = "ps -ef | grep %s/conf | grep master | grep -v grep| awk \
+    '{print $2}'" % name
+    p = subprocess.Popen(cmd, stderr=subprocess.PIPE,
+                         stdout=subprocess.PIPE, shell=True)
     p.wait()
     return p.stdout.read().strip()
+
 
 def get_workerres(pid):
     parent = psutil.Process(pid)
     children = parent.children(recursive=True)
     for p in children:
         cp = psutil.Process(p.pid)
-        print(p.pid,cp.cpu_percent(interval=1.0),cp.memory_percent())
+        print(p.pid, cp.cpu_percent(interval=1.0), cp.memory_percent())
+
 
 def cur_file_dir():
     return os.path.split(os.path.realpath(__file__))[0]
 
-def requesttest(url,times):
+
+def requesttest(url, times):
     start = time.time()
     tasks = []
     r = []
@@ -55,20 +64,21 @@ def requesttest(url,times):
         r.extend([i.status_code for i in res])
     return r
 
+
 def setup_module():
-    global headers,apisixhost,apisixpid,apisixpath
+    global headers, apisixhost, apisixpid, apisixpath
     apisixpid = int(get_pid_byname())
     apisixpath = psutil.Process(apisixpid).cwd()
     os.chdir(apisixpath)
-    subprocess.call("./bin/apisix stop",shell=True, stdout=subprocess.PIPE)
+    subprocess.call("./bin/apisix stop", shell=True, stdout=subprocess.PIPE)
     time.sleep(1)
-    subprocess.Popen("> logs/error.log",shell = True, stdout = subprocess.PIPE)
-    subprocess.call("etcd &",shell=True, stdout=subprocess.PIPE)
+    subprocess.Popen("> logs/error.log", shell=True, stdout=subprocess.PIPE)
+    subprocess.call("etcd &", shell=True, stdout=subprocess.PIPE)
     time.sleep(5)
-    subprocess.call("./bin/apisix start",shell=True, stdout=subprocess.PIPE)
+    subprocess.call("./bin/apisix start", shell=True, stdout=subprocess.PIPE)
     time.sleep(1)
     apisixpid = int(get_pid_byname())
-    print("=============APISIX's pid:",apisixpid)
+    print("=============APISIX's pid:", apisixpid)
     apisixhost = "http://127.0.0.1:9080"
     headers = {"X-API-KEY": "edd1c9f034335f136f87ad84b625c8f1"}
     confpath = "./t/specialtest/cases/nginx.conf"
@@ -76,48 +86,57 @@ def setup_module():
         os.makedirs("./t/specialtest/cases/logs")
     except Exception as e:
         pass
-    p = subprocess.Popen(['openresty', '-p', apisixpath ,'-c',confpath],
-    stderr = subprocess.PIPE, stdout = subprocess.PIPE, shell = False)
+    p = subprocess.Popen(['openresty', '-p', apisixpath, '-c', confpath],
+                         stderr=subprocess.PIPE, stdout=subprocess.PIPE,
+                         shell=False)
     p.wait()
+
 
 def teardown_module():
     pass
 
+
 def test_fuzzing_uri_of_route():
     print("====APISIX's resource occupation(before test):")
     get_workerres(apisixpid)
-    #use environment variables "FUZZING_URI" you can setting the numbers of test uris
-    fuzzing_uri_nums = 1000 if not os.getenv('FUZZING_URI') else os.getenv('FUZZING_URI')
-    #not spupport * :
-    orgin_char = '''ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789/-_~!();@&=+$,?#'.[]'''
+    # use environment variables "FUZZING_URI"
+    # you can setting the numbers of test uris
+    fuzzing_uri_nums = 1000 if not os.getenv('FUZZING_URI')\
+        else os.getenv('FUZZING_URI')
+    # not spupport * :
+    orgin_char = '''ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz
+                 0123456789/-_~!();@&=+$,?#'.[]'''
     for i in range(int(fuzzing_uri_nums)):
         length = random.randint(1, 4080)
-        tmpuri = "".join(random.sample(list(orgin_char)*(length//len(orgin_char) + 1),length))
+        tmpuri = "".join(random.sample(list(orgin_char) *
+                         (length//len(orgin_char) + 1), length))
         tmpuri = re.sub(r"/(\.+)/", "", tmpuri)
         tmpuri = re.sub(r"/+", "/", tmpuri)
-        uri = "/hello%s"%tmpuri
-        assert len("/hello%s"%uri)<=4096
+        uri = "/hello%s" % tmpuri
+        assert len("/hello%s" % uri) <= 4096
         cfgdata = {
-        "uri": uri,
-        "upstream": {
-            "type": "roundrobin",
-            "nodes": {
-                "127.0.0.1:9666": 1
-            }
-        }
-    }
+                    "uri": uri,
+                    "upstream": {
+                        "type": "roundrobin",
+                        "nodes": {
+                            "127.0.0.1:9666": 1
+                        }
+                    }
+                }
 
-        r = requests.put("%s/apisix/admin/routes/1"%apisixhost, json=cfgdata,headers=headers )
+        r = requests.put("%s/apisix/admin/routes/1" % apisixhost,
+                         json=cfgdata, headers=headers)
         assert str(r.status_code).startswith("2")
         time.sleep(0.2)
-        #verify route
+        # verify route
         uri = urllib.quote(uri)
-        r = requests.get("%s%s"%(apisixhost,uri))
-        if r.status_code != 200 or "Hello, World!" not in r.content :
-            print(uri,r.status_code, r.content)
+        r = requests.get("%s%s" % (apisixhost, uri))
+        if r.status_code != 200 or "Hello, World!" not in r.content:
+            print(uri, r.status_code, r.content)
             raise AssertionError('assertError')
 
-    print("====APISIX's resource occupation(after set route and request test):")
+    print("====APISIX's resource occupation\
+        (after set route and request test):")
     get_workerres(apisixpid)
     print("====APISIX's error log:")
     with open(apisixpath+r"/logs/error.log") as fh:
