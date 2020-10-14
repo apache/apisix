09e67e89c59992d1419628008043d4ac3cb68b44
idbeta <idbeta@gmail.com>
2020-10-14 14:50:50 +0800

    Modify the code to comply with pep8

--- t/specialtest/test_runtest.py
+++ t/specialtest/test_runtest.py
@@ -15,43 +15,53 @@
 # limitations under the License.
 #
 
-#!/usr/bin/env python
-#-*- coding: utf-8 -*-
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-import random,subprocess,shutil
-import time, datetime
-import sys,os
+import random
+import subprocess
+import shutil
+import time
+import datetime
+import sys
+import os
 import json
 import re
 import base64
 from sys import version_info
 
+
 def cur_file_dir():
     return os.path.split(os.path.realpath(__file__))[0]
+
 
 def env_prepare(casedirpath):
     updatepip = "curl https://bootstrap.pypa.io/get-pip.py | python"
     if version_info.major == 2:
-        requirements = "python -m pip install -r %s/requirements.txt"%cur_file_dir()
+        requirements = "python -m pip install -r %s/requirements.txt" %\
+                     cur_file_dir()
     else:
-        requirements = "python3 -m pip install -r %s/requirements.txt"%cur_file_dir()
-    r_exc_case_cmd = subprocess.Popen(updatepip, stderr=subprocess.PIPE,shell=True)
+        requirements = "python3 -m pip install -r %s/requirements.txt" %\
+                     cur_file_dir()
+    r_exc_case_cmd = subprocess.Popen(updatepip, stderr=subprocess.PIPE,
+                                      shell=True)
     r_exc_case_cmd.wait()
     err = r_exc_case_cmd.stderr.read()
 
-    r_exc_case_cmd = subprocess.Popen(requirements, stderr=subprocess.PIPE,shell=True)
+    r_exc_case_cmd = subprocess.Popen(requirements, stderr=subprocess.PIPE,
+                                      shell=True)
     r_exc_case_cmd.wait()
     err = r_exc_case_cmd.stderr.read()
 
-    #exc_case_cmd='pytest --force-flaky --max-runs=3 --no-flaky-report -q "%s" --html="%s/result.html" --self-contained-html > "%s/result.log"'%(casedirpath,casedirpath,casedirpath)
-    #exc_case_cmd2='pytest --force-flaky --max-runs=3 --no-flaky-report -v -s "%s" > "%s/result.log"'%(casedirpath,casedirpath)
-    #exc_case_cmd3='pytest --force-flaky --max-runs=3 --no-flaky-report -v -s "%s" '%(casedirpath)
-    #r_exc_case_cmd = subprocess.Popen(exc_case_cmd3, stderr=subprocess.PIPE,shell=True)
-    #r_exc_case_cmd.wait()
-    #err = r_exc_case_cmd.stderr.read()
+    # exc_case_cmd3='pytest --force-flaky --max-runs=3
+    # --no-flaky-report -v -s "%s" '%(casedirpath)
+    # r_exc_case_cmd = subprocess.Popen(exc_case_cmd3,
+    # stderr=subprocess.PIPE,shell=True)
+    # r_exc_case_cmd.wait()
+    # err = r_exc_case_cmd.stderr.read()
 
-    #shutil.rmtree(cur_file_dir()+r'/.pytest_cache')
-    #shutil.rmtree(casedirpath+r'/__pycache__')
+    # shutil.rmtree(cur_file_dir()+r'/.pytest_cache')
+    # shutil.rmtree(casedirpath+r'/__pycache__')
 
 casepath = cur_file_dir()+"/cases"
 env_prepare(casepath)
