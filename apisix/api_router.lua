apache/apisix

commit dab3d77f99d48f51d94fca131a509831d16f2e90

feat: use interceptors to protect plugin's route (#2416)

apisix/api_router.lua
@@ -16,13 +16,57 @@
 --
 local require = require
 local router = require("resty.radixtree")
-local plugin = require("apisix.plugin")
+local plugin_mod = require("apisix.plugin")
+local ip_restriction = require("apisix.plugins.ip-restriction")
 local core = require("apisix.core")
 local ipairs = ipairs
 
 
 local _M = {}
 local match_opts = {}
+local interceptors = {
+    ["ip-restriction"] = {
+        run = function (conf, ctx)
+            return ip_restriction.access(conf, ctx)
+        end,
+        schema = ip_restriction.schema,
+    }
+}
+
+
+_M.interceptors_schema = {
+    type = "array",
+    items = {
+        type = "object",
+        minItems = 1,
+        properties = {
+            name = {
+                type = "string",
+                enum = {"ip-restriction"},
+            },
+            conf = {
+                type = "object",
+            }
+        },
+        required = {"name", "conf"},
+        dependencies = {
+            name = {
+                oneOf = {}
+            }
+        }
+    }
+}
+for name, attrs in pairs(interceptors) do
+    core.table.insert(_M.interceptors_schema.items.properties.name.enum, name)
+    core.table.insert(_M.interceptors_schema.items.dependencies.name.oneOf, {
+        properties = {
+            name = {
+                enum = {name},
+            },
+            conf = attrs.schema,
+        }
+    })
+end
 
 
 local fetch_api_router
@@ -31,9 +75,10 @@ do
 function fetch_api_router()
     core.table.clear(routes)
 
-    for _, plugin in ipairs(plugin.plugins) do
+    for _, plugin in ipairs(plugin_mod.plugins) do
         local api_fun = plugin.api
         if api_fun then
+            local name = plugin.name
             local api_routes = api_fun()
             core.log.debug("fetched api routes: ",
                            core.json.delay_encode(api_routes, true))
@@ -41,8 +86,25 @@ function fetch_api_router()
                 core.table.insert(routes, {
                         methods = route.methods,
                         paths = route.uri,
-                        handler = function (...)
-                            local code, body = route.handler(...)
+                        handler = function (api_ctx)
+                            local code, body
+
+                            local metadata = plugin_mod.plugin_metadata(name)
+                            if metadata and metadata.interceptors then
+                                for _, rule in ipairs(metadata.interceptors) do
+                                    local f = interceptors[rule.name]
+                                    if f == nil then
+                                        core.log.error("unknown interceptor: ", rule.name)
+                                    else
+                                        code, body = f.run(rule.conf, api_ctx)
+                                        if code or body then
+                                            return core.response.exit(code, body)
+                                        end
+                                    end
+                                end
+                            end
+
+                            code, body = route.handler(api_ctx)
                             if code or body then
                                 core.response.exit(code, body)
                             end
@@ -59,7 +121,7 @@ end -- do
 
 
 function _M.match(api_ctx)
-    local api_router = core.lrucache.global("api_router", plugin.load_times, fetch_api_router)
+    local api_router = core.lrucache.global("api_router", plugin_mod.load_times, fetch_api_router)
     if not api_router then
         core.log.error("failed to fetch valid api router")
         return false

t/admin/plugin-metadata.t
@@ -237,37 +237,7 @@ GET /t
 
 
 
-=== TEST 8: no plugin metadata schema
---- config
-    location /t {
-        content_by_lua_block {
-            local t = require("lib.test_admin").test
-            local code, body = t('/apisix/admin/plugin_metadata/echo',
-                 ngx.HTTP_PUT,
-                 [[{"k": "v"}]],
-                [[{
-                    "node": {
-                        "value": "sdf"
-                    },
-                    "action": "set"
-                }]]
-                )
-
-            ngx.status = code
-            ngx.print(body)
-        }
-    }
---- request
-GET /t
---- error_code: 400
---- response_body
-{"error_msg":"no metadata schema for plugin echo"}
---- no_error_log
-[error]
-
-
-
-=== TEST 9: verify metadata schema fail
+=== TEST 8: verify metadata schema fail
 --- config
     location /t {
         content_by_lua_block {
@@ -297,5 +267,187 @@ GET /t
 --- error_code: 400
 --- response_body eval
 qr/\{"error_msg":"invalid configuration: property \\"ikey\\" is required"\}/
+--- no_error_log
+[error]
+
+
+
+=== TEST 9: set plugin interceptors
+--- config
+    location /t {
+        content_by_lua_block {
+            local t = require("lib.test_admin").test
+            local code, body = t('/apisix/admin/plugin_metadata/prometheus',
+                ngx.HTTP_PUT,
+                [[{
+                    "interceptors": [
+                        {
+                            "name": "ip-restriction",
+                            "conf": {
+                                "whitelist": ["192.168.1.0/24"]
+                            }
+                        }
+                    ]
+                }]]
+                )
+
+            if code >= 300 then
+                ngx.status = code
+            end
+            ngx.say(body)
+        }
+    }
+--- request
+GET /t
+--- response_body
+passed
+--- no_error_log
+[error]
+
+
+
+=== TEST 10: hit prometheus route
+--- request
+GET /apisix/prometheus/metrics
+-- error_code: 403
+
+
+
+=== TEST 11: set plugin interceptors (allow ip access)
+--- config
+    location /t {
+        content_by_lua_block {
+            local t = require("lib.test_admin").test
+            local code, body = t('/apisix/admin/plugin_metadata/prometheus',
+                ngx.HTTP_PUT,
+                [[{
+                    "interceptors": [
+                        {
+                            "name": "ip-restriction",
+                            "conf": {
+                                "whitelist": ["127.0.0.1"]
+                            }
+                        }
+                    ]
+                }]]
+                )
+
+            if code >= 300 then
+                ngx.status = code
+            end
+            ngx.say(body)
+        }
+    }
+--- request
+GET /t
+--- response_body
+passed
+--- no_error_log
+[error]
+
+
+
+=== TEST 12: hit prometheus route again
+--- request
+GET /apisix/prometheus/metrics
+-- error_code: 200
+
+
+
+=== TEST 13: invalid interceptors configure (unknown interceptor)
+--- config
+    location /t {
+        content_by_lua_block {
+            local t = require("lib.test_admin").test
+            local code, body = t('/apisix/admin/plugin_metadata/prometheus',
+                ngx.HTTP_PUT,
+                [[{
+                    "interceptors": [
+                        {
+                            "name": "unknown",
+                            "conf": {
+                                "whitelist": ["127.0.0.1"]
+                            }
+                        }
+                    ]
+                }]]
+                )
+
+            if code >= 300 then
+                ngx.status = code
+            end
+            ngx.say(body)
+        }
+    }
+--- request
+GET /t
+--- response_body eval
+qr/\{"error_msg":"invalid configuration: property \\"interceptors\\" validation failed: failed to validate item 1: property \\"name\\" validation failed: matches non of the enum values"\}/
+--- error_code: 400
+--- no_error_log
+[error]
+
+
+
+=== TEST 14: invalid interceptors configure (missing conf)
+--- config
+    location /t {
+        content_by_lua_block {
+            local t = require("lib.test_admin").test
+            local code, body = t('/apisix/admin/plugin_metadata/prometheus',
+                ngx.HTTP_PUT,
+                [[{
+                    "interceptors": [
+                        {
+                            "name": "ip-restriction"
+                        }
+                    ]
+                }]]
+                )
+
+            if code >= 300 then
+                ngx.status = code
+            end
+            ngx.say(body)
+        }
+    }
+--- request
+GET /t
+--- error_code: 400
+--- response_body eval
+qr/\{"error_msg":"invalid configuration: property \\"interceptors\\" validation failed: failed to validate item 1: property \\"conf\\" is required"\}/
+--- no_error_log
+[error]
+
+
+
+=== TEST 15: invalid interceptors configure (invalid interceptor configure)
+--- config
+    location /t {
+        content_by_lua_block {
+            local t = require("lib.test_admin").test
+            local code, body = t('/apisix/admin/plugin_metadata/prometheus',
+                ngx.HTTP_PUT,
+                [[{
+                    "interceptors": [
+                        {
+                            "name": "ip-restriction",
+                            "conf": {"aa": "b"}
+                        }
+                    ]
+                }]]
+                )
+
+            if code >= 300 then
+                ngx.status = code
+            end
+            ngx.say(body)
+        }
+    }
+--- request
+GET /t
+--- error_code: 400
+--- response_body eval
+qr/\{"error_msg":"invalid configuration: property \\"interceptors\\" validation failed: failed to validate item 1: failed to validate dependent schema for \\"name\\": value should match only one schema, but matches none"\}/
 --- no_error_log
 [error]

apisix/admin/plugin_metadata.lua
@@ -14,9 +14,11 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 --
+local error   = error
 local pcall   = pcall
 local require = require
 local core    = require("apisix.core")
+local api_router = require("apisix.api_router")
 
 local _M = {
 }
@@ -44,14 +46,25 @@ local function check_conf(plugin_name, conf)
         return nil, {error_msg = "invalid plugin name"}
     end
 
-    local schema = plugin_object.metadata_schema
-    if not schema then
-        return nil, {error_msg = "no metadata schema for plugin " .. plugin_name}
-    end
-
     if not conf then
         return nil, {error_msg = "missing configurations"}
     end
+
+    local schema = plugin_object.metadata_schema or {
+        type = "object",
+        properties = {},
+    }
+    if not schema.properties then
+        schema.properties = {
+            additionalProperties = false,
+        }
+    end
+
+    -- inject interceptors schema to each plugins
+    if schema.properties.interceptors then
+        error("'interceptors' can not be used as the name of metadata schema's field")
+    end
+    schema.properties.interceptors = api_router.interceptors_schema
 
     core.log.info("schema: ", core.json.delay_encode(schema))
     core.log.info("conf  : ", core.json.delay_encode(conf))

commit 74aee2989e6eab6b18e34c009fcd40821e1bbee2

fix: we didn't traverse the plugin interceptors correctly (#2462)
This bug is introduced in dab3d77.

apisix/api_router.lua
@@ -90,8 +90,8 @@ function fetch_api_router()
                             local code, body
 
                             local metadata = plugin_mod.plugin_metadata(name)
-                            if metadata and metadata.interceptors then
-                                for _, rule in ipairs(metadata.interceptors) do
+                            if metadata and metadata.value.interceptors then
+                                for _, rule in ipairs(metadata.value.interceptors) do
                                     local f = interceptors[rule.name]
                                     if f == nil then
                                         core.log.error("unknown interceptor: ", rule.name)

t/admin/plugin-metadata.t
@@ -309,7 +309,7 @@ passed
 === TEST 10: hit prometheus route
 --- request
 GET /apisix/prometheus/metrics
--- error_code: 403
+--- error_code: 403
 
 
 
@@ -350,7 +350,7 @@ passed
 === TEST 12: hit prometheus route again
 --- request
 GET /apisix/prometheus/metrics
--- error_code: 200
+--- error_code: 200
 
 
 