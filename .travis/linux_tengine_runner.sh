930124cddceec2715cf618dfd1f19c53a7a0bfac
EnableAsync <sunjiajunboy@gmail.com>
2020-10-24 17:05:30 +0800

    bugfix: fix whitespace

--- .travis/linux_tengine_runner.sh
+++ .travis/linux_tengine_runner.sh
@@ -37,7 +37,6 @@ create_lua_deps() {
 before_install() {
     sudo cpanm --notest Test::Nginx >build.log 2>&1 || (cat build.log && exit 1)
     docker pull redis:3.0-alpine
-
     docker run --rm -itd -p 6379:6379 --name apisix_redis redis:3.0-alpine
     docker run --rm -itd -e HTTP_PORT=8888 -e HTTPS_PORT=9999 -p 8888:8888 -p 9999:9999 mendhak/http-https-echo
     # Runs Keycloak version 10.0.2 with inbuilt policies for unit tests
