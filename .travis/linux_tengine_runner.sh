e512fa5a11ab7dbacee1d23b55623fbff2d94d3f
EnableAsync <sunjiajunboy@gmail.com>
2020-10-26 09:57:31 +0800

    fix: code format

--- .travis/linux_tengine_runner.sh
+++ .travis/linux_tengine_runner.sh
@@ -259,7 +259,6 @@ do_install() {
 
     sudo luarocks install luacheck > build.log 2>&1 || (cat build.log && exit 1)
 
-    # ./utils/install-etcd.sh
     ./utils/install-etcd-client.sh
 
     git clone https://github.com/iresty/test-nginx.git test-nginx
