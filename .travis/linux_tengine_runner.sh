c5a5487ad0b8bed704ab6fd3dfaba5c64d6f2169
EnableAsync <43645467+EnableAsync@users.noreply.github.com>
2020-10-28 23:41:14 +0800

    CI: move etcd cluster in docker (#2225)

--- .travis/linux_tengine_runner.sh
+++ .travis/linux_tengine_runner.sh
@@ -259,7 +259,7 @@ do_install() {
 
     sudo luarocks install luacheck > build.log 2>&1 || (cat build.log && exit 1)
 
-    ./utils/install-etcd.sh
+    ./utils/linux-install-etcd-client.sh
 
     git clone https://github.com/iresty/test-nginx.git test-nginx
     make utils
@@ -280,11 +280,7 @@ script() {
     export_or_prefix
     export PATH=$OPENRESTY_PREFIX/nginx/sbin:$OPENRESTY_PREFIX/luajit/bin:$OPENRESTY_PREFIX/bin:$PATH
     openresty -V
-    sudo service etcd stop
-    mkdir -p ~/etcd-data
-    etcd --listen-client-urls 'http://0.0.0.0:2379' --advertise-client-urls='http://0.0.0.0:2379' --data-dir ~/etcd-data > /dev/null 2>&1 &
-    etcdctl version
-    sleep 5
+
 
     ./build-cache/grpc_server_example &
 
