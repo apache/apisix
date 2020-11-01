7b6ca805449ac162311f17c4376c9364b096a9e2
YuanSheng Wang <membphis@gmail.com>
2020-11-01 14:54:24 +0800

    bug(CLI): CI failed, failed to load `apisix.cli.ngx_tpl` (#2588)
    * for test.
    
    * CLI: load the Lua module after updated the `package.path`.
    
    * CI: used patch to make the CI run normal.
    
    * bugfix: add sudo
    
    * chore:  print the location of `apisix`.
    
    * reverted not related change.

--- .travis/linux_apisix_master_luarocks_runner.sh
+++ .travis/linux_apisix_master_luarocks_runner.sh
@@ -59,6 +59,11 @@ script() {
     sudo mkdir -p /usr/local/apisix/deps
     sudo PATH=$PATH ./utils/install-apisix.sh install > build.log 2>&1 || (cat build.log && exit 1)
 
+    which apisix
+    # TODO: DELETE ME AFTER https://github.com/apache/apisix/pull/2588 merged
+    sudo cp ../bin/apisix /usr/bin/apisix
+
+    # run test
     sudo PATH=$PATH apisix help
     sudo PATH=$PATH apisix init
     sudo PATH=$PATH apisix start
@@ -68,6 +73,9 @@ script() {
 
     # install APISIX by luarocks
     sudo luarocks install $APISIX_MAIN > build.log 2>&1 || (cat build.log && exit 1)
+
+    # TODO: DELETE ME AFTER https://github.com/apache/apisix/pull/2588 merged
+    sudo cp ../bin/apisix /usr/bin/apisix
 
     # show install files
     luarocks show apisix
