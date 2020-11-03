0952b84fd7d007d8d0c7bd153e9355ef64857e6a
Alex Zhang <zchao1995@gmail.com>
2020-11-03 13:53:39 +0800

    feat: support TLS connection with etcd. (#2548)
    Support the TLS connection when communicating with etcd cluster. We added a configuration item to custom the certificate verification. Whether to setup TLS connection or not depends on the endpoints' scheme, for instance, when endpoints are:
    
    ```
    etcd:
      host:
        - "https://127.0.0.1:2379"
        - "https://127.0.0.1:3379"
    ```
    
    APISIX will originate TLS connection automatically, and the Server Name Indication extention will be set by the endpoint host (`127.0.0.1` in above case). Note by default APISIX will verify the certificate, close the verification in configuration explicitly if you want to bypass it.
    
    ```
    etcd:
      tls:
        verfiy: false
    ```

--- apisix/core/config_etcd.lua
+++ apisix/core/config_etcd.lua
@@ -519,6 +519,12 @@ function _M.new(key, opts)
     etcd_conf.prefix = nil
     etcd_conf.protocol = "v3"
     etcd_conf.api_prefix = "/v3"
+    etcd_conf.ssl_verify = true
+
+    -- default to verify etcd cluster certificate
+    if etcd_conf.tls and etcd_conf.tls.verify == false then
+        etcd_conf.ssl_verify = false
+    end
 
     local automatic = opts and opts.automatic
     local item_schema = opts and opts.item_schema
