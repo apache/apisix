366d5040cf10de31961bf5146dec626607d3bbfd
罗泽轩 <spacewanderlzx@gmail.com>
2020-11-04 15:45:27 +0800

    test: make CI pass again (#2619)

--- apisix/cli/env.lua
+++ apisix/cli/env.lua
@@ -15,6 +15,7 @@
 -- limitations under the License.
 --
 
+local require = require
 local util = require("apisix.cli.util")
 
 local pcall = pcall
@@ -22,61 +23,59 @@ local error = error
 local exit = os.exit
 local stderr = io.stderr
 local str_find = string.find
-local pkg_cpath_org = package.cpath
-local pkg_path_org = package.path
+local arg = arg
+local package = package
 
-local min_etcd_version = "3.4.0"
-local apisix_home = "/usr/local/apisix"
-local pkg_cpath = apisix_home .. "/deps/lib64/lua/5.1/?.so;"
-                  .. apisix_home .. "/deps/lib/lua/5.1/?.so;;"
-local pkg_path = apisix_home .. "/deps/share/lua/5.1/?.lua;;"
 
--- only for developer, use current folder as working space
-local is_root_path = false
-local script_path = arg[0]
-if script_path:sub(1, 2) == './' then
-    apisix_home = util.trim(util.execute_cmd("pwd"))
-    if not apisix_home then
-        error("failed to fetch current path")
+return function (apisix_home, pkg_cpath_org, pkg_path_org)
+    -- only for developer, use current folder as working space
+    local is_root_path = false
+    local script_path = arg[0]
+    if script_path:sub(1, 2) == './' then
+        apisix_home = util.trim(util.execute_cmd("pwd"))
+        if not apisix_home then
+            error("failed to fetch current path")
+        end
+
+        if str_find(apisix_home .. "/", '/root/', nil, true) == 1 then
+            is_root_path = true
+        end
+
+        local pkg_cpath = apisix_home .. "/deps/lib64/lua/5.1/?.so;"
+                          .. apisix_home .. "/deps/lib/lua/5.1/?.so;"
+
+        local pkg_path = apisix_home .. "/?/init.lua;"
+                         .. apisix_home .. "/deps/share/lua/5.1/?.lua;;"
+
+        package.cpath = pkg_cpath .. package.cpath
+        package.path  = pkg_path .. package.path
     end
 
-    if str_find(apisix_home .. "/", '/root/', nil, true) == 1 then
-        is_root_path = true
-    end
-
-    pkg_cpath = apisix_home .. "/deps/lib64/lua/5.1/?.so;"
-                .. apisix_home .. "/deps/lib/lua/5.1/?.so;"
-
-    pkg_path = apisix_home .. "/?/init.lua;"
-               .. apisix_home .. "/deps/share/lua/5.1/?.lua;;"
-end
-
-package.cpath = pkg_cpath .. pkg_cpath_org
-package.path  = pkg_path .. pkg_path_org
-
-do
-    -- skip luajit environment
-    local ok = pcall(require, "table.new")
-    if not ok then
-        local ok, json = pcall(require, "cjson")
-        if ok and json then
-            stderr:write("please remove the cjson library in Lua, it may "
-                         .. "conflict with the cjson library in openresty. "
-                         .. "\n luarocks remove cjson\n")
-            exit(1)
+    do
+        -- skip luajit environment
+        local ok = pcall(require, "table.new")
+        if not ok then
+            local ok, json = pcall(require, "cjson")
+            if ok and json then
+                stderr:write("please remove the cjson library in Lua, it may "
+                            .. "conflict with the cjson library in openresty. "
+                            .. "\n luarocks remove cjson\n")
+                exit(1)
+            end
         end
     end
+
+    local openresty_args = [[openresty -p ]] .. apisix_home .. [[ -c ]]
+                           .. apisix_home .. [[/conf/nginx.conf]]
+
+    local min_etcd_version = "3.4.0"
+
+    return {
+        apisix_home = apisix_home,
+        is_root_path = is_root_path,
+        openresty_args = openresty_args,
+        pkg_cpath_org = pkg_cpath_org,
+        pkg_path_org = pkg_path_org,
+        min_etcd_version = min_etcd_version,
+    }
 end
-
-local openresty_args = [[openresty -p ]] .. apisix_home .. [[ -c ]]
-                       .. apisix_home .. [[/conf/nginx.conf]]
-
-
-return {
-    apisix_home = apisix_home,
-    is_root_path = is_root_path,
-    openresty_args = openresty_args,
-    pkg_cpath_org = pkg_cpath_org,
-    pkg_path_org = pkg_path_org,
-    min_etcd_version = min_etcd_version,
-}
