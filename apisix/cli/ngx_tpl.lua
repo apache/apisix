e3d34b86a596cfc0d63fb1cff5eb746006a40e48
HelloBug0 <73410835+HelloBug0@users.noreply.github.com>
2020-11-19 09:03:05 +0800

    feat: enable access log or not
    1. add the feature that it works as same as before even through this configuration is absent

--- apisix/cli/ngx_tpl.lua
+++ apisix/cli/ngx_tpl.lua
@@ -171,12 +171,12 @@ http {
     lua_regex_match_limit 100000;
     lua_regex_cache_max_entries 8192;
 
-    {% if http.enable_access_log then %}
+    {% if http.enable_access_log == false then %}
+    access_log off;
+    {% else %}
     log_format main escape={* http.access_log_format_escape *} '{* http.access_log_format *}';
 
     access_log {* http.access_log *} main buffer=16384 flush=3;
-    {% else %}
-    access_log off;
     {% end %}
     open_file_cache  max=1000 inactive=60;
     client_max_body_size {* http.client_max_body_size *};
