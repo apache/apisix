5eab5fdff8cd93bd667fd4a3866d7bc9afadf86d
spacewander <spacewanderlzx@gmail.com>
2020-12-02 11:05:40 +0800

    fix

--- apisix/plugins/request-id.lua
+++ apisix/plugins/request-id.lua
@@ -61,7 +61,7 @@ function _M.header_filter(conf, ctx)
 
     local headers = ngx.resp.get_headers()
     if not headers[conf.header_name] then
-        core.response.set_header(ctx, conf.header_name, ctx.x_request_id)
+        core.response.set_header(conf.header_name, ctx.x_request_id)
     end
 end
 
