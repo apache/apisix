b9ac92feb98aa07547fb72d4f447ac1a24f6735b
yiyiyimu <wosoyoung@gmail.com>
2020-12-10 20:07:00 +0800

    fix CI
    Signed-off-by: yiyiyimu <wosoyoung@gmail.com>

--- .travis/apisix_cli_test.sh
+++ .travis/apisix_cli_test.sh
@@ -267,6 +267,9 @@ git checkout conf/config.yaml
 
 echo "
 apisix:
+    admin_api_mtls:
+        admin_ssl_cert: '../t/certs/apisix_admin_ssl.crt'
+        admin_ssl_cert_key: '../t/certs/apisix_admin_ssl.key'
     port_admin: 9180
     https_admin: true
 " > conf/config.yaml
