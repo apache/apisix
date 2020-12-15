34bcac8bf7a188edbfa9262626c4ec5347e5c495
John Bampton <jbampton@gmail.com>
2020-12-15 12:03:24 +1000

    feat(CI): add a GitHub Action to lint the YAML files
    Lint some YAML
    Add a `.yamllint` config file

--- t/APISIX.pm
+++ t/APISIX.pm
@@ -72,7 +72,7 @@ if ($enable_local_dns) {
 
 my $default_yaml_config = read_file("conf/config-default.yaml");
 # enable example-plugin as some tests require it
-$default_yaml_config =~ s/#- example-plugin/- example-plugin/;
+$default_yaml_config =~ s/# - example-plugin/- example-plugin/;
 
 my $user_yaml_config = read_file("conf/config.yaml");
 my $ssl_crt = read_file("t/certs/apisix.crt");
