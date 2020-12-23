6c16ec6a2c0e521e7a9ac5df8b3dc09e4f0778cd
罗泽轩 <spacewanderlzx@gmail.com>
2020-12-23 16:40:19 +0800

    feat: expose schema via control API (#3092)
    Close #3022

--- apisix/control/v1.lua
+++ apisix/control/v1.lua
@@ -14,11 +14,36 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 --
+local core = require("apisix.core")
+local plugin = require("apisix.plugin")
+
+
 local _M = {}
 
 
 function _M.schema()
-    return 200, {}
+    local schema = {
+        main = {
+            consumer = core.schema.consumer,
+            global_rule = core.schema.global_rule,
+            plugins = core.schema.plugins,
+            proto = core.schema.proto,
+            route = core.schema.route,
+            service = core.schema.service,
+            ssl = core.schema.ssl,
+            stream_route = core.schema.stream_route,
+            upstream = core.schema.upstream,
+        },
+        plugins = plugin.get_all({
+            version = true,
+            priority = true,
+            schema = true,
+            metadata_schema = true,
+            consumer_schema = true,
+            type = true,
+        }),
+    }
+    return 200, schema
 end
 
 
