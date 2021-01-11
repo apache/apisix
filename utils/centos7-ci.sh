31b2c0cf1fd6630b92335518c56d38a5c2655c2c
idbeta <idbeta@gmail.com>
2021-01-11 16:13:26 +0800

    fixed typo

--- utils/centos7-ci.sh
+++ utils/centos7-ci.sh
@@ -36,7 +36,7 @@ install_dependencies() {
     yum install -y yum-utils && yum-config-manager --add-repo https://openresty.org/package/centos/openresty.repo
     yum install -y openresty-debug
 
-    # install etcd
+    # install etcdctl
     wget https://github.com/etcd-io/etcd/releases/download/v3.4.0/etcd-v3.4.0-linux-amd64.tar.gz
     tar xf etcd-v3.4.0-linux-amd64.tar.gz
     cp /etcd-v3.4.0-linux-amd64/etcdctl /usr/local/bin/
