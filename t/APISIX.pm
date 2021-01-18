d97f283b295c7413f7416a9390c60e6d64c72d80
Jens Keiner <jens@nfft.org>
2021-01-18 16:00:34 +0100

    Add shared dictionary for discovery documents.

--- t/APISIX.pm
+++ t/APISIX.pm
@@ -252,6 +252,7 @@ _EOC_
     lua_shared_dict plugin-limit-count-redis-cluster-slot-lock 1m;
     lua_shared_dict tracing_buffer       10m;    # plugin skywalking
     lua_shared_dict access_tokens         1m; # plugin authz-keycloak
+    lua_shared_dict discovery             1m; # plugin authz-keycloak
     lua_shared_dict plugin-api-breaker   10m;
     lua_capture_error_log                 1m;    # plugin error-log-logger
 
