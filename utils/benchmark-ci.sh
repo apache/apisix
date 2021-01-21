1db1d47ae693e41a1079fda07694c12089e8cfcf
idbeta <idbeta@gmail.com>
2021-01-21 11:53:29 +0800

    CI: automatically check performance

---
+++ utils/benchmark-ci.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#!/bin/bash
+
+set -ex
+
+install_dependencies() {
+    # add OpenResty source
+    wget -qO - https://openresty.org/package/pubkey.gpg | apt-key add -
+    apt-get update
+    apt-get -y install software-properties-common
+    add-apt-repository -y "deb http://openresty.org/package/ubuntu $(lsb_release -sc) main"
+    apt-get update
+
+    # install OpenResty and some compilation tools
+    apt-get install -y git openresty curl luarocks
+
+    # install wrk
+    apt-get install build-essential libssl-dev git -y
+    git clone https://github.com/wg/wrk.git wrk
+    cd wrk
+    make
+    cp wrk /usr/local/bin
+}
+
+run_apisix() {
+    export PATH=/usr/local/openresty/nginx/sbin:/usr/local/openresty/bin:$PATH
+    make deps
+    make init
+    make run
+
+    # create route
+    curl -i http://127.0.0.1:9080/apisix/admin/routes/1 \
+    -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' \
+    -X PUT -d ' 
+    { 
+    "uri": "/index.html", 
+    "upstream": { 
+        "type": "roundrobin", 
+        "nodes": { 
+            "127.0.0.1:80": 1 
+        } 
+    } 
+    }'
+}
+
+
+case_opt=$1
+case $case_opt in
+    (install_dependencies)
+        install_dependencies
+        ;;
+    (run_apisix)
+        run_apisix
+        ;;
+esac
