67a7fe7dbc3c8495a5f6dd12078dca21bc5ea3c6
Jens Keiner <jens@nfft.org>
2021-01-21 13:07:53 +0100

    Fix stray conf.http_request_decorator.

--- apisix/plugins/authz-keycloak.lua
+++ apisix/plugins/authz-keycloak.lua
@@ -459,7 +459,7 @@ local function authz_keycloak_ensure_sa_access_token(conf)
 
         local current_time = ngx.time()
 
-        local res, err = httpc:request_uri(token_endpoint, conf.http_request_decorator, params)
+        local res, err = httpc:request_uri(token_endpoint, params)
 
         if not res then
             err = "Accessing token endpoint URL (" .. token_endpoint .. ") failed: " .. err
