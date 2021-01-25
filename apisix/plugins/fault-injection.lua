87ddba0f19bee55670963e73e94151a4907ace61
Yuelin Zheng <2226815922@qq.com>
2021-01-25 23:44:00 +0800

    feat(fault-injection): support conditional fault injection using nginx variables (#3363)
    close #2511

--- apisix/plugins/fault-injection.lua
+++ apisix/plugins/fault-injection.lua
@@ -15,9 +15,11 @@
 -- limitations under the License.
 --
 local core = require("apisix.core")
+local expr = require("resty.expr.v1")
 
 local sleep = core.sleep
 local random = math.random
+local ipairs = ipairs
 
 local plugin_name   = "fault-injection"
 
@@ -30,7 +32,11 @@ local schema = {
             properties = {
                 http_status = {type = "integer", minimum = 200},
                 body = {type = "string", minLength = 0},
-                percentage = {type = "integer", minimum = 0, maximum = 100}
+                percentage = {type = "integer", minimum = 0, maximum = 100},
+                vars = {
+                    type = "array",
+                    maxItems = 20
+                }
             },
             required = {"http_status"},
         },
@@ -38,7 +44,11 @@ local schema = {
             type = "object",
             properties = {
                 duration = {type = "number", minimum = 0},
-                percentage = {type = "integer", minimum = 0, maximum = 100}
+                percentage = {type = "integer", minimum = 0, maximum = 100},
+                vars = {
+                    type = "array",
+                    maxItems = 20
+                }
             },
             required = {"duration"},
         }
@@ -64,10 +74,44 @@ local function sample_hit(percentage)
 end
 
 
+local function vars_match(vars, ctx)
+    local match_result
+    for _, var in ipairs(vars) do
+        local expr, _ = expr.new(var)
+        match_result = expr:eval(ctx.var)
+        if match_result then
+            break
+        end
+    end
+
+    return match_result
+end
+
+
 function _M.check_schema(conf)
     local ok, err = core.schema.check(schema, conf)
     if not ok then
         return false, err
+    end
+
+    if conf.abort and conf.abort.vars then
+        for _, var in ipairs(conf.abort.vars) do
+            local _, err = expr.new(var)
+            if err then
+                core.log.error("failed to create vars expression: ", err)
+                return false, err
+            end
+        end
+    end
+
+    if conf.delay and conf.delay.vars then
+        for _, var in ipairs(conf.delay.vars) do
+            local _, err = expr.new(var)
+            if err then
+                core.log.error("failed to create vars expression: ", err)
+                return false, err
+            end
+        end
     end
 
     return true
@@ -77,11 +121,23 @@ end
 function _M.rewrite(conf, ctx)
     core.log.info("plugin rewrite phase, conf: ", core.json.delay_encode(conf))
 
-    if conf.delay and sample_hit(conf.delay.percentage) then
+    local abort_vars = true
+    if conf.abort and conf.abort.vars then
+        abort_vars = vars_match(conf.abort.vars, ctx)
+    end
+    core.log.info("abort_vars: ", abort_vars)
+
+    local delay_vars = true
+    if conf.delay and conf.delay.vars then
+        delay_vars = vars_match(conf.delay.vars, ctx)
+    end
+    core.log.info("delay_vars: ", delay_vars)
+
+    if conf.delay and sample_hit(conf.delay.percentage) and delay_vars then
         sleep(conf.delay.duration)
     end
 
-    if conf.abort and sample_hit(conf.abort.percentage) then
+    if conf.abort and sample_hit(conf.abort.percentage) and abort_vars then
         return conf.abort.http_status, core.utils.resolve_var(conf.abort.body, ctx.var)
     end
 end
