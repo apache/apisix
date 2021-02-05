2748f93d047a24796cd5494ef357d5f5d1ad217a
idbeta <idbeta@gmail.com>
2021-02-05 10:45:39 +0800

    test: add fuzzing on apisix dp

---
+++ t/fuzzing/getcase.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python 
+# Designed for use with boofuzz v0.2.0
+import subprocess
+from boofuzz import *
+
+def create_route():
+    command = '''curl http://127.0.0.1:9080/apisix/admin/routes/1 -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' -X PUT -d '
+{
+    "uri": "/get*",
+    "upstream": {
+        "type": "roundrobin",
+        "nodes": {
+            "127.0.0.1:6666": 1
+        }
+    }
+}'
+    '''
+    r = subprocess.Popen(command,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+    print(r.stdout.read())
+
+def main():
+    session = Session(
+        target=Target(
+            connection=SocketConnection("127.0.0.1", 9080, proto='tcp')
+        ),
+    )
+
+    s_initialize(name="Request")
+    with s_block("Request-Line"):
+        s_group("Method", ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE'])
+        s_delim(" ", name='space-1')
+        s_string("/get?", name='Request-URI')
+        s_delim(" ", name='space-2')
+        s_string('HTTP/1.1', name='HTTP-Version')
+        s_static("\r\n", name="Request-Line-CRLF")
+        s_string("Host:", name="Host-Line")
+        s_delim(" ", name="space-3")
+        s_string("example.com", name="Host-Line-Value")
+        s_static("\r\n", name="Host-Line-CRLF")
+    s_static("\r\n", "Request-CRLF")
+
+    session.connect(s_get("Request"))
+
+    session.fuzz()
+
+
+if __name__ == "__main__":
+    create_route()
+    # main()
