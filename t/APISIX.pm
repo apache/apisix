e957bc3daae73696051dd24ef4ef8e5eb9583e6f
罗泽轩 <spacewanderlzx@gmail.com>
2021-03-14 18:06:04 +0800

    fix: correct the luasocket patch (#3819)

--- t/APISIX.pm
+++ t/APISIX.pm
@@ -367,6 +367,8 @@ _EOC_
     $extra_init_by_lua
 _EOC_
 
+    my $extra_init_worker_by_lua = $block->extra_init_worker_by_lua // "";
+
     my $http_config = $block->http_config // '';
     $http_config .= <<_EOC_;
     $lua_deps_path
@@ -417,6 +419,7 @@ _EOC_
 
     init_worker_by_lua_block {
         require("apisix").http_init_worker()
+        $extra_init_worker_by_lua
     }
 
     log_format main escape=default '\$remote_addr - \$remote_user [\$time_local] \$http_host "\$request" \$status \$body_bytes_sent \$request_time "\$http_referer" "\$http_user_agent" \$upstream_addr \$upstream_status \$upstream_response_time "\$upstream_scheme://\$upstream_host\$upstream_uri"';
