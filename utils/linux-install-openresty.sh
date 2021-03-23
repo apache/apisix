62a186aa8179f8905540924af9fe5d34450fc619
罗泽轩 <spacewanderlzx@gmail.com>
2021-03-23 20:07:48 +0800

    ci: use build-apisix-openresty.sh instead (#3847)

--- utils/linux-install-openresty.sh
+++ utils/linux-install-openresty.sh
@@ -26,49 +26,9 @@ sudo apt-get update
 
 if [ "$OPENRESTY_VERSION" == "source" ]; then
     cd ..
-
-    wget https://openresty.org/download/openresty-1.19.3.1.tar.gz
-    tar -zxvpf openresty-1.19.3.1.tar.gz
-
-    git clone --depth=1 https://github.com/api7/ngx_multi_upstream_module.git
-    cd ngx_multi_upstream_module || exit 1
-    ./patch.sh ../openresty-1.19.3.1
-
-    git clone --depth=1 https://github.com/api7/mod_dubbo.git ../mod_dubbo
-
-    cd ../openresty-1.19.3.1 || exit 1
-    ./configure --prefix="/usr/local/openresty-debug" \
-        --add-module=../mod_dubbo --add-module=../ngx_multi_upstream_module \
-        --with-debug \
-        --with-poll_module \
-        --with-pcre-jit \
-        --without-http_rds_json_module \
-        --without-http_rds_csv_module \
-        --without-lua_rds_parser \
-        --with-stream \
-        --with-stream_ssl_module \
-        --with-stream_ssl_preread_module \
-        --with-http_v2_module \
-        --without-mail_pop3_module \
-        --without-mail_imap_module \
-        --without-mail_smtp_module \
-        --with-http_stub_status_module \
-        --with-http_realip_module \
-        --with-http_addition_module \
-        --with-http_auth_request_module \
-        --with-http_secure_link_module \
-        --with-http_random_index_module \
-        --with-http_gzip_static_module \
-        --with-http_sub_module \
-        --with-http_dav_module \
-        --with-http_flv_module \
-        --with-http_mp4_module \
-        --with-http_gunzip_module \
-        --with-threads \
-        --with-compat \
-        --with-luajit-xcflags='-DLUAJIT_NUMMODE=2 -DLUAJIT_ENABLE_LUA52COMPAT'
-    make
-    sudo make install
+    wget https://raw.githubusercontent.com/api7/apisix-build-tools/master/build-apisix-openresty.sh
+    chmod +x build-apisix-openresty.sh
+    ./build-apisix-openresty.sh latest
 
     sudo apt-get install openresty-openssl111-debug-dev
     exit 0
