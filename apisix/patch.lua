f95a0d8a4f15de9c82917b199d297e34d9f138c2
spacewander <spacewanderlzx@gmail.com>
2021-03-25 10:47:20 +0800

    lint
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>

--- apisix/patch.lua
+++ apisix/patch.lua
@@ -14,6 +14,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 --
+local require = require
 local socket = require("socket")
 local unix_socket = require("socket.unix")
 local ssl = require("ssl")
