f498e468d5f6bee3dc48eb3692d4eaafa4d1ca80
yiyiyimu <wosoyoung@gmail.com>
2021-04-06 22:29:27 +0800

    fix typo
    Signed-off-by: yiyiyimu <wosoyoung@gmail.com>

--- t/chaos/utils.go
+++ t/chaos/utils.go
@@ -90,7 +90,7 @@ func setRoute(e *httpexpect.Expect, expectStatus int) {
 			 },
 			 "upstream": {
 				 "nodes": {
-					 "localhost:8080": 1
+					 "httpbin.default.svc.cluster.local:8000": 1
 				 },
 				 "type": "roundrobin"
 			 }
