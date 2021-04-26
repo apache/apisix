89358d27f3361011a4a167db5cc016b5caec5773
seven dickens <41516298+dickens7@users.noreply.github.com>
2021-04-26 13:14:29 +0800

    feat: optimize skywalking instancename (#4069)

--- apisix/plugins/skywalking.lua
+++ apisix/plugins/skywalking.lua
@@ -129,6 +129,11 @@ function _M.init()
 
     -- TODO: maybe need to fetch them from plugin-metadata
     local metadata_shdict = ngx.shared.tracing_buffer
+
+    if local_plugin_info.service_instance_name == "$hostname" then
+        local_plugin_info.service_instance_name = core.utils.gethostname()
+    end
+
     metadata_shdict:set('serviceName', local_plugin_info.service_name)
     metadata_shdict:set('serviceInstanceName', local_plugin_info.service_instance_name)
 
