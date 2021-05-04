9d89a510961cbd482c21d4c726e3ba880ae51a86
daming <zteny@foxmail.com>
2021-05-04 18:02:18 +0800

    fix: fix peer missed in skywalking

--- apisix/plugins/skywalking.lua
+++ apisix/plugins/skywalking.lua
@@ -93,6 +93,7 @@ function _M.body_filter(conf, ctx)
     if ctx.skywalking_sample and ngx.arg[2] then
         Span.setComponentId(ngx.ctx.exitSpan, 6002)
         Span.setComponentId(ngx.ctx.entrySpan, 6002)
+        ngx.ctx.exitSpan.peer = ctx.balancer_ip .. ":" .. ctx.balancer_port
         sw_tracer:finish()
         core.log.info("tracer finish")
     end
