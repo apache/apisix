e50ab9df9418564892758801f9f5ffbbeb1de512
罗泽轩 <spacewanderlzx@gmail.com>
2021-06-25 10:56:33 +0800

    feat: patch UDP socket to resolve host by ourselves (#4473)
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>

--- apisix/patch.lua
+++ apisix/patch.lua
@@ -15,6 +15,7 @@
 -- limitations under the License.
 --
 local require = require
+require("resty.dns.resolver") -- preload dns resolver to prevent recursive patch
 local ipmatcher = require("resty.ipmatcher")
 local socket = require("socket")
 local unix_socket = require("socket.unix")
@@ -22,6 +23,7 @@ local ssl = require("ssl")
 local get_phase = ngx.get_phase
 local ngx_socket = ngx.socket
 local original_tcp = ngx.socket.tcp
+local original_udp = ngx.socket.udp
 local concat_tab = table.concat
 local new_tab = require("table.new")
 local log = ngx.log
@@ -79,6 +81,43 @@ do
         end
 
         sock.connect = new_tcp_sock_connect
+        return sock
+    end
+end
+
+
+local patch_udp_socket
+do
+    local old_udp_sock_setpeername
+
+    local function new_udp_sock_setpeername(sock, host, port)
+        local core_str = require("apisix.core.string")
+        local resolver = require("apisix.core.resolver")
+
+        if host then
+            if core_str.has_prefix(host, "unix:") then
+                return old_udp_sock_setpeername(sock, host)
+            end
+
+            if not ipmatcher.parse_ipv4(host) and not ipmatcher.parse_ipv6(host) then
+                local err
+                host, err = resolver.parse_domain(host)
+                if not host then
+                    return nil, "failed to parse domain: " .. err
+                end
+            end
+        end
+
+        return old_udp_sock_setpeername(sock, host, port)
+    end
+
+
+    function patch_udp_socket(sock)
+        if not old_udp_sock_setpeername then
+            old_udp_sock_setpeername = sock.setpeername
+        end
+
+        sock.setpeername = new_udp_sock_setpeername
         return sock
     end
 end
@@ -277,6 +316,10 @@ function _M.patch()
         end
 
         return luasocket_tcp()
+    end
+
+    ngx_socket.udp = function ()
+        return patch_udp_socket(original_udp())
     end
 end
 
