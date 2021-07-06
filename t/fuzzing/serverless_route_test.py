5e34ebbe79a3c9d46aa47b0dfdb12b4ad9229090
罗泽轩 <spacewanderlzx@gmail.com>
2021-07-06 09:50:41 +0800

    test: avoid `from moudle_name import *` (#4537)
    Fix #3688
    
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>

--- t/fuzzing/serverless_route_test.py
+++ t/fuzzing/serverless_route_test.py
@@ -19,7 +19,7 @@
 
 import subprocess
 from public import initfuzz, run_test
-from boofuzz import *
+from boofuzz import s_block, s_delim, s_get, s_group, s_initialize, s_size, s_static, s_string
 
 def create_route():
     command = '''curl -i http://127.0.0.1:9080/apisix/admin/routes/1 -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' -X PUT -d '
