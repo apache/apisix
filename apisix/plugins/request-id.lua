9e69b2ebf29dfcc520fc1a6f7a91995bfc317c57
dickens7 <languageusa@163.com>
2021-07-07 23:12:29 +0800

    feat Request-ID plugin adds Snowflake algorithm

--- apisix/plugins/request-id.lua
+++ apisix/plugins/request-id.lua
@@ -15,52 +15,69 @@
 -- limitations under the License.
 --
 
-local ngx           = ngx
-local core          = require("apisix.core")
-local snowflake     = require("snowflake")
-local uuid          = require('resty.jit-uuid')
-local process       = require("ngx.process")
-local tostring      = tostring
-local math_ceil     = math.ceil
+local ngx = ngx
+local bit = require("bit")
+local core = require("apisix.core")
+local snowflake = require("snowflake")
+local uuid = require("resty.jit-uuid")
+local process = require("ngx.process")
+local tostring = tostring
+local math_pow = math.pow
 
-local plugin_name   = "request-id"
+local plugin_name = "request-id"
 
 local worker_number = nil
 local snowflake_init = nil
+
+local attr = nil
 
 local schema = {
     type = "object",
     properties = {
         header_name = {type = "string", default = "X-Request-Id"},
         include_in_response = {type = "boolean", default = true},
-        algorithm = {type = "string", enum = {"uuid", "snowflake"}, default = "uuid"},
+        algorithm = {type = "string", enum = {"uuid", "snowflake"}, default = "uuid"}
     }
 }
 
+local attr_schema = {
+    type = "object",
+    properties = {
+        snowflake = {
+            type = "object",
+            properties = {
+                enable = {type = "boolean"},
+                snowflake_epoc = {type = "integer", minimum = 1, default = 1609459200000},
+                node_id_bits = {type = "integer", minimum = 1, default = 5},
+                sequence_bits = {type = "integer", minimum = 1, default = 10},
+                datacenter_id_bits = {type = "integer", minimum = 1, default = 5},
+                worker_number_ttl = {type = "integer", minimum = 1, default = 30},
+                worker_number_interval = {type = "integer", minimum = 1, default = 10}
+            }
+        }
+    }
+}
 
 local _M = {
     version = 0.1,
     priority = 11010,
     name = plugin_name,
-    schema = schema,
+    schema = schema
 }
-
 
 function _M.check_schema(conf)
     return core.schema.check(schema, conf)
 end
 
-
-local function gen_worker_number()
+local function gen_worker_number(max_number)
     if worker_number == nil then
         local etcd_cli, prefix = core.etcd.new()
-        local res, _ = etcd_cli:grant(20)
+        local res, _ = etcd_cli:grant(attr.worker_number_ttl)
 
-        local prefix = prefix .. "/plugins/snowflake/"
+        local prefix = prefix .. "/plugins/request-id/snowflake/"
         local uuid = uuid.generate_v4()
         local id = 1
-        while( id < 1024 )
-        do
+        while (id <= max_number) do
             ::continue::
             local _, err1 = etcd_cli:setnx(prefix .. tostring(id), uuid)
             local res2, err2 = etcd_cli:get(prefix .. tostring(id))
@@ -71,13 +88,20 @@ local function gen_worker_number()
             else
                 worker_number = id
 
-                local _, err3 = etcd_cli:set(prefix .. tostring(id)
-                    , uuid, {prev_kv = true, lease = res.body.ID})
+                local _, err3 =
+                    etcd_cli:set(
+                    prefix .. tostring(id),
+                    uuid,
+                    {
+                        prev_kv = true,
+                        lease = res.body.ID
+                    }
+                )
 
                 if err3 then
                     id = id + 1
                     etcd_cli:delete(prefix .. tostring(id))
-                    core.log.error("set worker_number " ..id .. " lease error: " .. err3 )
+                    core.log.error("set worker_number " .. id .. " lease error: " .. err3)
                     goto continue
                 end
 
@@ -90,34 +114,52 @@ local function gen_worker_number()
                     end
                     core.log.info("snowflake worker_number lease success.")
                 end
-                ngx.timer.every(10, handler, etcd_cli, res.body.ID)
+                ngx.timer.every(attr.worker_number_interval, handler, etcd_cli, res.body.ID)
 
                 core.log.notice("snowflake worker_number: " .. id)
                 break
             end
         end
 
-        if worker_number  == nil then
+        if worker_number == nil then
             core.log.error("No worker_number is not available")
+            return nil
         end
     end
     return worker_number
 end
 
+local function split_worker_number(worker_number, node_id_bits, datacenter_id_bits)
+    local num = bit.tobit(worker_number)
+    local worker_id = bit.band(num, math_pow(2, node_id_bits) - 1) + 1
+    num = bit.rshift(num, node_id_bits)
+    local datacenter_id = bit.band(num, math_pow(2, datacenter_id_bits) - 1) + 1
+    return worker_id, datacenter_id
+end
 
 local function next_id()
     if snowflake_init == nil then
-        worker_number = gen_worker_number()
-        local worker_id = worker_number % 32
-        local datacenter_id = math_ceil(worker_number / 32)
-        core.log.info("snowflake init datacenter_id: "
-            .. datacenter_id .. " worker_id: " .. worker_id)
-        snowflake.init(worker_id, datacenter_id)
+        local max_number = math_pow(2, (attr.node_id_bits + attr.datacenter_id_bits))
+        worker_number = gen_worker_number(max_number)
+        if worker_number == nil then
+            return ""
+        end
+        local worker_id, datacenter_id =
+            split_worker_number(worker_number, attr.node_id_bits, attr.datacenter_id_bits)
+        core.log.notice("snowflake init datacenter_id: " .. datacenter_id ..
+                        " worker_id: " .. worker_id)
+        snowflake.init(
+            worker_id,
+            datacenter_id,
+            attr.snowflake_epoc,
+            attr.node_id_bits,
+            attr.datacenter_id_bits,
+            attr.sequence_bits
+        )
         snowflake_init = true
     end
     return snowflake:next_id()
 end
-
 
 function _M.rewrite(conf, ctx)
     local headers = ngx.req.get_headers()
@@ -136,7 +178,6 @@ function _M.rewrite(conf, ctx)
     end
 end
 
-
 function _M.header_filter(conf, ctx)
     if not conf.include_in_response then
         return
@@ -149,26 +190,34 @@ function _M.header_filter(conf, ctx)
 end
 
 function _M.init()
-    if process.type() == "worker" then
-        ngx.timer.at(0, next_id)
+    local local_conf = core.config.local_conf()
+    local attr = core.table.try_read_attr(local_conf, "plugin_attr", plugin_name)
+    local ok, err = core.schema.check(attr_schema, attr)
+    if not ok then
+        core.log.error("failed to check the plugin_attr[", plugin_name, "]", ": ", err)
+        return
+    end
+    attr = attr
+    if attr.snowflake.enable then
+        if process.type() == "worker" then
+            ngx.timer.at(0, next_id)
+        end
     end
 end
-
 
 function _M.api()
     return {
         {
             methods = {"GET"},
             uri = "/apisix/plugin/request_id/uuid",
-            handler = uuid,
+            handler = uuid
         },
         {
             methods = {"GET"},
             uri = "/apisix/plugin/request_id/snowflake_id",
-            handler = next_id,
-        },
+            handler = next_id
+        }
     }
 end
-
 
 return _M
