2b510b84397eea4629a465159b22c05018ed807b
xin.gao <bluesgao@126.com>
2021-07-09 15:19:29 +0800

    admin api proto删除操作异常

--- apisix/admin/proto.lua
+++ apisix/admin/proto.lua
@@ -117,25 +117,31 @@ function _M.post(id, conf)
     return res.status, res.body
 end
 
-function _M.check_proto_used(plugins, deleting, ptype, pid)
+local function check_proto_used(plugins, deleting, ptype, pid)
 
-    core.log.info("plugins1: ", core.json.delay_encode(plugins, true))
+    core.log.info("check_proto_used plugins: ", core.json.delay_encode(plugins, true))
+    core.log.info("check_proto_used deleting: ", deleting)
+    core.log.info("check_proto_used ptype: ", ptype)
+    core.log.info("check_proto_used pid: ", pid)
 
     if plugins then
         if type(plugins) == "table" and plugins["grpc-transcode"]
            and plugins["grpc-transcode"].proto_id
            and tostring(plugins["grpc-transcode"].proto_id) == deleting then
-            return 400, {error_msg = "can not delete this proto,"
+            --core.log.info("check_proto_used 此协议正在使用: ", core.json.delay_encode(plugins, true))
+            return false, {error_msg = "can not delete this proto,"
                                      .. ptype .. " [" .. pid
                                      .. "] is still using it now"}
         end
     end
+    return true,"ok"
 end
 
 function _M.delete(id)
     if not id then
         return 400, {error_msg = "missing proto id"}
     end
+    core.log.info("proto delete: ", id)
 
     local routes, routes_ver = get_routes()
 
@@ -144,13 +150,16 @@ function _M.delete(id)
 
     if routes_ver and routes then
         for _, route in ipairs(routes) do
-            if type(route) == "table" and route.value
-               and route.value.plugins then
-                  return _M.check_proto_used(route.value.plugins, id, "route",
-                                             route.value.id)
+            core.log.info("proto delete route item: ", core.json.delay_encode(route, true))
+            if type(route) == "table" and route.value and route.value.plugins then
+                   local ret, err = check_proto_used(route.value.plugins, id, "route",route.value.id)
+                   if not ret then
+                        return 400, err
+                    end
             end
         end
     end
+    core.log.info("proto delete route ref check pass: ", id)
 
     local services, services_ver = get_services()
 
@@ -159,13 +168,15 @@ function _M.delete(id)
 
     if services_ver and services then
         for _, service in ipairs(services) do
-            if type(service) == "table" and service.value
-               and service.value.plugins then
-                  return _M.check_proto_used(service.value.plugins, id,
-                                             "service", service.value.id)
+            if type(service) == "table" and service.value and service.value.plugins then
+                   local ret, err = check_proto_used(service.value.plugins, id, "service", service.value.id)
+                   if not ret then
+                        return 400, err
+                    end
             end
         end
     end
+    core.log.info("proto delete service ref check pass: ", id)
 
     local key = "/proto/" .. id
     -- core.log.info("key: ", key)
