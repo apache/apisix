04971a4b4b7fe330d2c7345bc1c57a23458c4256
xin.gao <bluesgao@126.com>
2021-07-12 12:25:47 +0800

    fix: ref check while deleting proto via Admin api

--- apisix/admin/proto.lua
+++ apisix/admin/proto.lua
@@ -169,10 +169,10 @@ function _M.delete(id)
     if services_ver and services then
         for _, service in ipairs(services) do
             if type(service) == "table" and service.value and service.value.plugins then
-                   local ret, err = check_proto_used(service.value.plugins, id, "service", service.value.id)
-                   if not ret then
-                        return 400, err
-                    end
+                local ret, err = check_proto_used(service.value.plugins, id, "service", service.value.id)
+                if not ret then
+                    return 400, err
+                end
             end
         end
     end
