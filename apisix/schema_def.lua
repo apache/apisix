79af5088f497f018eb6d639a11a84820154c2ead
罗泽轩 <spacewanderlzx@gmail.com>
2021-07-16 10:27:34 +0800

    feat(discovery): check node format (#4610)
    To prevent 500 error or infinite loop because of a malformed nodes
    format.
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>

--- apisix/schema_def.lua
+++ apisix/schema_def.lua
@@ -331,6 +331,37 @@ local nodes_schema = {
         }
     }
 }
+_M.discovery_nodes = {
+    type = "array",
+    items = {
+        type = "object",
+        properties = {
+            host = {
+                description = "domain or ip",
+            },
+            port = {
+                description = "port of node",
+                type = "integer",
+                minimum = 1,
+            },
+            weight = {
+                description = "weight of node",
+                type = "integer",
+                minimum = 0,
+            },
+            priority = {
+                description = "priority of node",
+                type = "integer",
+            },
+            metadata = {
+                description = "metadata of node",
+                type = "object",
+            }
+        },
+        -- nodes from DNS discovery may not contain port
+        required = {"host", "weight"},
+    },
+}
 
 
 local certificate_scheme = {
