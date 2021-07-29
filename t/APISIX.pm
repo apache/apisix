8f75e811e281c3d9e6249df5eee83574e049181e
spacewander <spacewanderlzx@gmail.com>
2021-07-29 12:01:14 +0800

    fix: when a request caused a 500 error, the status was converted to 405
    It happened when this request is not a GET or HEAD request.
    
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>

--- t/APISIX.pm
+++ t/APISIX.pm
@@ -89,7 +89,6 @@ my $ssl_ecc_crt = read_file("t/certs/apisix_ecc.crt");
 my $ssl_ecc_key = read_file("t/certs/apisix_ecc.key");
 my $test2_crt = read_file("t/certs/test2.crt");
 my $test2_key = read_file("t/certs/test2.key");
-my $test_50x_html = read_file("t/error_page/50x.html");
 $user_yaml_config = <<_EOC_;
 apisix:
   node_listen: 1984
@@ -541,7 +540,9 @@ _EOC_
 
         location \@50x.html {
             set \$from_error_page 'true';
-            try_files /50x.html \$uri;
+            content_by_lua_block {
+                require("apisix.error_handling").handle_500()
+            }
             header_filter_by_lua_block {
                 apisix.http_header_filter_phase()
             }
@@ -627,7 +628,9 @@ _EOC_
 
         location \@50x.html {
             set \$from_error_page 'true';
-            try_files /50x.html \$uri;
+            content_by_lua_block {
+                require("apisix.error_handling").handle_500()
+            }
             header_filter_by_lua_block {
                 apisix.http_header_filter_phase()
             }
@@ -774,8 +777,6 @@ $ssl_ecc_key
 $test2_crt
 >>> ../conf/cert/test2.key
 $test2_key
->>> 50x.html
-$test_50x_html
 $user_apisix_yaml
 _EOC_
 
