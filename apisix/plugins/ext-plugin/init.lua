69b9b0b404341fc1331eb52b1ca509bd1535caa7
spacewander <spacewanderlzx@gmail.com>
2021-08-02 11:49:06 +0800

    feat(ext-plugin): send the idempotent key when preparing conf
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>

--- apisix/plugins/ext-plugin/init.lua
+++ apisix/plugins/ext-plugin/init.lua
@@ -254,8 +254,10 @@ end
 local rpc_call
 local rpc_handlers = {
     nil,
-    function (conf, ctx, sock)
+    function (conf, ctx, sock, unique_key)
         builder:Clear()
+
+        local key = builder:CreateString(unique_key)
 
         local conf_vec
         if conf.conf then
@@ -278,6 +280,7 @@ local rpc_handlers = {
         end
 
         prepare_conf_req.Start(builder)
+        prepare_conf_req.AddKey(builder, key)
         if conf_vec then
             prepare_conf_req.AddConf(builder, conf_vec)
         end
@@ -306,8 +309,10 @@ local rpc_handlers = {
         return token
     end,
     function (conf, ctx, sock, entry)
+        local lrucache_id = core.lrucache.plugin_ctx_id(ctx, entry)
         local token, err = core.lrucache.plugin_ctx(lrucache, ctx, entry, rpc_call,
-                                                    constants.RPC_PREPARE_CONF, conf, ctx)
+                                                    constants.RPC_PREPARE_CONF, conf, ctx,
+                                                    lrucache_id)
         if not token then
             return nil, err
         end
