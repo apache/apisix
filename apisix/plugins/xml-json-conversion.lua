41f3880eca4876a4cfc2c48bf4051f901110275d
xuwei <>
2021-08-13 15:37:34 +0800

    Plugin: xml-json-conversion plugin convert xml data from request body to json response, and vice versa Docs: xml-json-conversion.md, include en and zh TEST: xml-json-conversion.t

---
+++ apisix/plugins/xml-json-conversion.lua
@@ -0,0 +1,146 @@
+--
+-- Licensed to the Apache Software Foundation (ASF) under one or more
+-- contributor license agreements.  See the NOTICE file distributed with
+-- this work for additional information regarding copyright ownership.
+-- The ASF licenses this file to You under the Apache License, Version 2.0
+-- (the "License"); you may not use this file except in compliance with
+-- the License.  You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
+local ngx     = ngx
+local core    = require("apisix.core")
+local xml2lua = require("xml2lua")
+local handler = require("xmlhandler.tree")
+local cjson   = require('cjson.safe')
+
+local metadata_schema = {
+    type = "object",
+    properties = {},
+    additionalProperties = false,
+}
+
+local schema = {
+    type = "object",
+    properties = {},
+    additionalProperties = false,
+}
+
+local plugin_name = "xml-json-conversion"
+
+local _M = {
+    version = 0.1,
+    priority = 90,
+    name = plugin_name,
+    schema = schema,
+    metadata_schema = metadata_schema,
+}
+
+function _M.check_schema(conf)
+    return core.schema.check(schema, conf)
+end
+
+local function getFile(file_name)
+    local f = assert(io.open(file_name, 'r'))
+    local string = f:read("*all")
+    f:close()
+    return string
+end
+
+local function xml2json(xml_data)
+    local convertHandler = handler:new()
+    local parser = xml2lua.parser(convertHandler)
+    parser:parse(xml_data)
+    return 200, cjson.encode(convertHandler.root)
+end
+
+local function json2xml(table_data)
+    return 200, xml2lua.toXml(cjson.decode(table_data))
+end
+
+function _M.access(conf, ctx)
+    core.log.warn("\n||||||| ctx ,", core.json.encode(ctx, true))
+    core.log.warn("\n||||||| conf ,", core.json.encode(conf, true))
+
+    local request_header = ngx.req.get_headers()
+    ngx.req.read_body()
+    local request_body = ngx.req.get_body_data()
+    if nil == request_body then
+        local file_name = ngx.req.get_body_file()
+        if file_name then
+            request_body = getFile(file_name)
+        end
+    end
+
+    core.log.warn("------- request_body : ", request_body)
+    if request_header["Content-Type"] == "application/json" then
+        if string.find(request_header["Accept"], "text/xml") == nil then
+            return 401, {message = "Operation not supported"}
+        end
+        return json2xml(request_body)
+    elseif request_header["Content-Type"] == "text/xml" then
+        if string.find(request_header["Accept"], "application/json") == nil then
+            return 401, {message = "Operation not supported"}
+        end
+        return xml2json(request_body)
+    else
+        return 401, {message = "Operation not supported"}
+    end
+end
+
+local function get_json()
+    local request_header = ngx.req.get_headers()
+    ngx.req.read_body()
+    local request_body = ngx.req.get_body_data()
+    if nil == request_body then
+        local file_name = ngx.req.get_body_file()
+        if file_name then
+            request_body = getFile(file_name)
+        end
+    end
+    if nil == request_body then
+        return 401, {message = "Invalid request body"}
+    end
+    if request_header["Content-Type"] == "application/json" then
+        if string.find(request_header["Accept"], "text/xml") == nil then
+            return 401, {message = "Operation not supported"}
+        end
+        return json2xml(request_body)
+    elseif request_header["Content-Type"] == "text/xml" then
+        if string.find(request_header["Accept"], "application/json") == nil then
+            return 401, {message = "Operation not supported"}
+        end
+        return xml2json(request_body)
+    else
+        return 401, {message = "Operation not supported"}
+    end
+end
+
+function _M.api()
+    return {
+        {
+            methods = {"GET"},
+            uri = "/v1/plugin/xml-json-conversion",
+            handler = get_json,
+        }
+    }
+end
+
+--function _M.control_api()
+--    return {
+--        {
+--            methods = {"GET"},
+--            uris ={"/v1/plugin/xml-json-conversion"},
+--            handler = get_json,
+--        }
+--    }
+--end
+
+return _M
---
+++ docs/zh/latest/plugins/xml-json-conversion.md
@@ -0,0 +1,97 @@
+---
+title: xml-json-conversion
+---
+
+
+<!--
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+-->
+
+## 目录
+
+- [**名称**](#名称)
+- [**属性**](#属性)
+- [**如何开启**](#如何开启)
+- [**测试插件**](#测试插件)
+- [**禁用插件**](#禁用插件)
+
+
+## 名称
+
+xml-json-conversion 插件意在将request body中的xml数据转换成json输出,或者将json数据转换成xml数据输出
+
+## 属性
+
+无
+
+## 如何开启
+
+下面是一个示例，如何在指定的路由上开启插件:
+
+```shell
+curl -i http://127.0.0.1:9080/apisix/admin/routes/1  -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' -X PUT -d '
+{
+    "uri": "/hello",
+    "plugins": {
+        "xml-json-conversion": {}
+    },
+    "upstream": {
+        "type": "roundrobin",
+        "nodes": {
+            "39.97.63.215:80": 1
+        }
+    }
+}'
+```
+
+## 测试插件
+
+通过curl访问
+
+```shell
+curl -X GET http://127.0.0.1:9080/hello \
+-H 'Content-Type: text/xml' \
+-H 'Accept: application/json' \
+--data '
+<people>
+  <person>
+    <name>Manoel</name>
+    <city>Palmas-TO</city>
+  </person>
+</people>'
+
+
+{"people":{"person":{"name":"Manoel","city":"Palmas-TO"}}}
+```
+
+## 禁用插件
+
+想要禁用“xml-json-conversion”插件，是非常简单的，将对应的插件配置从 json 配置删除，就会立即生效，不需要重新启动服务：
+
+```shell
+curl -i http://127.0.0.1:9080/apisix/admin/routes/1  -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' -X PUT -d '
+{
+    "uri": "/hello",
+    "upstream": {
+        "type": "roundrobin",
+        "nodes": {
+            "39.97.63.215:80": 1
+        }
+    }
+}'
+```
