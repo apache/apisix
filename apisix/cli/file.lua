8fbce3ca148ec523a1092a409a7d9722a17eb247
wayne-cheng <zhengwei@tiduyun.com>
2021-08-27 17:20:17 +0800

    add apisix.ssl.listen in merge_conf validation
    Signed-off-by: wayne-cheng <zhengwei@tiduyun.com>

--- apisix/cli/file.lua
+++ apisix/cli/file.lua
@@ -126,6 +126,10 @@ local function path_is_multi_type(path, type_val)
         return true
     end
 
+    if path == "apisix->ssl->listen" and type_val == "number" then
+        return true
+    end
+
     return false
 end
 
