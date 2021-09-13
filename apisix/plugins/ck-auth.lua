836019259a2a1a76a67df4a66d6d7880cba9d009
ckallcoud@foxmail.com <19990205>
2021-09-13 16:44:53 +0800

    新增ck-auth逻辑

--- apisix/plugins/ck-auth.lua
+++ apisix/plugins/ck-auth.lua
@@ -1,7 +1,9 @@
 local core = require("apisix.core")
 
 local plugin_name = "ck-auth"
-
+local status_codes = {
+    200,401
+}
 local schema = {
     type = "object",
     properties = {
@@ -24,11 +26,29 @@ function _M.check_schema(conf)
     return core.schema.check(schema, conf)
 end
 
--- 入口
 function _M.access(conf, ctx)
 
-    core.log.warn(core.json.encode(conf))
+    core.log.info("access")
 
 end
+
+-- 返回参数的固定格式
+local function responseBody(status_code, message)
+    return status_code, {
+        status_code = status_code,
+        message = message,
+        data = nil
+    }
+end
+
+function _M.rewrite(conf, ctx)
+    core.log.info("rewrite")
+    local auth_token = core.request.header(ctx, "ck")
+    if not auth_token then
+        return responseBody(status_codes[2], "没有在header里面传入程锟的插件认证关键参数")
+    end
+    return responseBody(status_codes[1], auth_token)
+end
+
 
 return _M
