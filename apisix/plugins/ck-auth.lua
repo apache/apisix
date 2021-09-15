7f122fb7cf5b9a869602f74db1d22feb8df39492
ckallcoud@foxmail.com <19990205>
2021-09-15 10:08:59 +0800

    新增ck-auth逻辑

--- apisix/plugins/ck-auth.lua
+++ apisix/plugins/ck-auth.lua
@@ -2,7 +2,8 @@ local core = require("apisix.core")
 
 local plugin_name = "ck-auth"
 local status_codes = {
-    200, 401
+    success = 200,
+    error = 400
 }
 local schema = {
     type = "object",
@@ -33,21 +34,37 @@ function _M.access(conf, ctx)
 end
 
 -- 返回参数的固定格式
-local function responseBody(status_code, message)
+local function responseBody(status_code, message, data)
     return status_code, {
         status_code = status_code,
         message = message,
-        data = nil
+        data = data
     }
 end
 
 function _M.rewrite(conf, ctx)
     core.log.info("rewrite")
     local auth_token = core.request.header(ctx, "ck")
+    local data = {
+        request = {
+            host = core.request.get_host(ctx),
+            http_version = core.request.get_http_version(),
+            ip = core.request.get_ip(ctx),
+            port = core.request.get_port(ctx),
+            remote_client_ip = core.request.get_remote_client_ip(ctx),
+            remote_client_port = core.request.get_remote_client_port(ctx),
+            scheme = core.request.get_scheme(ctx),
+            uri_args = core.request.get_uri_args(ctx),
+            headers = core.request.headers()
+        },
+        response = {
+            upstream_status = core.response.get_upstream_status(ctx)
+        }
+    }
     if not auth_token then
-        return responseBody(status_codes[2], "没有在header里面传入程锟的插件认证关键参数")
+        return responseBody(status_codes.error, "没有在header里面传入程锟的插件认证关键参数", data)
     end
-    return responseBody(status_codes[1], auth_token)
+    return responseBody(status_codes.success, auth_token, data)
 end
 
 
