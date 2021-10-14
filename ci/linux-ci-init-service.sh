7a77b1a8c2b3198714e875f35e07b789b73238ef
tzssangglass <tzssangglass@gmail.com>
2021-10-14 19:23:11 +0800

    test: adapt the kafka v3.0.0-0 in the kafka:latest image
    Signed-off-by: tzssangglass <tzssangglass@gmail.com>

--- ci/linux-ci-init-service.sh
+++ ci/linux-ci-init-service.sh
@@ -16,6 +16,6 @@
 # limitations under the License.
 #
 
-docker exec -i apache-apisix_kafka-server1_1 /opt/bitnami/kafka/bin/kafka-topics.sh --create --zookeeper zookeeper-server1:2181 --replication-factor 1 --partitions 1 --topic test2
-docker exec -i apache-apisix_kafka-server1_1 /opt/bitnami/kafka/bin/kafka-topics.sh --create --zookeeper zookeeper-server1:2181 --replication-factor 1 --partitions 3 --topic test3
-docker exec -i apache-apisix_kafka-server2_1 /opt/bitnami/kafka/bin/kafka-topics.sh --create --zookeeper zookeeper-server2:2181 --replication-factor 1 --partitions 1 --topic test4
+docker exec -i apache-apisix_kafka-server1_1 /opt/bitnami/kafka/bin/kafka-topics.sh --create --bootstrap-server zookeeper-server1:2181 --replication-factor 1 --partitions 1 --topic test2
+docker exec -i apache-apisix_kafka-server1_1 /opt/bitnami/kafka/bin/kafka-topics.sh --create --bootstrap-server zookeeper-server1:2181 --replication-factor 1 --partitions 3 --topic test3
+docker exec -i apache-apisix_kafka-server2_1 /opt/bitnami/kafka/bin/kafka-topics.sh --create --bootstrap-server zookeeper-server2:2181 --replication-factor 1 --partitions 1 --topic test4
