fcab58667b09990c01eecf4c1f7f083e3e1e03fb
Bisakh Mondal <bisakhmondal00@gmail.com>
2021-10-25 02:24:23 +0530

    install_grpcurl bash method

--- ci/centos7-ci.sh
+++ ci/centos7-ci.sh
@@ -58,8 +58,7 @@ install_dependencies() {
     sleep 3
 
     # installing grpcurl
-    wget https://github.com/fullstorydev/grpcurl/releases/download/v1.8.5/grpcurl_1.8.5_linux_x86_64.tar.gz
-    tar -xvf grpcurl_1.8.5_linux_x86_64.tar.gz -C /usr/local/bin
+    install_grpcurl
 
     # install dependencies
     git clone https://github.com/iresty/test-nginx.git test-nginx
--- ci/linux_openresty_common_runner.sh
+++ ci/linux_openresty_common_runner.sh
@@ -58,8 +58,7 @@ do_install() {
     fi
 
     # installing grpcurl
-    wget https://github.com/fullstorydev/grpcurl/releases/download/v1.8.5/grpcurl_1.8.5_linux_x86_64.tar.gz
-    tar -xvf grpcurl_1.8.5_linux_x86_64.tar.gz -C /usr/local/bin
+    install_grpcurl
 }
 
 script() {
