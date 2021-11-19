0acf43ffe8093e4f5291dd5c94c7afc1c4a4320f
bzp2010 <bzp2010@apache.org>
2021-11-19 20:26:16 +0800

    fix: lint

--- apisix/plugins/openwhisk.lua
+++ apisix/plugins/openwhisk.lua
@@ -70,7 +70,8 @@ end
 
 function _M.access(conf, ctx)
     if core.request.get_method() ~= "GET" then
-        if not core.string.has_prefix(core.request.header(ctx, "Content-Type"), "application/json") then
+        local content_type = core.request.header(ctx, "Content-Type")
+        if not core.string.has_prefix(content_type, "application/json") then
             core.log.error("only support json request body")
             return 415
         end
