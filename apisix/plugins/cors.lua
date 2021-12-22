a5b6c0ac4e0961b8f89626c412fef2db1a220cad
jack.fu <jack.fu@yijinin.com>
2021-12-22 23:21:38 +0800

    fix cors bug

--- apisix/plugins/cors.lua
+++ apisix/plugins/cors.lua
@@ -227,6 +227,7 @@ end
 
 
 function _M.rewrite(conf, ctx)
+    ctx["raw-request-origin"] = core.request.header(ctx, "Origin")
     if ctx.var.request_method == "OPTIONS" then
         return 200
     end
@@ -234,7 +235,7 @@ end
 
 
 function _M.header_filter(conf, ctx)
-    local req_origin = core.request.header(ctx, "Origin")
+    local req_origin =  ctx["raw-request-origin"]
     -- Try allow_origins first, if mismatched, try allow_origins_by_regex.
     local allow_origins
     allow_origins = process_with_allow_origins(conf, ctx, req_origin)
