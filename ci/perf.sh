aec24c4888add3dca03515ce73a7ce099eb3611c
tzssangglass <tzssangglass@gmail.com>
2022-01-17 13:03:31 +0800

    add install_stap_tools
    Signed-off-by: tzssangglass <tzssangglass@gmail.com>

--- ci/perf.sh
+++ ci/perf.sh
@@ -19,8 +19,39 @@ install_wrk2() {
     cd ..
 }
 
-run_perf_test() {
+install_stap_tools() {
+    # install ddeb source repo
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C8CAB6595FDFF622
+
+    codename=$(lsb_release -c | awk  '{print $2}')
+    sudo tee /etc/apt/sources.list.d/ddebs.list << EOF
+    deb http://ddebs.ubuntu.com/ ${codename}      main restricted universe multiverse
+    deb http://ddebs.ubuntu.com/ ${codename}-security main restricted universe multiverse
+    deb http://ddebs.ubuntu.com/ ${codename}-updates  main restricted universe multiverse
+    deb http://ddebs.ubuntu.com/ ${codename}-proposed main restricted universe multiverse
+EOF
+
+    sudo apt-get update
+    sudo apt-get install linux-image-$(uname -r)-dbgsym
+    sudo apt install elfutils libdw-dev
     sudo apt-get install -y python3-setuptools python3-wheel
+
+    # install systemtap
+    cd ../
+    wget http://sourceware.org/systemtap/ftp/releases/systemtap-4.6.tar.gz
+    tar -zxf systemtap-4.6.tar.gz
+    mv systemtap-4.6 systemtap
+    cd systemtap
+    ./configure && make all && sudo make install &&  stap --version
+    cd ..
+
+    git clone https://github.com/openresty/stapxx.git
+    git clone https://github.com/openresty/openresty-systemtap-toolkit.git
+    git clone https://github.com/brendangregg/FlameGraph.git
+}
+
+
+run_perf_test() {
     pip3 install -r t/perf/requirements.txt --user
     sudo chmod -R 777 ./
     ulimit -n 10240
@@ -36,6 +67,9 @@ case $case_opt in
         ;;
     (install_wrk2)
         install_wrk2
+        ;;
+    (install_stap_tools)
+        install_stap_tools
         ;;
     (run_perf_test)
         run_perf_test
