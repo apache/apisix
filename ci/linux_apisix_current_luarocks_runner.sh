apache/apisix

commit 73f797d02d2030c2bc6b47ef70fe7b90deb23f46

test: ensure all Lua code is installed (#6192)

ci/linux_apisix_master_luarocks_runner.sh
@@ -30,7 +30,7 @@ script() {
     export_or_prefix
     openresty -V
 
-    sudo rm -rf /usr/local/apisix
+    sudo rm -rf /usr/local/share/lua/5.1/apisix
 
     # run the test case in an empty folder
     mkdir tmp && cd tmp
@@ -38,6 +38,8 @@ script() {
 
     # install APISIX by luarocks
     sudo luarocks install $APISIX_MAIN > build.log 2>&1 || (cat build.log && exit 1)
+    # ensure all files under ../apisix is installed
+    diff -rq ../apisix /usr/local/share/lua/5.1/apisix || exit 1
     cp ../bin/apisix /usr/local/bin/apisix
 
     # show install files

commit 960d4fa38f249840733aaee5114cdbfd223bb39a

test: fix way to check file installation
This bug is introduced in https://github.com/apache/apisix/pull/6192

Signed-off-by: spacewander <spacewanderlzx@gmail.com>

ci/linux_apisix_current_luarocks_runner.sh
@@ -30,11 +30,13 @@ script() {
     export_or_prefix
     openresty -V
 
-    sudo rm -rf /usr/local/apisix
+    sudo rm -rf /usr/local/share/lua/5.1/apisix
 
     # install APISIX with local version
     sudo luarocks install rockspec/apisix-master-0.rockspec --only-deps > build.log 2>&1 || (cat build.log && exit 1)
     sudo luarocks make rockspec/apisix-master-0.rockspec > build.log 2>&1 || (cat build.log && exit 1)
+    # ensure all files under apisix is installed
+    diff -rq apisix /usr/local/share/lua/5.1/apisix
 
     mkdir cli_tmp && cd cli_tmp
 

ci/linux_apisix_master_luarocks_runner.sh
@@ -30,7 +30,7 @@ script() {
     export_or_prefix
     openresty -V
 
-    sudo rm -rf /usr/local/share/lua/5.1/apisix
+    sudo rm -rf /usr/local/apisix
 
     # run the test case in an empty folder
     mkdir tmp && cd tmp
@@ -38,8 +38,6 @@ script() {
 
     # install APISIX by luarocks
     sudo luarocks install $APISIX_MAIN > build.log 2>&1 || (cat build.log && exit 1)
-    # ensure all files under ../apisix is installed
-    diff -rq ../apisix /usr/local/share/lua/5.1/apisix || exit 1
     cp ../bin/apisix /usr/local/bin/apisix
 
     # show install files