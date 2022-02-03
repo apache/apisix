7168d1e5ce39bd7a22e501f4dfce2fb4992ea0ea
Soham Banerjee <63705023+soham4abc@users.noreply.github.com>
2022-02-03 10:55:26 +0530

    Update apisix/plugins/redirect.lua
    Co-authored-by: 罗泽轩 <spacewanderlzx@gmail.com>

--- apisix/plugins/redirect.lua
+++ apisix/plugins/redirect.lua
@@ -151,7 +151,7 @@ function _M.rewrite(conf, ctx)
     local regex_uri = conf.regex_uri
 
     local proxy_proto = core.request.header(ctx, "x-forwarded-proto")
-    local _scheme = proxy_proto and proxy_proto or ctx.var.scheme
+    local _scheme = proxy_proto or ctx.var.scheme
     if conf.http_to_https and _scheme == "http" then
         -- TODO： add test case
         -- PR: https://github.com/apache/apisix/pull/1958
