260ca4d48bad36efb6057697fc4771f5654babc9
Douglas Lee <leeys.top@gmail.com>
2022-03-21 09:49:43 +0800

    fixed: code style
    Co-authored-by: leslie <59061168+leslie-tsang@users.noreply.github.com>

--- apisix/plugins/recaptcha.lua
+++ apisix/plugins/recaptcha.lua
@@ -51,6 +51,7 @@ function _M.check_schema(conf)
 end
 
 
+
 local function retrieve_captcha(ctx, conf)
     if conf.parameter_source == "header" then
         return core.request.header(ctx, conf.parameter_name)
