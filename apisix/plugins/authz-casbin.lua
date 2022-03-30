c900a629172656cc173f6d1af3a7dce4048eff44
高亮亮 <gll287546@alibaba-inc.com>
2022-03-30 09:41:08 +0800

    fix: use core.log

--- apisix/plugins/authz-casbin.lua
+++ apisix/plugins/authz-casbin.lua
@@ -20,7 +20,6 @@ local core            = require("apisix.core")
 local plugin          = require("apisix.plugin")
 local ngx             = ngx
 local get_headers     = ngx.req.get_headers
-local log = core.log
 
 local plugin_name = "authz-casbin"
 
@@ -113,7 +112,7 @@ function _M.rewrite(conf, ctx)
     -- creates an enforcer when request sent for the first time
     local ok, err = new_enforcer_if_need(conf)
     if not ok then
-        log.error(err)
+        core.log.error(err)
         return 503
     end
 
