53a7b635e0eae221ad053ccf86c6bbf58f1f4baa
soulbird <zhaothreee@gmail.com>
2022-04-25 16:03:52 +0800

    chore: replace ngx.req.get_post_args to core.request.get_post_args

--- apisix/plugins/wolf-rbac.lua
+++ apisix/plugins/wolf-rbac.lua
@@ -344,7 +344,7 @@ local function get_args()
             core.log.error("json.decode(", req_body, ") failed! ", err)
         end
     else
-        args = req_get_post_args()
+        args = core.request.get_post_args(ctx)
     end
 
     return args
