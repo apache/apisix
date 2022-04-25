dfe0d53365cb8a689e3816b291b0c47a4c3e0bbe
soulbird <zhaothreee@gmail.com>
2022-04-25 16:10:42 +0800

    fix lint

--- apisix/plugins/wolf-rbac.lua
+++ apisix/plugins/wolf-rbac.lua
@@ -29,7 +29,6 @@ local setmetatable = setmetatable
 local type     = type
 local string   = string
 local req_read_body = ngx.req.read_body
-local req_get_post_args = ngx.req.get_post_args
 local req_get_body_data = ngx.req.get_body_data
 
 local plugin_name = "wolf-rbac"
