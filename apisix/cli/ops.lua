65fefa383031c55ed21ed8ca9671eb34e3894a7b
kwanhur <huang_hua2012@163.com>
2022-05-14 12:13:15 +0800

    feat(ops): use lua libs to backup config file insteadof shell command
    * backup and recover files with os.rename insteadof mv
    * remove files with os.remove insteadof rm
    * link file with lfs.link insteadof ln
    * exist file with penlight.exist insteadof io.open
    
    Signed-off-by: kwanhur <huang_hua2012@163.com>

--- apisix/cli/ops.lua
+++ apisix/cli/ops.lua
@@ -25,6 +25,7 @@ local profile = require("apisix.core.profile")
 local template = require("resty.template")
 local argparse = require("argparse")
 local pl_path = require("pl.path")
+local lfs = require("lfs")
 
 local stderr = io.stderr
 local ipairs = ipairs
@@ -36,6 +37,7 @@ local tonumber = tonumber
 local io_open = io.open
 local execute = os.execute
 local os_rename = os.rename
+local os_remove = os.remove
 local table_insert = table.insert
 local getenv = os.getenv
 local max = math.max
@@ -756,16 +758,19 @@ local function start(env, ...)
     if customized_yaml then
         profile.apisix_home = env.apisix_home .. "/"
         local local_conf_path = profile:yaml_path("config")
+        local local_conf_path_bak = local_conf_path .. ".bak"
 
-        local err = util.execute_cmd_with_error("mv " .. local_conf_path .. " "
-                                                .. local_conf_path .. ".bak")
-        if #err > 0 then
-            util.die("failed to mv config to backup, error: ", err)
+        local ok, err = os_rename(local_conf_path, local_conf_path_bak)
+        if not ok then
+            util.die("failed to backup config, error: ", err)
         end
-        err = util.execute_cmd_with_error("ln " .. customized_yaml .. " " .. local_conf_path)
-        if #err > 0 then
-            util.execute_cmd("mv " .. local_conf_path .. ".bak " .. local_conf_path)
-            util.die("failed to link customized config, error: ", err)
+        local ok, err1 = lfs.link(customized_yaml, local_conf_path)
+        if not ok then
+            ok, err = os_rename(local_conf_path_bak,  local_conf_path)
+            if not ok then
+                util.die("failed to recover original config file, error: ", err)
+            end
+            util.die("failed to link customized config, error: ", err1)
         end
 
         print("Use customized yaml: ", customized_yaml)
@@ -780,15 +785,15 @@ end
 
 local function cleanup()
     local local_conf_path = profile:yaml_path("config")
-    local bak_exist = io_open(local_conf_path .. ".bak")
-    if bak_exist then
-        local err = util.execute_cmd_with_error("rm " .. local_conf_path)
-        if #err > 0 then
+    local local_conf_path_bak = local_conf_path .. ".bak"
+    if pl_path.exists(local_conf_path_bak) then
+        local ok, err = os_remove(local_conf_path)
+        if not ok then
             print("failed to remove customized config, error: ", err)
         end
-        err = util.execute_cmd_with_error("mv " .. local_conf_path .. ".bak " .. local_conf_path)
-        if #err > 0 then
-            util.die("failed to mv original config file, error: ", err)
+        ok, err = os_rename(local_conf_path_bak,  local_conf_path)
+        if not ok then
+            util.die("failed to recover original config file, error: ", err)
         end
     end
 end
@@ -797,9 +802,10 @@ end
 local function test(env, backup_ngx_conf)
     -- backup nginx.conf
     local ngx_conf_path = env.apisix_home .. "/conf/nginx.conf"
-    local ngx_conf_exist = util.is_file_exist(ngx_conf_path)
+    local ngx_conf_path_bak = ngx_conf_path .. ".bak"
+    local ngx_conf_exist = pl_path.exists(ngx_conf_path)
     if ngx_conf_exist then
-        local ok, err = os_rename(ngx_conf_path, ngx_conf_path .. ".bak")
+        local ok, err = os_rename(ngx_conf_path, ngx_conf_path_bak)
         if not ok then
             util.die("failed to backup nginx.conf, error: ", err)
         end
@@ -813,7 +819,7 @@ local function test(env, backup_ngx_conf)
 
     -- restore nginx.conf
     if ngx_conf_exist then
-        local ok, err = os_rename(ngx_conf_path .. ".bak", ngx_conf_path)
+        local ok, err = os_rename(ngx_conf_path_bak, ngx_conf_path)
         if not ok then
             util.die("failed to restore original nginx.conf, error: ", err)
         end
