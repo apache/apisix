a5671a1cdabae01f4885383ac3d3dbebf5f06e33
chengzw <chengzw258@163.com>
2022-05-20 16:45:44 +0800

    fix install dependencies issues

--- utils/linux-install-luarocks.sh
+++ utils/linux-install-luarocks.sh
@@ -53,5 +53,12 @@ if [ -d ${OPENRESTY_PREFIX}/openssl111 ]; then
     OPENSSL_PREFIX=${OPENRESTY_PREFIX}/openssl111
 fi
 
+FOUND_PATH=$(echo $PATH | grep -oP '(?<=:|)/usr/local/bin(?=:|)') || true
+if [[ "${FOUND_PATH}" == "" ]]; then
+   echo "the path /usr/local/bin is not included in the system default PATH variable, add PATH variable in ~/.bashrc."
+   echo "export PATH=$PATH:/usr/local/bin" >> ~/.bashrc
+   source ~/.bashrc
+fi
+
 luarocks config variables.OPENSSL_LIBDIR ${OPENSSL_PREFIX}/lib
 luarocks config variables.OPENSSL_INCDIR ${OPENSSL_PREFIX}/include
