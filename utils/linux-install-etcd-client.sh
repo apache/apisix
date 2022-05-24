54445c206d5057e1d19396bbad08d9ce79f52bac
soulbird <zhaoshir@outlook.com>
2022-05-24 09:01:03 +0800

    chore(script): support to install dependencies under arm64 (#7091)

--- utils/linux-install-etcd-client.sh
+++ utils/linux-install-etcd-client.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 #
 # Licensed to the Apache Software Foundation (ASF) under one or more
@@ -17,7 +17,15 @@
 # limitations under the License.
 #
 
-wget https://github.com/etcd-io/etcd/releases/download/v3.4.18/etcd-v3.4.18-linux-amd64.tar.gz
-tar xf etcd-v3.4.18-linux-amd64.tar.gz
-sudo cp etcd-v3.4.18-linux-amd64/etcdctl /usr/local/bin/
-rm -rf etcd-v3.4.18-linux-amd64
+ETCD_ARCH="amd64"
+ETCD_VERSION=${ETCD_VERSION:-'3.4.18'}
+ARCH=${ARCH:-`(uname -m | tr '[:upper:]' '[:lower:]')`}
+
+if [[ $ARCH == "arm64" ]] || [[ $ARCH == "aarch64" ]]; then
+    ETCD_ARCH="arm64"
+fi
+
+wget https://github.com/etcd-io/etcd/releases/download/v${ETCD_VERSION}/etcd-v3.4.18-linux-${ETCD_ARCH}.tar.gz
+tar xf etcd-v${ETCD_VERSION}-linux-${ETCD_ARCH}.tar.gz
+sudo cp etcd-v${ETCD_VERSION}-linux-${ETCD_ARCH}/etcdctl /usr/local/bin/
+rm -rf etcd-v${ETCD_VERSION}-linux-${ETCD_ARCH}
