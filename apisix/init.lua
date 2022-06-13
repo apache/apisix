63aaef8beb32d2a10f5d9b8f8dbd34ecf9d3c8af
soulbird <zhaoshir@outlook.com>
2022-06-13 16:52:51 +0800

    fix: duplicate X-Forwarded-Proto will be sent as string (#7229)
    Co-authored-by: soulbird <zhaothree@gmail.com>

--- apisix/init.lua
+++ apisix/init.lua
@@ -55,6 +55,7 @@ local str_byte        = string.byte
 local str_sub         = string.sub
 local tonumber        = tonumber
 local pairs           = pairs
+local type            = type
 local control_api_router
 
 local is_http = false
@@ -277,7 +278,11 @@ local function set_upstream_headers(api_ctx, picked_server)
 
     local hdr = core.request.header(api_ctx, "X-Forwarded-Proto")
     if hdr then
-        api_ctx.var.var_x_forwarded_proto = hdr
+        if type(hdr) == "table" then
+            api_ctx.var.var_x_forwarded_proto = hdr[1]
+        else
+            api_ctx.var.var_x_forwarded_proto = hdr
+        end
     end
 end
 
