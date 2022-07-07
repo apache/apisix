869e801d6dc2e6dcbacad6dd0486b552c8c2678b
罗泽轩 <spacewanderlzx@gmail.com>
2022-07-07 16:09:12 +0800

    feat(deployment): control plane connects to conf server (#7392)
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>

--- t/cli/test_deployment_control_plane_customed_nginx.sh
+++ t/cli/test_deployment_control_plane_customed_nginx.sh
@@ -19,61 +19,9 @@
 
 . ./t/cli/common.sh
 
-echo '
-deployment:
-    role: control_plane
-    role_control_plane:
-        config_provider: etcd
-        conf_server:
-            cert: t/certs/mtls_server.crt
-    etcd:
-        prefix: "/apisix"
-        host:
-            - http://127.0.0.1:2379
-' > conf/config.yaml
+exit_if_not_customed_nginx
 
-out=$(make init 2>&1 || true)
-if ! echo "$out" | grep 'property "cert_key" is required'; then
-    echo "failed: should check deployment schema during init"
-    exit 1
-fi
-
-echo "passed: should check deployment schema during init"
-
-# The 'admin.apisix.dev' is injected by ci/common.sh@set_coredns
-echo '
-apisix:
-    enable_admin: false
-deployment:
-    role: control_plane
-    role_control_plane:
-        config_provider: etcd
-        conf_server:
-            listen: admin.apisix.dev:12345
-            cert: t/certs/mtls_server.crt
-            cert_key: t/certs/mtls_server.key
-    etcd:
-        prefix: "/apisix"
-        host:
-            - http://127.0.0.1:2379
-    certs:
-        trusted_ca_cert: t/certs/mtls_ca.crt
-' > conf/config.yaml
-
-make run
-sleep 1
-
-code=$(curl -o /dev/null -s -w %{http_code} http://127.0.0.1:9080/apisix/admin/routes -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1')
-make stop
-
-if [ ! $code -eq 200 ]; then
-    echo "failed: control_plane should enable Admin API"
-    exit 1
-fi
-
-echo "passed: control_plane should enable Admin API"
-
-# use https
+# use mTLS
 # The 'admin.apisix.dev' is injected by ci/common.sh@set_coredns
 echo '
 deployment:
@@ -84,6 +32,7 @@ deployment:
             listen: admin.apisix.dev:12345
             cert: t/certs/mtls_server.crt
             cert_key: t/certs/mtls_server.key
+            client_ca_cert: t/certs/mtls_ca.crt
     etcd:
         prefix: "/apisix"
         host:
