cc57904478030f75fa75d63184508c24013cf866
罗泽轩 <spacewanderlzx@gmail.com>
2022-07-14 11:12:28 +0800

    feat(deployment): support standalone (#7405)
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>

--- t/cli/test_standalone.sh
+++ t/cli/test_standalone.sh
@@ -65,3 +65,46 @@ if [ ! $code -eq 200 ]; then
 fi
 
 echo "passed: resolve variables in apisix.yaml conf success"
+
+# configure standalone via deployment
+echo '
+deployment:
+    role: data_plane
+    role_data_plane:
+       config_provider: yaml
+' > conf/config.yaml
+
+var_test_path=/test make run
+sleep 0.1
+code=$(curl -o /dev/null -s -m 5 -w %{http_code} http://127.0.0.1:9080/apisix/admin/routes)
+if [ ! $code -eq 404 ]; then
+    echo "failed: admin API should be disabled automatically"
+    exit 1
+fi
+
+echo "passed: admin API should be disabled automatically"
+
+# support environment variables
+echo '
+routes:
+  -
+    uri: ${{var_test_path}}
+    plugins:
+      proxy-rewrite:
+        uri: ${{var_test_proxy_rewrite_uri:=/apisix/nginx_status}}
+    upstream:
+      nodes:
+        "127.0.0.1:9091": 1
+      type: roundrobin
+#END
+' > conf/apisix.yaml
+
+var_test_path=/test make run
+sleep 0.1
+code=$(curl -o /dev/null -s -m 5 -w %{http_code} http://127.0.0.1:9080/test)
+if [ ! $code -eq 200 ]; then
+    echo "failed: resolve variables in apisix.yaml conf failed"
+    exit 1
+fi
+
+echo "passed: resolve variables in apisix.yaml conf success"
