69ee6519bd95b66c6b03334c8477900136b318dc
LCW <Lcw769193516@163.com>
2022-08-09 14:10:44 +0800

    ci: fix openfunction ci params

--- ci/init-plugin-test-service.sh
+++ ci/init-plugin-test-service.sh
@@ -80,8 +80,8 @@ create_functions() {
     kubectl apply -f function-sample.yaml
     kubectl apply -f function-sample-test-body.yaml
 
-    kubectl set resources deployment -n openfunction --all=true --requests=cpu=40m,memory=64Mi
-    kubectl set resources deployment -n kube-system --all=true --requests=cpu=50m,memory=64Mi
+    kubectl set resources deployment -n openfunction --all=true --requests=cpu=40m,memory=100Mi
+    kubectl set resources deployment -n kube-system --all=true --requests=cpu=40m,memory=100Mi
 
     kubectl wait fn function-sample --for=jsonpath='{.status.build.state}'=Succeeded  --timeout=500s
     kubectl wait fn function-sample --for=jsonpath='{.status.build.state}'=Running  --timeout=500s
