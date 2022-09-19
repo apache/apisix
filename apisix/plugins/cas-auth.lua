3d4dab41728a0dfab6f66e29f6e523d80b01c609
kingluo <home_king@163.com>
2022-09-19 15:49:47 +0800

    fix PR

--- apisix/plugins/cas-auth.lua
+++ apisix/plugins/cas-auth.lua
@@ -54,51 +54,36 @@ function _M.check_schema(conf)
     return core.schema.check(schema, conf)
 end
 
-local function to_table(v)
-    if v == nil then
-        return {}
-    elseif type(v) == "table" then
-        return v
-    else
-        return {v}
-    end
+local function uri_without_ticket(conf, ctx)
+    return ctx.var.scheme .. "://" .. ctx.var.host .. ":" ..
+        ctx.var.server_port .. conf.cas_callback_uri
 end
 
-local function set_cookie(cookie_str)
-    local h = to_table(ngx.header['Set-Cookie'])
-    table.insert(h, cookie_str)
-    ngx.header['Set-Cookie'] = h
-end
-
-local function uri_without_ticket(conf)
-    return ngx.var.scheme .. "://" .. ngx.var.host .. ":" ..
-        ngx.var.server_port .. conf.cas_callback_uri
-end
-
-local function get_session_id()
-    return ngx.var["cookie_" .. COOKIE_NAME]
+local function get_session_id(ctx)
+    return ctx.var["cookie_" .. COOKIE_NAME]
 end
 
 local function set_our_cookie(name, val)
-    set_cookie(name .. "=" .. val .. COOKIE_PARAMS)
+    core.response.add_header("Set-Cookie", name .. "=" .. val .. COOKIE_PARAMS)
 end
 
-local function first_access(conf)
+local function first_access(conf, ctx)
     local login_uri = conf.idp_uri .. "/login?" ..
-        ngx.encode_args({ service = uri_without_ticket(conf) })
-    ngx.log(ngx.INFO, "first access: ", login_uri,
-        ", cookie: ", ngx.var.http_cookie, ", request_uri: ", ngx.var.request_uri)
-    set_our_cookie(CAS_REQUEST_URI, ngx.var.request_uri)
-    ngx.redirect(login_uri, ngx.HTTP_MOVED_TEMPORARILY)
+        ngx.encode_args({ service = uri_without_ticket(conf, ctx) })
+    core.log.info("first access: ", login_uri,
+        ", cookie: ", ctx.var.http_cookie, ", request_uri: ", ctx.var.request_uri)
+    set_our_cookie(CAS_REQUEST_URI, ctx.var.request_uri)
+    core.response.set_header("Location", login_uri)
+    return ngx.HTTP_MOVED_TEMPORARILY
 end
 
-local function with_session_id(conf, session_id)
+local function with_session_id(conf, ctx, session_id)
     -- does the cookie exist in our store?
     local user = store:get(session_id);
-    ngx.log(ngx.INFO, "ticket=", session_id, ", user=", user)
+    core.log.info("ticket=", session_id, ", user=", user)
     if user == nil then
         set_our_cookie(COOKIE_NAME, "deleted; Max-Age=0")
-        first_access(conf)
+        return first_access(conf, ctx)
     else
         -- refresh the TTL
         store:set(session_id, user, SESSION_LIFETIME)
@@ -110,72 +95,74 @@ local function set_store_and_cookie(session_id, user)
     local success, err, forcible = store:add(session_id, user, SESSION_LIFETIME)
     if success then
         if forcible then
-            ngx.log(ngx.INFO, "CAS cookie store is out of memory")
+            core.log.info("CAS cookie store is out of memory")
         end
         set_our_cookie(COOKIE_NAME, session_id)
     else
         if err == "no memory" then
-            ngx.log(ngx.EMERG, "CAS cookie store is out of memory")
+            core.log.emerg("CAS cookie store is out of memory")
         elseif err == "exists" then
-            ngx.log(ngx.ERR, "Same CAS ticket validated twice, this should never happen!")
+            core.log.error("Same CAS ticket validated twice, this should never happen!")
         end
     end
     return success
 end
 
-local function validate(conf, ticket)
+local function validate(conf, ctx, ticket)
     -- send a request to CAS to validate the ticket
     local httpc = http.new()
     local res, err = httpc:request_uri(conf.idp_uri ..
-        "/serviceValidate", { query = { ticket = ticket, service = uri_without_ticket(conf) } })
+        "/serviceValidate", { query = { ticket = ticket, service = uri_without_ticket(conf, ctx) } })
 
     if res and res.status == ngx.HTTP_OK and res.body ~= nil then
-        if string.find(res.body, "<cas:authenticationSuccess>") then
+        if core.string.find(res.body, "<cas:authenticationSuccess>") then
             local m = ngx.re.match(res.body, "<cas:user>(.*?)</cas:user>");
             if m then
                 return m[1]
             end
         else
-            ngx.log(ngx.INFO, "CAS serviceValidate failed: " .. res.body)
+            core.log.info("CAS serviceValidate failed: ", res.body)
         end
     else
-        ngx.log(ngx.ERR, "validate ticket failed: res=", res, ", err=", err)
+        core.log.error("validate ticket failed: res=", res, ", err=", err)
     end
     return nil
 end
 
-local function validate_with_cas(conf, ticket)
-    local user = validate(conf, ticket)
+local function validate_with_cas(conf, ctx, ticket)
+    local user = validate(conf, ctx, ticket)
     if user and set_store_and_cookie(ticket, user) then
-        local request_uri = ngx.var["cookie_" .. CAS_REQUEST_URI]
+        local request_uri = ctx.var["cookie_" .. CAS_REQUEST_URI]
         set_our_cookie(CAS_REQUEST_URI, "deleted; Max-Age=0")
-        ngx.log(ngx.INFO, "ticket: ", ticket,
-            ", cookie: ", ngx.var.http_cookie, ", request_uri: ", request_uri, ", user=", user)
-        ngx.redirect(request_uri, ngx.HTTP_MOVED_TEMPORARILY)
+        core.log.info("ticket: ", ticket,
+            ", cookie: ", ctx.var.http_cookie, ", request_uri: ", request_uri, ", user=", user)
+        core.response.set_header("Location", request_uri)
+        return ngx.HTTP_MOVED_TEMPORARILY
     else
         return ngx.HTTP_UNAUTHORIZED, {message = "invalid ticket"}
     end
 end
 
-local function logout(conf)
-    local session_id = get_session_id()
+local function logout(conf, ctx)
+    local session_id = get_session_id(ctx)
     if session_id == nil then
         return ngx.HTTP_UNAUTHORIZED
     end
 
-    ngx.log(ngx.INFO, "logout: ticket=", session_id, ", cookie=", ngx.var.http_cookie)
+    core.log.info("logout: ticket=", session_id, ", cookie=", ctx.var.http_cookie)
     store:delete(session_id)
     set_our_cookie(COOKIE_NAME, "deleted; Max-Age=0")
 
-    ngx.redirect(conf.idp_uri .. "/logout")
+    core.response.set_header("Location", conf.idp_uri .. "/logout")
+    return ngx.HTTP_MOVED_TEMPORARILY
 end
 
 function _M.access(conf, ctx)
     local method = ngx.req.get_method()
-    local uri = ngx.var.uri
+    local uri = ctx.var.uri
 
     if method == "GET" and uri == conf.logout_uri then
-        return logout(conf)
+        return logout(conf, ctx)
     elseif method == "POST" and uri == conf.cas_callback_uri then
         ngx.req.read_body()
         local data = ngx.req.get_body_data()
@@ -183,24 +170,24 @@ function _M.access(conf, ctx)
         if ticket == nil then
             return 400, {message = "invalid logout request from IdP, no ticket"}
         end
-        ngx.log(ngx.INFO, "Back-channel logout (SLO) from IdP: LogoutRequest: ", data)
+        core.log.info("Back-channel logout (SLO) from IdP: LogoutRequest: ", data)
         local session_id = ticket
         local user = store:get(session_id);
         if user then
             store:delete(session_id)
-            ngx.log(ngx.INFO, "SLO: user=", user, ", tocket=", ticket)
+            core.log.info("SLO: user=", user, ", tocket=", ticket)
         end
     else
-        local session_id = get_session_id()
+        local session_id = get_session_id(ctx)
         if session_id ~= nil then
-            return with_session_id(conf, session_id)
+            return with_session_id(conf, ctx, session_id)
         end
 
-        local ticket = ngx.var.arg_ticket
+        local ticket = ctx.var.arg_ticket
         if ticket ~= nil and uri == conf.cas_callback_uri then
-            return validate_with_cas(conf, ticket)
+            return validate_with_cas(conf, ctx, ticket)
         else
-            first_access(conf)
+            return first_access(conf, ctx)
         end
     end
 end
