ac7713b4c0647fbc33baab3b221f7a71be737f34
levy liu <375636559@qq.com>
2022-09-23 13:03:22 +0800

    feat: support ssl key-encrypt-salt rotation (#7925)

--- apisix/cli/schema.lua
+++ apisix/cli/schema.lua
@@ -219,7 +219,25 @@ local config_schema = {
                                     }
                                 }
                             }
-                        }
+                        },
+                        key_encrypt_salt = {
+                            anyOf = {
+                                {
+                                    type = "array",
+                                    minItems = 1,
+                                    items = {
+                                        type = "string",
+                                        minLength = 16,
+                                        maxLength = 16
+                                    }
+                                },
+                                {
+                                    type = "string",
+                                    minLength = 16,
+                                    maxLength = 16
+                                }
+                            }
+                        },
                     }
                 },
             }
