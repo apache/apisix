2ab132d5dc283b94c0ea1787a985bc26856d130e
monkeyDluffy6017 <375636559@qq.com>
2022-09-23 10:31:11 +0800

    fix: fix code lint error

--- apisix/core/config_local.lua
+++ apisix/core/config_local.lua
@@ -19,8 +19,9 @@
 --
 -- @module core.config_local
 
-local file = require("apisix.cli.file")
+local file   = require("apisix.cli.file")
 local schema = require("apisix.cli.schema")
+local error  = error
 
 
 local _M = {}
