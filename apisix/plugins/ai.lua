6c7ae0bae497d1643b03eb3e917bc30abcf2787d
kingluo <home_king@163.com>
2022-10-20 11:47:50 +0800

    fix(ai): use # to concat route cache key parts

--- apisix/plugins/ai.lua
+++ apisix/plugins/ai.lua
@@ -31,10 +31,10 @@ return function(ctx)
     local var = ctx.var
     return var.uri
         {% if route_flags["methods"] then %}
-        .. "\0" .. var.method
+        .. "#" .. var.method
         {% end %}
         {% if route_flags["host"] then %}
-        .. "\0" .. var.host
+        .. "#" .. var.host
         {% end %}
 end
 ]]
@@ -68,7 +68,7 @@ end
 
 local function ai_match(ctx)
     local key = get_cache_key_func(ctx)
-    core.log.info("route cache key: ", core.log.delay_exec(encode_base64, key))
+    core.log.info("route cache key: ", key)
     local ver = router.router_http.user_routes.conf_version
     local route_cache = route_lrucache(key, ver,
                                        match_route, ctx)
