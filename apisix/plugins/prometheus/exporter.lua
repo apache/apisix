011ab0cdb34da94f9f68f90e5292d49adcee1778
tzssangglass <tzssangglass@gmail.com>
2022-12-04 20:39:23 +0800

    revert exporter

--- apisix/plugins/prometheus/exporter.lua
+++ apisix/plugins/prometheus/exporter.lua
@@ -14,7 +14,6 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 --
-local require   = require
 local base_prometheus = require("prometheus")
 local core      = require("apisix.core")
 local plugin    = require("apisix.plugin")
