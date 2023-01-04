73f239f6f458b88f8dda5400db9206f4f8cf4690
paul-unifra <paul@unifra.io>
2023-01-04 21:30:49 +0000

    feat(calculate-cu): cu calculator based on jsonrpc method

---
+++ apisix/plugins/calculate-cu.lua
@@ -0,0 +1,46 @@
+local core = require "apisix.core"
+
+local plugin_name = "calculate-cu"
+
+local schema = {
+    type = "object",
+    properties = {
+        methods = {
+            type = "object",
+            patternProperties = {
+                ["^[a-zA-Z_][a-zA-Z0-9_]*$"] = { type = "integer", minimum = 1 }
+            }
+        }
+    },
+    required = { "methods" },
+}
+
+local _M = {
+    version = 0.1,
+    priority = 1012,
+    name = plugin_name,
+    schema = schema,
+}
+
+function _M.access(conf, ctx)
+    -- Check if it's a batch request
+    local method = ctx.var["jsonrpc_method"]
+    if method == "batch" then
+        -- Calculate cu for each method in the batch request
+        local methods = ctx.var["jsonrpc_methods"]
+        if methods then
+            local cu = 0
+            for _, m in ipairs(methods) do
+                cu = cu + (conf.methods[m] or 1)
+            end
+            ctx.var["cu"] = cu
+        else
+            return 400, { error_msg = "Invalid JSON-RPC request" }
+        end
+    else
+        -- Calculate cu for a single request
+        ctx.var["cu"] = conf.methods[method] or 1
+    end
+end
+
+return _M
