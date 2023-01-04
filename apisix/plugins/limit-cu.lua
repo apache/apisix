993f25f4540978c088650ab0d99a1b6c5b14a099
paul-unifra <paul@unifra.io>
2023-01-04 21:30:14 +0000

    feat(limit-cu): rate limiter by cu, support batch requests

---
+++ apisix/plugins/limit-cu.lua
@@ -0,0 +1,36 @@
+--
+-- Licensed to the Apache Software Foundation (ASF) under one or more
+-- contributor license agreements.  See the NOTICE file distributed with
+-- this work for additional information regarding copyright ownership.
+-- The ASF licenses this file to You under the Apache License, Version 2.0
+-- (the "License"); you may not use this file except in compliance with
+-- the License.  You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+local limit_cu = require("apisix.plugins.limit-cu.init")
+
+local plugin_name = "limit-cu"
+local _M = {
+    version = 0.4,
+    priority = 1010,
+    name = plugin_name,
+    schema = limit_cu.schema,
+}
+
+
+function _M.check_schema(conf)
+    return limit_cu.check_schema(conf)
+end
+
+function _M.access(conf, ctx)
+    return limit_cu.rate_limit(conf, ctx)
+end
+
+return _M
