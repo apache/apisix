bca13a388f8f511d59e128122594e5ca5712383c
Joanthan Chen <mscb402@gmail.com>
2023-01-10 13:51:39 +0800

    feat: limit count plugin support X-RateLimit-Reset (#8578)
    Fixes https://github.com/apache/apisix/issues/8381

--- apisix/cli/ngx_tpl.lua
+++ apisix/cli/ngx_tpl.lua
@@ -276,6 +276,7 @@ http {
     {% if enabled_plugins["limit-count"] then %}
     lua_shared_dict plugin-limit-count {* http.lua_shared_dict["plugin-limit-count"] *};
     lua_shared_dict plugin-limit-count-redis-cluster-slot-lock {* http.lua_shared_dict["plugin-limit-count-redis-cluster-slot-lock"] *};
+    lua_shared_dict plugin-limit-count-reset-header {* http.lua_shared_dict["plugin-limit-count"] *};
     {% end %}
 
     {% if enabled_plugins["prometheus"] and not enabled_stream_plugins["prometheus"] then %}
