fd488dc1d8888facc46b635a419981508c2b5460
paul-unifra <paul@unifra.io>
2023-01-11 18:56:38 +0000

    feat(limit-monthly-cu): let schema be variable, so the plugin can be global/service level, variable shoud set by consumer.

--- apisix/plugins/limit-monthly-cu.lua
+++ apisix/plugins/limit-monthly-cu.lua
@@ -1,10 +1,12 @@
+local core = require("apisix.core")
+
 local plugin_name = "limit-monthly-cu"
 
 local schema = {
     type = "object",
     properties = {
-        monthly_quota = { type = "integer", minimum = 0, default = 100000 },
-        monthly_used = { type = "integer", minimum = 0, default = 0 },
+        monthly_quota = { type = "string", default = "$monthly_quota" },
+        monthly_used = { type = "string", default = "$monthly_used" },
     },
     required = { "monthly_quota", "monthly_used" }
 }
@@ -16,9 +18,30 @@ local _M = {
     schema = schema,
 }
 
+function _M.check_schema(conf)
+    local ok, err = core.schema.check(schema, conf)
+    if not ok then
+        return false, err
+    end
+
+    if conf.monthly_quota then
+        if conf.monthly_quota:byte(1, 1) ~= string.byte("$") then
+            return false, "monthly_quota is a variable, it must start with $"
+        end
+    end
+
+    if conf.monthly_used then
+        if conf.monthly_used:byte(1, 1) ~= string.byte("$") then
+            return false, "monthly_used is a variable, it must start with $"
+        end
+    end
+    return true
+end
 
 function _M.access(conf, ctx)
-    if conf.monthly_quota <= conf.monthly_used then
+    local monthly_quota = tonumber(ctx.var[conf.monthly_quota:sub(2)])
+    local monthly_used = tonumber(ctx.var[conf.monthly_used:sub(2)])
+    if monthly_quota <= monthly_used then
         return 429, { error_msg = "quota exceeded" }
     end
 end
