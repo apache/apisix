c0de1d2e19c53ec694ea6c5df2c12602aa73bd62
paul-unifra <paul@unifra.io>
2023-01-14 21:10:54 +0000

    fix: incorrect custom gzip resp_body

--- apisix/utils/log-util.lua
+++ apisix/utils/log-util.lua
@@ -261,10 +261,7 @@ function _M.collect_body(conf, ctx)
             local resp_headers = ngx.resp.get_headers()
             local encoding = resp_headers["Content-Encoding"]
             if encoding == "gzip" then
-                ctx.var.resp_body = {
-                    jsonrpc = "2.0",
-                    result = "gzip body"
-                }
+                ctx.var.resp_body = '{ "jsonrpc": "2.0", "result": "gzip body" }'
             else
                 ctx.var.resp_body = final_body
             end
