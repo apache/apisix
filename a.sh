0fbecf42b38f47a1c2465852430d7eb64284635d
Abhishek Choudhary <shreemaan.abhishek@gmail.com>
2023-01-24 23:15:17 +0530

    sfasd

--- a.sh
+++ a.sh
@@ -4,7 +4,7 @@ all_tags="redis:latest 122 34234 test-body-image test-uri-image"
 to_pull=""
           for tag in $all_tags
           do
-          if ! ( docker inspect $tag )
+          if ! ( docker inspect $tag &> /dev/null)
           then
           # echo $? : $tag
           to_pull="${to_pull} ${tag}"
