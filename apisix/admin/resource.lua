1f644252ff011f55c27eb9c5597f60964342697d
dongjunduo <andj4cn@gmail.com>
2023-03-01 17:49:08 +0800

    refactor(admin): secrets and ssls (#8911)

--- apisix/admin/resource.lua
+++ apisix/admin/resource.lua
@@ -16,6 +16,7 @@
 --
 local core = require("apisix.core")
 local utils = require("apisix.admin.utils")
+local apisix_ssl = require("apisix.ssl")
 local setmetatable = setmetatable
 local tostring = tostring
 local type = type
@@ -37,7 +38,21 @@ local no_id_res = {
 }
 
 
-function _M:check_conf(id, conf, need_id)
+local function split_typ_and_id(id, sub_path)
+    local uri_segs = core.utils.split_uri(sub_path)
+    local typ = id
+    local id = nil
+    if #uri_segs > 0 then
+        id = uri_segs[1]
+    end
+    return typ, id
+end
+
+
+function _M:check_conf(id, conf, need_id, typ)
+    if self.name == "secrets" then
+        id = typ .. "/" .. id
+    end
     -- check if missing configurations
     if not conf then
         return nil, {error_msg = "missing configurations"}
@@ -61,11 +76,14 @@ function _M:check_conf(id, conf, need_id)
         conf.id = id
     end
 
-    core.log.info("schema: ", core.json.delay_encode(self.schema))
     core.log.info("conf  : ", core.json.delay_encode(conf))
 
     -- check the resource own rules
-    local ok, err = self.checker(id, conf, need_id, self.schema)
+    if self.name ~= "secrets" then
+        core.log.info("schema: ", core.json.delay_encode(self.schema))
+    end
+
+    local ok, err = self.checker(id, conf, need_id, self.schema, typ)
 
     if not ok then
         return ok, err
@@ -79,13 +97,22 @@ function _M:check_conf(id, conf, need_id)
 end
 
 
-function _M:get(id)
+function _M:get(id, conf, sub_path)
     if core.table.array_find(self.unsupported_methods, "get") then
         return 405, {error_msg = "not supported `GET` method for " .. self.kind}
     end
 
     local key = "/" .. self.name
+    local typ = nil
+    if self.name == "secrets" then
+        key = key .. "/"
+        typ, id = split_typ_and_id(id, sub_path)
+    end
+
     if id then
+        if self.name == "secrets" then
+            key = key .. typ
+        end
         key = key .. "/" .. id
     end
 
@@ -93,6 +120,13 @@ function _M:get(id)
     if not res then
         core.log.error("failed to get ", self.kind, "[", key, "] from etcd: ", err)
         return 503, {error_msg = err}
+    end
+
+    if self.name == "ssls" then
+        -- not return private key for security
+        if res.body and res.body.node and res.body.node.value then
+            res.body.node.value.key = nil
+        end
     end
 
     utils.fix_count(res.body, id)
@@ -108,6 +142,17 @@ function _M:post(id, conf, sub_path, args)
     local id, err = self:check_conf(id, conf, false)
     if not id then
         return 400, err
+    end
+
+    if self.name == "ssls" then
+        -- encrypt private key
+        conf.key = apisix_ssl.aes_encrypt_pkey(conf.key)
+
+        if conf.keys then
+            for i = 1, #conf.keys do
+                conf.keys[i] = apisix_ssl.aes_encrypt_pkey(conf.keys[i])
+            end
+        end
     end
 
     local key = "/" .. self.name
@@ -133,13 +178,35 @@ function _M:put(id, conf, sub_path, args)
         return 405, {error_msg = "not supported `PUT` method for " .. self.kind}
     end
 
+    local key = "/" .. self.name
+    local typ = nil
+    if self.name == "secrets" then
+        typ, id = split_typ_and_id(id, sub_path)
+        key = key .. "/" .. typ
+    end
+
     local need_id = not no_id_res[self.name]
-    local id, err = self:check_conf(id, conf, need_id)
-    if not id then
+    local ok, err = self:check_conf(id, conf, need_id, typ)
+    if not ok then
         return 400, err
     end
 
-    local key = "/" .. self.name .. "/" .. id
+    if self.name ~= "secrets" then
+        id = ok
+    end
+
+    if self.name == "ssls" then
+        -- encrypt private key
+        conf.key = apisix_ssl.aes_encrypt_pkey(conf.key)
+
+        if conf.keys then
+            for i = 1, #conf.keys do
+                conf.keys[i] = apisix_ssl.aes_encrypt_pkey(conf.keys[i])
+            end
+        end
+    end
+
+    key = key .. "/" .. id
 
     if self.name ~= "plugin_metadata" then
         local ok, err = utils.inject_conf_with_prev_conf(self.kind, key, conf)
@@ -162,15 +229,29 @@ function _M:put(id, conf, sub_path, args)
     return res.status, res.body
 end
 
-
-function _M:delete(id)
+-- Keep the unused conf to make the args list consistent with other methods
+function _M:delete(id, conf, sub_path)
     if core.table.array_find(self.unsupported_methods, "delete") then
         return 405, {error_msg = "not supported `DELETE` method for " .. self.kind}
+    end
+
+    local key = "/" .. self.name
+    local typ = nil
+    if self.name == "secrets" then
+        typ, id = split_typ_and_id(id, sub_path)
     end
 
     if not id then
         return 400, {error_msg = "missing " .. self.kind .. " id"}
     end
+
+    -- core.log.error("failed to delete ", self.kind, "[", key, "] in etcd: ", err)
+
+    if self.name == "secrets" then
+        key = key .. "/" .. typ
+    end
+
+    key = key .. "/" .. id
 
     if self.delete_checker then
         local code, err = self.delete_checker(id)
@@ -179,7 +260,6 @@ function _M:delete(id)
         end
     end
 
-    local key = "/" .. self.name .. "/" .. id
     local res, err = core.etcd.delete(key)
     if not res then
         core.log.error("failed to delete ", self.kind, "[", key, "] in etcd: ", err)
@@ -195,9 +275,27 @@ function _M:patch(id, conf, sub_path, args)
         return 405, {error_msg = "not supported `PATCH` method for " .. self.kind}
     end
 
+    local key = "/" .. self.name
+    local typ = nil
+    if self.name == "secrets" then
+        local uri_segs = core.utils.split_uri(sub_path)
+        if #uri_segs < 2 then
+            return 400, {error_msg = "no secret id and/or sub path in uri"}
+        end
+        typ = id
+        id = uri_segs[1]
+        sub_path = core.table.concat(uri_segs, "/", 2)
+    end
+
     if not id then
         return 400, {error_msg = "missing " .. self.kind .. " id"}
     end
+
+    if self.name == "secrets" then
+        key = key .. "/" .. typ
+    end
+
+    key = key .. "/" .. id
 
     if conf == nil then
         return 400, {error_msg = "missing new configuration"}
@@ -207,11 +305,6 @@ function _M:patch(id, conf, sub_path, args)
         if type(conf) ~= "table"  then
             return 400, {error_msg = "invalid configuration"}
         end
-    end
-
-    local key = "/" .. self.name
-    if id then
-        key = key .. "/" .. id
     end
 
     local res_old, err = core.etcd.get(key)
@@ -229,6 +322,15 @@ function _M:patch(id, conf, sub_path, args)
     local modified_index = res_old.body.node.modifiedIndex
 
     if sub_path and sub_path ~= "" then
+        if self.name == "ssls" then
+            if sub_path == "key" then
+                conf = apisix_ssl.aes_encrypt_pkey(conf)
+            elseif sub_path == "keys" then
+                for i = 1, #conf do
+                    conf[i] = apisix_ssl.aes_encrypt_pkey(conf[i])
+                end
+            end
+        end
         local code, err, node_val = core.table.patch(node_value, sub_path, conf)
         node_value = node_val
         if code then
@@ -236,14 +338,25 @@ function _M:patch(id, conf, sub_path, args)
         end
         utils.inject_timestamp(node_value, nil, true)
     else
+        if self.name == "ssls" then
+            if conf.key then
+                conf.key = apisix_ssl.aes_encrypt_pkey(conf.key)
+            end
+
+            if conf.keys then
+                for i = 1, #conf.keys do
+                    conf.keys[i] = apisix_ssl.aes_encrypt_pkey(conf.keys[i])
+                end
+            end
+        end
         node_value = core.table.merge(node_value, conf)
         utils.inject_timestamp(node_value, nil, conf)
     end
 
     core.log.info("new conf: ", core.json.delay_encode(node_value, true))
 
-    local id, err = self:check_conf(id, node_value, true)
-    if not id then
+    local ok, err = self:check_conf(id, node_value, true, typ)
+    if not ok then
         return 400, err
     end
 
