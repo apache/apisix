ac51fa47a3aa440e221db112977630157b14eb01
levy liu <monkeydluffy6017@gmail.com>
2023-03-09 11:06:44 +0800

    refactor: more detailed log info

--- apisix/core/config_util.lua
+++ apisix/core/config_util.lua
@@ -95,7 +95,7 @@ function _M.cancel_clean_handler(item, idx, fire)
     if f then
         f(item)
     else
-        log.error("call cancel_clean_handler error, f is nil")
+        log.error("The function used to clear the health checker is nil, plese check")
     end
 end
 
