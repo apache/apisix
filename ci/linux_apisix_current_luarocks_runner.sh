d2c51f3f89cb37baa9276955463b12db5eef1781
Zeping Bai <bzp2010@apache.org>
2023-03-13 01:17:28 +0800

    test

--- ci/linux_apisix_current_luarocks_runner.sh
+++ ci/linux_apisix_current_luarocks_runner.sh
@@ -36,10 +36,10 @@ script() {
 
     # install rust
     install_rust
-
+cargo -V
     # install APISIX with local version
-    sudo -E luarocks install rockspec/apisix-master-0.rockspec --only-deps > build.log 2>&1 || (cat build.log && exit 1)
-    sudo -E luarocks make rockspec/apisix-master-0.rockspec > build.log 2>&1 || (cat build.log && exit 1)
+    sudo luarocks install rockspec/apisix-master-0.rockspec --only-deps > build.log 2>&1 || (cat build.log && exit 1)
+    sudo luarocks make rockspec/apisix-master-0.rockspec > build.log 2>&1 || (cat build.log && exit 1)
     # ensure all files under apisix is installed
     diff -rq apisix /usr/local/share/lua/5.1/apisix
 
