615910e650a0963ccd70aea6ae56e7b3bb380b88
Zeping Bai <bzp2010@apache.org>
2023-03-13 17:58:52 +0800

    test: run luarocks without sudo

--- ci/linux_apisix_current_luarocks_runner.sh
+++ ci/linux_apisix_current_luarocks_runner.sh
@@ -39,8 +39,8 @@ script() {
 which cargo
 command -v cargo >/dev/null 2>&1 || echo "NOT EXIST"; exit 1
     # install APISIX with local version
-    sudo luarocks install rockspec/apisix-master-0.rockspec --only-deps > build.log 2>&1 || (cat build.log && exit 1)
-    sudo luarocks make rockspec/apisix-master-0.rockspec > build.log 2>&1 || (cat build.log && exit 1)
+    luarocks install rockspec/apisix-master-0.rockspec --only-deps > build.log 2>&1 || (cat build.log && exit 1)
+    luarocks make rockspec/apisix-master-0.rockspec > build.log 2>&1 || (cat build.log && exit 1)
     # ensure all files under apisix is installed
     diff -rq apisix /usr/local/share/lua/5.1/apisix
 
