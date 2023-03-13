2c1141e239f23a709610106d01388cc0956ebd38
Zeping Bai <bzp2010@apache.org>
2023-03-13 18:00:59 +0800

    test

--- ci/linux_apisix_current_luarocks_runner.sh
+++ ci/linux_apisix_current_luarocks_runner.sh
@@ -36,8 +36,7 @@ script() {
 
     # install rust
     install_rust
-which cargo
-command -v cargo >/dev/null 2>&1 || echo "NOT EXIST"; exit 1
+
     # install APISIX with local version
     luarocks install rockspec/apisix-master-0.rockspec --only-deps > build.log 2>&1 || (cat build.log && exit 1)
     luarocks make rockspec/apisix-master-0.rockspec > build.log 2>&1 || (cat build.log && exit 1)
