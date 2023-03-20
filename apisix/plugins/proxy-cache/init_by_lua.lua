e93691f3286103f0ee7f4d323cce398f2420ce44
Rohit Raj <88114930+ro4i7@users.noreply.github.com>
2023-03-20 16:11:14 +0530

    Update init_by_lua.lua

--- apisix/plugins/proxy-cache/init_by_lua.lua
+++ apisix/plugins/proxy-cache/init_by_lua.lua
@@ -6,16 +6,16 @@ local mock_service = require("spec.plugins.proxy-cache.mock_service")
 mock_service:start()
 
 -- extra_init_by_lua
-local function init()
-  local cjson = require("cjson")
-  cjson.encode_empty_table_as_object(false)
-end
+local cjson = require("cjson")
+cjson.encode_empty_table_as_object(false)
 
 -- After
-local function init()
-  local mock_service = require("spec.plugins.proxy-cache.mock_service")
-  mock_service:start()
+-- extra_init_by_lua_start
+local mock_service = require("spec.plugins.proxy-cache.mock_service")
+mock_service:start()
 
+-- extra_init_by_lua
+local function init()
   local cjson = require("cjson")
   cjson.encode_empty_table_as_object(false)
 end
