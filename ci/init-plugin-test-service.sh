2ff3d47f6b4c921bdd1087f2ccb898fabc3b0d79
dongjunduo <andj4cn@gmail.com>
2023-04-23 17:55:18 +0800

    chore(ci): remove 3rd keycloak docker image (#9341)

--- ci/init-plugin-test-service.sh
+++ ci/init-plugin-test-service.sh
@@ -42,8 +42,15 @@ after() {
 
     # wait for keycloak ready
     bash -c 'while true; do curl -s localhost:8080 &>/dev/null; ret=$?; [[ $ret -eq 0 ]] && break; sleep 3; done'
-    docker cp ci/kcadm_configure_cas.sh apisix_keycloak_new:/tmp/
-    docker exec apisix_keycloak_new bash /tmp/kcadm_configure_cas.sh
+
+    # install jq
+    wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -O jq
+    chmod +x jq
+    docker cp jq apisix_keycloak:/usr/bin/
+
+    # configure keycloak
+    docker exec apisix_keycloak bash /tmp/kcadm_configure_cas.sh
+    docker exec apisix_keycloak bash /tmp/kcadm_configure_university.sh
 }
 
 before() {
