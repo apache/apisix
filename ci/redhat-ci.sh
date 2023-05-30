5585d890571fad0118596e21e6072e60316080c3
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2023-05-30 15:30:33 +0530

    Update ci/redhat-ci.sh
    Co-authored-by: leslie <leslie@apache.org>

--- ci/redhat-ci.sh
+++ ci/redhat-ci.sh
@@ -31,7 +31,8 @@ install_dependencies() {
     install_curl
 
     # install openresty to make apisix's rpm test work
-    yum install -y --disablerepo=* --enablerepo=ubi-8-appstream-rpms --enablerepo=ubi-8-baseos-rpms yum-utils && yum-config-manager --add-repo https://openresty.org/package/redhat/openresty.repo
+    yum install -y --disablerepo=* --enablerepo=ubi-8-appstream-rpms --enablerepo=ubi-8-baseos-rpms yum-utils 
+    yum-config-manager --add-repo https://openresty.org/package/redhat/openresty.repo
     yum install -y --disablerepo=* --enablerepo=ubi-8-appstream-rpms --enablerepo=ubi-8-baseos-rpms openresty openresty-debug openresty-openssl111-debug-devel pcre pcre-devel
 
     # install luarocks
