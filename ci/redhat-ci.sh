982491355503f40bab4bbfd4824f700ef16ba8c7
revolyssup <ashishjaitiwari15112000@gmail.com>
2023-06-07 09:32:58 +0530

    remove extra lines
    Signed-off-by: revolyssup <ashishjaitiwari15112000@gmail.com>

--- ci/redhat-ci.sh
+++ ci/redhat-ci.sh
@@ -34,10 +34,6 @@ install_dependencies() {
     yum install -y yum-utils && yum-config-manager --add-repo https://openresty.org/package/centos/openresty.repo
     yum install -y openresty openresty-debug openresty-openssl111-debug-devel pcre pcre-devel xz
 
-
-
-
-
     # install luarocks
     ./utils/linux-install-luarocks.sh
 
