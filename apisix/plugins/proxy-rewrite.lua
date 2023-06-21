27cc9a8abba035c36909c4ee94b58136d91c7ed6
revolyssup <ashishjaitiwari15112000@gmail.com>
2023-06-21 11:07:25 +0530

    feat: allow unsafe_uri for proxy-pass plugin with regex_uri
    Signed-off-by: revolyssup <ashishjaitiwari15112000@gmail.com>

--- apisix/plugins/proxy-rewrite.lua
+++ apisix/plugins/proxy-rewrite.lua
@@ -63,14 +63,23 @@ local schema = {
             enum        = schema_method_enum
         },
         regex_uri = {
-            description = "new uri that substitute from client uri " ..
-                          "for upstream, lower priority than uri property",
-            type        = "array",
-            minItems    = 2,
-            items       = {
-                description = "regex uri",
-                type = "string",
-            }
+            description   = "new uri that substitute from client uri " ..
+                "for upstream, lower priority than uri property",
+            type          = "object",
+            minProperties = 2,
+            properties    = {
+                match = {
+                    type = "string"
+                },
+                target = {
+                    type = "string"
+                },
+                uri_unsafe = {
+                    description = "skip safe encoding of the uri",
+                    type        = "boolean",
+                    default     = false,
+                },
+            },
         },
         host = {
             description = "new host for upstream",
@@ -190,20 +199,6 @@ function _M.check_schema(conf)
         return false, err
     end
 
-    if conf.regex_uri and #conf.regex_uri > 0 then
-        if (#conf.regex_uri % 2 ~= 0) then
-            return false, "The length of regex_uri should be an even number"
-        end
-        for i = 1, #conf.regex_uri, 2 do
-            local _, _, err = re_sub("/fake_uri", conf.regex_uri[i],
-                conf.regex_uri[i + 1], "jo")
-            if err then
-                return false, "invalid regex_uri(" .. conf.regex_uri[i] ..
-                    ", " .. conf.regex_uri[i + 1] .. "): " .. err
-            end
-        end
-    end
-
     -- check headers
     if not conf.headers then
         return true
@@ -283,35 +278,33 @@ function _M.rewrite(conf, ctx)
         separator_escaped = true
         upstream_uri = core.utils.resolve_var(conf.uri, ctx.var, escape_separator)
     elseif conf.regex_uri ~= nil then
+        if conf.regex_uri.uri_unsafe then
+            upstream_uri = ctx.var.real_request_uri
+        end
         if not str_find(upstream_uri, "?") then
             separator_escaped = true
         end
-
         local error_msg
-        for i = 1, #conf.regex_uri, 2 do
-            local captures, err = re_match(upstream_uri, conf.regex_uri[i], "jo")
-            if err then
-                error_msg = "failed to match the uri " .. ctx.var.uri ..
-                    " (" .. conf.regex_uri[i] .. ") " .. " : " .. err
-                break
-            end
+        local captures, err = re_match(upstream_uri, conf.regex_uri.match, "jo")
+        if err then
+            error_msg = "failed to match the uri " .. ctx.var.uri ..
+            " (" .. conf.regex_uri.match .. ") " .. " : " .. err
+        end
 
-            if captures then
-                ctx.proxy_rewrite_regex_uri_captures = captures
+        if captures then
+            ctx.proxy_rewrite_regex_uri_captures = captures
 
-                local uri, _, err = re_sub(upstream_uri,
-                    conf.regex_uri[i], conf.regex_uri[i + 1], "jo")
-                if uri then
-                    upstream_uri = uri
-                else
-                    error_msg = "failed to substitute the uri " .. ngx.var.uri ..
-                        " (" .. conf.regex_uri[i] .. ") with " ..
-                        conf.regex_uri[i + 1] .. " : " .. err
-                end
-
-                break
+            local uri, _, err = re_sub(upstream_uri,
+            conf.regex_uri.match, conf.regex_uri.target, "jo")
+            if uri then
+                upstream_uri = uri
+            else
+                error_msg = "failed to substitute the uri " .. ngx.var.uri ..
+                " (" .. conf.regex_uri.match .. ") with " ..
+                conf.regex_uri.target .. " : " .. err
             end
         end
+
 
         if error_msg ~= nil then
             core.log.error(error_msg)
@@ -321,21 +314,21 @@ function _M.rewrite(conf, ctx)
 
     if not conf.use_real_request_uri_unsafe then
         local index
-        if separator_escaped then
-            index = str_find(upstream_uri, "?")
-        end
-
-        if index then
-            upstream_uri = core.utils.uri_safe_encode(sub_str(upstream_uri, 1, index - 1)) ..
-                sub_str(upstream_uri, index)
-        else
-            -- The '?' may come from client request '%3f' when we use ngx.var.uri directly or
-            -- via regex_uri
-            upstream_uri = core.utils.uri_safe_encode(upstream_uri)
-        end
-
+            -- By pass normalisation when regex_uri.uri_unsafe is true
+            if not conf.regex_uri.uri_unsafe then
+                if separator_escaped then
+                    index = str_find(upstream_uri, "?")
+                end
+                if index then
+                    upstream_uri = core.utils.uri_safe_encode(sub_str(upstream_uri, 1, index - 1)) ..
+                        sub_str(upstream_uri, index)
+                else
+                    -- The '?' may come from client request '%3f' when we use ngx.var.uri directly or
+                    -- via regex_uri
+                    upstream_uri = core.utils.uri_safe_encode(upstream_uri)
+                end         
+            end
         req_set_uri(upstream_uri)
-
         if ctx.var.is_args == "?" then
             if index then
                 ctx.var.upstream_uri = upstream_uri .. "&" .. (ctx.var.args or "")
