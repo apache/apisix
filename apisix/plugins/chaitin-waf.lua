82e4d3f9414b374cbb6c59ea7c0a204090ad4087
Ling Samuel (WSL) <lingsamuelgrace@gmail.com>
2023-07-19 00:34:04 +0800

    support healthcheck

--- apisix/plugins/chaitin-waf.lua
+++ apisix/plugins/chaitin-waf.lua
@@ -40,6 +40,99 @@ local plugin_schema = {
     required = { "upstream" },
 }
 
+local health_checker = {
+    type = "object",
+    properties = {
+        active = {
+            type = "object",
+            properties = {
+                type = {
+                    type = "string",
+                    enum = {"http", "https", "tcp"},
+                    default = "http"
+                },
+                timeout = {type = "number", default = 1},
+                concurrency = {type = "integer", default = 10},
+                host = core.schema.host_def,
+                port = {
+                    type = "integer",
+                    minimum = 1,
+                    maximum = 65535
+                },
+                http_path = {type = "string", default = "/"},
+                https_verify_certificate = {type = "boolean", default = true},
+                healthy = {
+                    type = "object",
+                    properties = {
+                        interval = {type = "integer", minimum = 1, default = 1},
+                        http_statuses = {
+                            type = "array",
+                            minItems = 1,
+                            items = {
+                                type = "integer",
+                                minimum = 200,
+                                maximum = 599
+                            },
+                            uniqueItems = true,
+                            default = {200, 302}
+                        },
+                        successes = {
+                            type = "integer",
+                            minimum = 1,
+                            maximum = 254,
+                            default = 2
+                        }
+                    }
+                },
+                unhealthy = {
+                    type = "object",
+                    properties = {
+                        interval = {type = "integer", minimum = 1, default = 1},
+                        http_statuses = {
+                            type = "array",
+                            minItems = 1,
+                            items = {
+                                type = "integer",
+                                minimum = 200,
+                                maximum = 599
+                            },
+                            uniqueItems = true,
+                            default = {429, 404, 500, 501, 502, 503, 504, 505}
+                        },
+                        http_failures = {
+                            type = "integer",
+                            minimum = 1,
+                            maximum = 254,
+                            default = 5
+                        },
+                        tcp_failures = {
+                            type = "integer",
+                            minimum = 1,
+                            maximum = 254,
+                            default = 2
+                        },
+                        timeouts = {
+                            type = "integer",
+                            minimum = 1,
+                            maximum = 254,
+                            default = 3
+                        }
+                    }
+                },
+                req_headers = {
+                    type = "array",
+                    minItems = 1,
+                    items = {
+                        type = "string",
+                        uniqueItems = true,
+                    },
+                }
+            }
+        }
+    },
+    {required = {"active"}},
+}
+
 local metadata_schema = {
     type = "object",
     properties = {
@@ -61,9 +154,13 @@ local metadata_schema = {
             },
             minItems = 1,
         },
+        checks = health_checker,
     },
     required = { "nodes" },
 }
+
+local global_server_picker
+local global_healthcheck = {}
 
 local _M = {
     version = 0.1, -- plugin version
@@ -81,6 +178,116 @@ local HEADER_CHAITIN_WAF_ACTION = "X-APISIX-CHAITIN-WAF-ACTION"
 
 local cjson = require "cjson.safe".new()
 
+local function release_checker()
+    if not global_healthcheck.checker then
+        return
+    end
+
+    core.log.info("try to release checker: ", tostring(global_healthcheck.checker))
+    global_healthcheck.checker:clear()
+    global_healthcheck.checker:stop()
+end
+
+local function get_health_checker(metadata)
+    if not metadata.checks then
+        release_checker()
+        return nil
+    end
+
+    if global_healthcheck.checker and global_healthcheck.checker_nodes == metadata.nodes then
+        return global_healthcheck.checker
+    end
+
+    local checker, err = healthcheck.new({
+        name = "chaitin-waf-healthcheck",
+        -- We can reuse existing shared memory
+        shm_name = "upstream-healthcheck",
+        checks = metadata.checks,
+    })
+
+    release_checker()
+
+    core.log.info("create new health checker for chaitin-waf: ", tostring(checker))
+
+    local host = metadata.checks and metadata.checks.active and metadata.checks.active.host
+    local port = metadata.checks and metadata.checks.active and metadata.checks.active.port
+
+    local host_hdr = "127.0.0.1"
+    for _, node in ipairs(metadata.nodes) do
+        local ok, err = checker:add_target(node.host, port or node.port, host, true, host_hdr)
+        if not ok then
+            core.log.error("failed to add new health check target: ", node.host, ":", port or node.port, " err: ", err)
+        end
+    end
+
+    global_healthcheck.checker = checker
+    global_healthcheck.checker_nodes = metadata.nodes
+    global_healthcheck.status_ver = checker.status_ver
+
+    return checker
+end
+
+local function get_healthy_chaitin_server(metadata, checker)
+    local nodes = metadata.nodes
+    local new_nodes = core.table.new(0, #nodes)
+    if not checker then
+        for i = 1, #nodes do
+            local host, port = nodes[i].host, nodes[i].port
+            new_nodes[host .. ":" .. tostring(port)] = 1
+        end
+        return new_nodes
+    end
+
+    local host = metadata.checks and metadata.checks.active and metadata.checks.active.host
+    local port = metadata.checks and metadata.checks.active and metadata.checks.active.port
+    for _, node in ipairs(nodes) do
+
+        core.log.error("testing chaitin servers: ", node.host, ":", port or node.port, ", host: ", host)
+        local ok, err = checker:get_target_status(node.host, port or node.port, host)
+        if ok then
+            core.log.error("get chaitin-waf health check target status, addr: ",
+                    node.host, ":", port or node.port, ", host: ", host, ", err: ", err)
+            new_nodes[node.host .. ":" .. tostring(node.port)] = 1
+        elseif err then
+            core.log.error("failed to get chaitin-waf health check target status, addr: ", node.host, ":", port or node.port, ", host: ", host, ", err: ", err)
+        else
+            core.log.error("failed to get chaitin-waf health check target status, addr: ", node.host, ":", port or node.port, ", host: ", host)
+        end
+    end
+
+    return new_nodes
+end
+
+local ERR_NO_METADATA = 1
+local ERR_NO_HEALTHY_NODES = 2
+local function get_chaitin_server(ctx)
+    local metadata = plugin.plugin_metadata(plugin_name)
+    if not core.table.try_read_attr(metadata, "value", "nodes") then
+        return nil, nil, { code = ERR_NO_METADATA, message = "chaitin-waf: missing metadata"}
+    end
+
+    local checker = get_health_checker(metadata.value)
+    if not global_server_picker or global_server_picker.upstream ~= metadata.value.nodes or
+            (checker and checker.status_ver ~= global_healthcheck.status_ver) then
+
+        local up_nodes = get_healthy_chaitin_server(metadata.value, checker)
+        if core.table.nkeys(up_nodes) == 0 then
+            return nil, nil, { code = ERR_NO_HEALTHY_NODES, message = "chaitin-waf: no healthy nodes"}
+        end
+        core.log.info("healthy chaitin-waf nodes: ", core.json.delay_encode(up_nodes))
+
+        global_server_picker = rr_balancer.new(up_nodes, metadata.value.nodes)
+        global_healthcheck.status_ver = checker and checker.status_ver
+    end
+
+    local server = global_server_picker.get(ctx)
+    local host, port, err = core.utils.parse_addr(server)
+    if err then
+        return nil, nil, err
+    end
+    return host, port, nil
+end
+
 local function get_server_from_metadata(metadata)
     -- TODO: healthcheck
 
@@ -96,6 +303,52 @@ end
 
 function _M.access(conf, ctx)
     local extra_headers = {}
+
+    -- 测试 plugin_metadata，包含一个错误的 server
+    -- curl http://127.0.0.1:9180/apisix/admin/plugin_metadata/chaitin-waf -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' -X PUT -d '
+    --{
+    --   "nodes":[
+    --      {
+    --        "host": "unix:/home/lingsamuel/chaitin-waf/safeline/resources/detector/snserver.sock",
+    --        "port": 8000
+    --      }, {
+    --        "host": "127.0.0.1",
+    --        "port": 1551,
+    --      }
+    --   ]
+    --}'
+
+    -- 测试 plugin_metadata，包含一个错误的 server
+    -- curl http://127.0.0.1:9180/apisix/admin/plugin_metadata/chaitin-waf -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' -X PUT -d '
+    --     "nodes":[
+    --         {
+    --             "host": "unix:/home/lingsamuel/chaitin-waf/safeline/resources/detector/snserver.sock",
+    --             "port": 8000
+    --         },
+    --         {
+    --             "host": "127.0.0.1",
+    --             "port": 1551
+    --         }
+    --     ],
+
+    --     "checks": {
+    --         "active": {
+    --             "type": "tcp",
+    --             "host": "localhost",
+    --             "timeout": 5,
+    --             "http_path": "/status",
+    --             "healthy": {
+    --                 "interval": 2,
+    --                 "successes": 1
+    --             },
+    --             "unhealthy": {
+    --                 "interval": 1,
+    --                 "http_failures": 2
+    --             },
+    --             "req_headers": ["User-Agent: curl/7.29.0"]
+    --         }
+    --     }
+    -- }'
 
     -- 测试路由：curl http://127.0.0.1:9180/apisix/admin/routes/1 -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' -X PUT -d '
     --{
@@ -129,8 +382,20 @@ function _M.access(conf, ctx)
         --return 503, { message = "Missing metadata for chaitin-waf" }
     end
 
-    --local host, port = get_server_from_metadata(metadata)
+    local host, port, err = get_chaitin_server(ctx)
+    if err.code then
+        ngx.log(ngx.ERR, "HEALTHY CHAITIN SERVER: host: " .. err.message)
+        extra_headers["X-APISIX-CHAITIN-WAF-SERVER"] = "no-healthy"
 
+        core.response.set_header(extra_headers)
+        return
+    end
+    ngx.log(ngx.ERR, "HEALTHY CHAITIN SERVER: host: " .. tostring(host) .. ", port: " .. tostring(port) .. ", err: " .. tostring(err))
+    if err then
+        return
+    end
+
+    extra_headers["X-APISIX-CHAITIN-WAF-SERVER"] = host
     -- TODO: FIXME? 实测发现请求如果不带 Host header 的话，在日志里只会显示原地址
     -- 例如：curl -H "Host: httpbin.default" localhost:9080/getid=1%20AND%201=1 -i
     -- 和：curl localhost:9080/getid=1%20AND%201=1 -i
@@ -142,8 +407,8 @@ function _M.access(conf, ctx)
     -- TODO: request id?
     local t = {
         mode = "block", -- block or monitor or off, default off
-        host = "unix:/home/lingsamuel/chaitin-waf/safeline/resources/detector/snserver.sock", -- required, SafeLine WAF detection service host, unix domain socket, IP, or domain is supported, string
-        port = 8000, -- required when the host is an IP or domain, SafeLine WAF detection service port, integer
+        host = host, -- required, SafeLine WAF detection service host, unix domain socket, IP, or domain is supported, string
+        port = port, -- required when the host is an IP or domain, SafeLine WAF detection service port, integer
         connect_timeout = 4000, -- connect timeout, in milliseconds, integer, default 1s (1000ms)
         send_timeout = 4000, -- send timeout, in milliseconds, integer, default 1s (1000ms)
         read_timeout = 4000, -- read timeout, in milliseconds, integer, default 1s (1000ms)
@@ -168,7 +433,7 @@ function _M.access(conf, ctx)
     extra_headers[HEADER_CHAITIN_WAF_TIME] = ngx_now() * 1000 - start_time
 
     local code = 200
-    if result.status then
+    if result and result.status then
         extra_headers[HEADER_CHAITIN_WAF_STATUS] = code
         extra_headers[HEADER_CHAITIN_WAF_ACTION] = "reject"
         core.response.set_header(extra_headers)
