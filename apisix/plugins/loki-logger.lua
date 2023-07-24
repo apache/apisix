1ded3a837c52fd268927d76f6708553934b77a03
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2023-07-24 06:38:00 +0530

    fix(loki-logger): nil context in batch processor (#9850)

--- apisix/plugins/loki-logger.lua
+++ apisix/plugins/loki-logger.lua
@@ -192,18 +192,18 @@ function _M.log(conf, ctx)
         return
     end
 
+    local labels = conf.log_labels
+
+    -- parsing possible variables in label value
+    for key, value in pairs(labels) do
+        local new_val, err, n_resolved = core.utils.resolve_var(value, ctx.var)
+        if not err and n_resolved > 0 then
+            labels[key] = new_val
+        end
+    end
+
     -- generate a function to be executed by the batch processor
     local func = function(entries)
-        local labels = conf.log_labels
-
-        -- parsing possible variables in label value
-        for key, value in pairs(labels) do
-            local new_val, err, n_resolved = core.utils.resolve_var(value, ctx.var)
-            if not err and n_resolved > 0 then
-                labels[key] = new_val
-            end
-        end
-
         -- build loki request data
         local data = {
             streams = {
