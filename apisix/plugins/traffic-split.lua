deb22f9bcc4976797ff626a533bdfb091855f9d9
Abhishek Choudhary <shreemaan.abhishek@gmail.com>
2023-08-13 09:31:26 +0545

    add nil check for upstream id

--- apisix/plugins/traffic-split.lua
+++ apisix/plugins/traffic-split.lua
@@ -236,10 +236,13 @@ function _M.access(conf, ctx)
         -- check if all upstream_ids are valid
         if rule.weighted_upstreams then
             for _, wupstream in ipairs(rule.weighted_upstreams) do
-                local ups = upstream.get_by_id(wupstream.upstream_id)
-                if not ups then
-                    return 500, "failed to fetch upstream info by "
-                                .. "upstream id: " .. wupstream.upstream_id
+                local ups_id = wupstream.upstream_id
+                if ups_id then
+                    local ups = upstream.get_by_id(ups_id)
+                    if not ups then
+                        return 500, "failed to fetch upstream info by "
+                                    .. "upstream id: " .. ups_id
+                    end
                 end
             end
         end
