aac965e598a25aea699f9d0b1e06c545799ad681
jinhua luo <home_king@163.com>
2023-09-01 14:51:29 +0800

    fix: remove etcd.use_grpc (#10015)

--- apisix/cli/schema.lua
+++ apisix/cli/schema.lua
@@ -62,11 +62,6 @@ local etcd_schema = {
             minimum = 1,
             description = "etcd connection timeout in seconds",
         },
-        use_grpc = {
-            type = "boolean",
-            -- TODO: set true by default in v3.2
-            default = false,
-        },
     },
     required = {"prefix", "host"}
 }
