100d654b08505fde5cc0b662cd95a8c502b635ab
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2023-10-23 11:05:19 +0530

    trigger CI
    Signed-off-by: Ashish Tiwari <ashishjaitiwari15112000@gmail.com>

--- utils/install-dependencies.sh
+++ utils/install-dependencies.sh
@@ -46,7 +46,7 @@ function install_dependencies_with_aur() {
 function install_dependencies_with_yum() {
     sudo yum install -y yum-utils
 
-    local common_dep="curl wget git gcc unzip pcre pcre-devel openldap-devel"
+    local common_dep="curl wget git gcc unzip pcre pcre-devel openldap-devel cpanm"
     if [ "${1}" == "centos" ]; then
         # add APISIX source
         local apisix_pkg=apache-apisix-repo-1.0-1.noarch
@@ -84,13 +84,13 @@ function install_dependencies_with_apt() {
     sudo apt-get update
 
     # install OpenResty and some compilation tools
-    sudo apt-get install -y git openresty curl make gcc libpcre3 libpcre3-dev libldap2-dev unzip
+    sudo apt-get install -y git openresty curl make gcc libpcre3 libpcre3-dev libldap2-dev unzip cpanm
 }
 
 # Install dependencies on mac osx
 function install_dependencies_on_mac_osx() {
     # install OpenResty, etcd and some compilation tools
-    brew install openresty/brew/openresty luarocks lua@5.1 wget curl git pcre openldap
+    brew install openresty/brew/openresty luarocks lua@5.1 wget curl git pcre openldap cpanm
 }
 
 # Identify the different distributions and call the corresponding function
