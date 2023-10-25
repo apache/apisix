36f6760daa97425eb01a11d2a84cb761a373146d
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2023-10-25 12:03:12 +0530

    fix ci
    Signed-off-by: Ashish Tiwari <ashishjaitiwari15112000@gmail.com>

--- ci/linux-install-openresty.sh
+++ ci/linux-install-openresty.sh
@@ -37,33 +37,43 @@ USE_OPENSSL3=${USE_OPENSSL3-no}
 OPENSSL3_PREFIX=${OPENSSL3_PREFIX-/home/runner}
 SSL_LIB_VERSION=${SSL_LIB_VERSION-openssl}
 
+install_openssl_3(){
+    # required for openssl 3.x config
+    cpanm IPC/Cmd.pm
+    wget --no-check-certificate  https://www.openssl.org/source/openssl-3.1.3.tar.gz
+    tar xvf openssl-*.tar.gz
+    cd openssl-*/
+    ./config --prefix=$OPENSSL3_PREFIX/openssl --openssldir=$OPENSSL3_PREFIX/openssl enable-fips
+    make -j $(nproc)
+    make install
+    ldconfig
+    cd ..
+}
+
 if [ "$OPENRESTY_VERSION" == "source" ]; then
-    export openssl_prefix=/usr/local/openssl
+    export openssl_prefix=${OPENSSL3_PREFIX}
     export zlib_prefix=/usr/local/openresty/zlib
     export pcre_prefix=/usr/local/openresty/pcre
 
-    export cc_opt="-DNGX_LUA_ABORT_AT_PANIC -I${zlib_prefix}/include -I${pcre_prefix}/include -I${openssl_prefix}/include"
-    export ld_opt="-L${zlib_prefix}/lib -L${pcre_prefix}/lib -L${openssl_prefix}/lib -Wl,-rpath,${zlib_prefix}/lib:${pcre_prefix}/lib:${openssl_prefix}/lib"
+
 
     apt install -y build-essential
-    wget https://www.openssl.org/source/openssl-3.1.3.tar.gz
-    tar xvf openssl-*.tar.gz
-    cd openssl-*/
-    ./Configure --prefix=$OPENSSL3_PREFIX/openssl-3.0 enable-fips
-    make install
-    bash -c "echo $OPENSSL3_PREFIX/openssl-3.0/lib64 > /etc/ld.so.conf.d/openssl3.conf"
-    ldconfig
+    install_openssl_3
+
+    export cc_opt="-DNGX_LUA_ABORT_AT_PANIC -I${zlib_prefix}/include -I${pcre_prefix}/include -I${openssl_prefix}/include"
+    export ld_opt="-L${zlib_prefix}/lib -L${pcre_prefix}/lib -L${openssl_prefix}/lib -Wl,-rpath,${zlib_prefix}/lib:${pcre_prefix}/lib:${openssl_prefix}/lib"
+    # wget https://www.openssl.org/source/openssl-3.1.3.tar.gz
+    # tar xvf openssl-*.tar.gz
+    # cd openssl-*/
+    # ./Configure --prefix=$OPENSSL3_PREFIX/openssl-3.0 enable-fips
+    # make install
+    # bash -c "echo $OPENSSL3_PREFIX/openssl-3.0/lib64 > /etc/ld.so.conf.d/openssl3.conf"
+    # ldconfig
     if [ "$COMPILE_OPENSSL3" == "yes" ]; then
         $OPENSSL3_PREFIX/openssl-3.0/bin/openssl fipsinstall -out $OPENSSL3_PREFIX/openssl-3.0/ssl/fipsmodule.cnf -module $OPENSSL3_PREFIX/openssl-3.0/lib64/ossl-modules/fips.so
         sed -i 's@# .include fipsmodule.cnf@.include '"$OPENSSL3_PREFIX"'/openssl-3.0/ssl/fipsmodule.cnf@g; s/# \(fips = fips_sect\)/\1\nbase = base_sect\n\n[base_sect]\nactivate=1\n/g' $OPENSSL3_PREFIX/openssl-3.0/ssl/openssl.cnf
     fi
-    cd ..
-
-
-    bash -c "echo $OPENSSL3_PREFIX/openssl-3.0/lib64 > /etc/ld.so.conf.d/openssl3.conf"
     ldconfig
-    export cc_opt="-I$OPENSSL3_PREFIX/openssl-3.0/include"
-    export ld_opt="-L$OPENSSL3_PREFIX/openssl-3.0/lib64 -Wl,-rpath,$OPENSSL3_PREFIX/openssl-3.0/lib64"
 
 
     if [ "$SSL_LIB_VERSION" == "tongsuo" ]; then
@@ -74,8 +84,6 @@ if [ "$OPENRESTY_VERSION" == "source" ]; then
         export cc_opt="-DNGX_LUA_ABORT_AT_PANIC -I${zlib_prefix}/include -I${pcre_prefix}/include -I${openssl_prefix}/include"
         export ld_opt="-L${zlib_prefix}/lib -L${pcre_prefix}/lib -L${openssl_prefix}/lib64 -Wl,-rpath,${zlib_prefix}/lib:${pcre_prefix}/lib:${openssl_prefix}/lib64"
     fi
-
-    cd ..
     wget -q https://raw.githubusercontent.com/revolyssup/apisix-build-tools/revolyssup/upgrade-apisix-base-openssl3/build-apisix-base.sh
     chmod +x build-apisix-base.sh
     ./build-apisix-base.sh latest
