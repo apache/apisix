76bc6dede6ba5d0870b31ccbb26e77f92629299f
Liu Wei <monkeydluffy6017@gmail.com>
2023-11-24 11:10:35 +0800

    fix(limit-count): all consumers should share the same counter (#10541)

--- apisix/plugins/limit-count/init.lua
+++ apisix/plugins/limit-count/init.lua
@@ -230,7 +230,9 @@ local function gen_limit_key(conf, ctx, key)
     -- Here we use plugin-level conf version to prevent the counter from being resetting
     -- because of the change elsewhere.
     -- A route which reuses a previous route's ID will inherits its counter.
-    local new_key = ctx.conf_type .. ctx.conf_id .. ':' .. apisix_plugin.conf_version(conf)
+    local conf_type = ctx.conf_type_without_consumer or ctx.conf_type
+    local conf_id = ctx.conf_id_without_consumer or ctx.conf_id
+    local new_key = conf_type .. conf_id .. ':' .. apisix_plugin.conf_version(conf)
                     .. ':' .. key
     if conf._vid then
         -- conf has _vid means it's from workflow plugin, add _vid to the key
