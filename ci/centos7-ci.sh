2987dfb21ba13c435608f66e6e4b78e866c3da0e
AlinsRan <alinsran333@gmail.com>
2023-12-13 16:22:22 +0000

    add fips ci

--- ci/centos7-ci.sh
+++ ci/centos7-ci.sh
@@ -25,7 +25,8 @@ install_dependencies() {
     # install build & runtime deps
     yum install -y wget tar gcc automake autoconf libtool make unzip \
         git sudo openldap-devel which ca-certificates openssl-devel \
-        epel-release
+        epel-release  \
+        cpanminus perl
 
     # install newer curl
     yum makecache
