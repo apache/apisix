388af950b555cb576f217b41d9c34da3c3a76be8
wuhanqing <hanqingwu@126.com>
2024-01-02 14:48:57 +0800

    fix: apisix restart failed use sleep method(#10495)

--- t/cli/test_cmd.sh
+++ t/cli/test_cmd.sh
@@ -24,7 +24,7 @@ git checkout conf/config.yaml
 # check restart with old nginx.pid exist
 echo "-1" > logs/nginx.pid
 out=$(./bin/apisix start 2>&1 || true)
-if echo "$out" | grep "APISIX is running"; then
+if echo "$out" | grep "the old process is still running"; then
     rm logs/nginx.pid
     echo "failed: should reject bad nginx.pid"
     exit 1
@@ -50,7 +50,7 @@ echo "pass: no corresponding process"
 
 # check running when run repeatedly
 out=$(make run; make run || true)
-if ! echo "$out" | grep "APISIX is running"; then
+if ! echo "$out" | grep "the old process is still running"; then
     echo "failed: should find APISIX running"
     exit 1
 fi
