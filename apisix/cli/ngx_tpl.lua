62698ac96b31043c0999e2dc74b754ad55ddb6fc
wangye <1031205858@qq.com>
2024-02-18 16:16:06 +0800

    fix shared dict

--- apisix/cli/ngx_tpl.lua
+++ apisix/cli/ngx_tpl.lua
@@ -293,7 +293,7 @@ http {
     {% end %}
 
     {% if enabled_plugins["limit-req"] then %}
-    lua_shared_dict plugin-limit-req-redis-cluster-slot-lock {* http.lua_shared_dict["plugin-limit-req"] *};
+    lua_shared_dict plugin-limit-req-redis-cluster-slot-lock {* http.lua_shared_dict["plugin-limit-req-redis-cluster-slot-lock"] *};
     lua_shared_dict plugin-limit-req {* http.lua_shared_dict["plugin-limit-req"] *};
     {% end %}
 
