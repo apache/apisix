32a2cd9f3bc0e911e2f3c06091b353cdcaa0b4a1
Leigang Zhang <71714656+zll600@users.noreply.github.com>
2024-02-26 17:38:13 +0800

    feat: support openresty-1.25.3.1 (#10887)

--- t/cli/test_main.sh
+++ t/cli/test_main.sh
@@ -152,7 +152,7 @@ if [ $count_http_specific_ip -ne 2 ]; then
     exit 1
 fi
 
-count_http_specific_ip_and_enable_http2=`grep -c "listen 127.0.0..:908. default_server http2" conf/nginx.conf || true`
+count_http_specific_ip_and_enable_http2=`grep -c "http2 on" conf/nginx.conf || true`
 if [ $count_http_specific_ip_and_enable_http2 -ne 1 ]; then
     echo "failed: failed to support specific IP and enable http2 listen in http"
     exit 1
@@ -164,7 +164,7 @@ if [ $count_https_specific_ip -ne 2 ]; then
     exit 1
 fi
 
-count_https_specific_ip_and_enable_http2=`grep -c "listen 127.0.0..:944. ssl default_server http2" conf/nginx.conf || true`
+count_https_specific_ip_and_enable_http2=`grep -c "http2 on" conf/nginx.conf || true`
 if [ $count_https_specific_ip_and_enable_http2 -ne 1 ]; then
     echo "failed: failed to support specific IP and enable http2 listen in https"
     exit 1
