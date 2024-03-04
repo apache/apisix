17ad90d06a63d6620262d096a554be55cecae047
illidan <illidan33@qq.com>
2024-03-04 23:24:11 +0800

    feat(openid-connect): add session.cookie configuration (#10919)

--- apisix/plugins/openid-connect.lua
+++ apisix/plugins/openid-connect.lua
@@ -72,6 +72,15 @@ local schema = {
                     description = "the key used for the encrypt and HMAC calculation",
                     minLength = 16,
                 },
+                cookie = {
+                    type = "object",
+                    properties = {
+                        lifetime = {
+                            type = "integer",
+                            description = "it holds the cookie lifetime in seconds in the future",
+                        }
+                    }
+                }
             },
             required = {"secret"},
             additionalProperties = false,
