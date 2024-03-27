e07d3f5326cd0d8c08ff8db32f96620de9a6d98f
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2024-03-27 21:47:28 +0530

    echo for debug

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -33,7 +33,7 @@ get_admin_key() {
     local admin_key=$(grep "key:" -A3 conf/config.yaml | grep "key: *" | awk '{print $2}')
     echo "$admin_key"
 }
-admin_key=$(get_admin_key)
+admin_key=$(get_admin_key); echo $admin_key
 mkdir -p benchmark/server/logs
 mkdir -p benchmark/fake-apisix/logs
 
