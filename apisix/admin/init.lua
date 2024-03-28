0edef02322f02d17eb78bf54101a6a73421e9320
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2024-03-28 11:14:37 +0530

    add log

--- apisix/admin/init.lua
+++ apisix/admin/init.lua
@@ -117,7 +117,7 @@ local function set_ctx_and_check_token()
     local ok, err = check_token(api_ctx)
     if not ok then
         core.log.warn("failed to check token: ", err)
-        core.response.exit(401, { error_msg = "failed to check token" })
+        core.response.exit(401, { error_msg = "failed to check token - "+err })
     end
 end
 
