5cdc0d73d4c771cf0f5087f35443d9f0a5331bc2
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2024-04-12 09:16:50 +0530

    remove err logs

--- apisix/core/config_etcd.lua
+++ apisix/core/config_etcd.lua
@@ -330,10 +330,11 @@ local function sync_data(self)
 
     ::waitdir::
     local dir_res, err = waitdir(self.etcd_cli, self.key, self.prev_index + 1, self.timeout)
-    log.error("waitdir key: ", self.key, " prev_index: ", self.prev_index + 1)
-    log.error("res: ", json.delay_encode(dir_res, true))
+    log.info("waitdir key: ", self.key, " prev_index: ", self.prev_index + 1)
+    log.info("res: ", json.delay_encode(dir_res, true))
     if not dir_res then
         if err == "timeout" then
+            -- get latest uncompacted revision
             local res, err = self.etcd_cli:get('/nonexisting',{
                 count_only = true,
                 range_end = "\0"
@@ -345,8 +346,7 @@ local function sync_data(self)
             if res and res.body and res.body.header and res.body.header.revision then
                 rev = tonumber(res.body.header.revision)
             end
-            log.error("GOT LATEST UNCOMPACTED REVISION ",rev)
-            log.error("updating "..self.prev_index.." to "..rev )
+            log.info("updating prev_index from "..self.prev_index.." to "..rev )
             if rev then
                 if rev == self.prev_index then
                     goto fail
@@ -359,7 +359,7 @@ local function sync_data(self)
         log.error("waitdir failed: ", err)
         return false, err
     end
-    log.error("waitdir success")
+    log.info("waitdir success")
     local res = dir_res.body.node
     local err_msg = dir_res.body.message
     if err_msg then
