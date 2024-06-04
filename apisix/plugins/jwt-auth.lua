bd76c435a33c7dac91a938654c334eb3d10bd2ae
Michele Righi <righi.michy@gmail.com>
2024-06-04 15:44:44 +0200

    Revert style changes to jwt-auth plugin

--- apisix/plugins/jwt-auth.lua
+++ apisix/plugins/jwt-auth.lua
@@ -36,71 +36,45 @@ local schema = {
     type = "object",
     properties = {
         header = {
-            description = "The name of the HTTP header where the JWT token is expected to be "
-                            .. "found.",
             type = "string",
             default = "authorization"
         },
         query = {
-            description = "The name of the query parameter where the JWT token is expected to be "
-                            .. "found.",
             type = "string",
             default = "jwt"
         },
         cookie = {
-            description = "The name of the cookie where the JWT token is expected to be found.",
             type = "string",
             default = "jwt"
         },
         hide_credentials = {
-            description = "If true, the plugin will remove the JWT token from the header, query, "
-                            .. "or cookie after extracting it to avoid sending it to the upstream "
-                            .. "service.",
             type = "boolean",
             default = false
         },
         key_claim_name = {
-            description = "The name of the claim in the JWT token that contains the user key.",
             type = "string",
             default = "key"
         }
     },
 }
 
-
 local consumer_schema = {
     type = "object",
     -- can't use additionalProperties with dependencies
     properties = {
-        key = {
-            description = "A unique key used to identify the Consumer (corresponds to "
-                            .. "conf.key_claim_name in JWT).",
-            type = "string"
-        },
-        secret = {
-            description = "The encryption key used for signing and verifying the JWT token. "
-                            .. "If unspecified, auto generated in the background.",
-            type = "string"
-        },
+        key = {type = "string"},
+        secret = {type = "string"},
         algorithm = {
-            description = "The encryption algorithm used for signing and verifying the JWT token.",
             type = "string",
             enum = {"HS256", "HS512", "RS256", "ES256"},
             default = "HS256"
         },
-        exp = {
-            description = "The expiration time of the JWT token in seconds.",
-            type = "integer", minimum = 1, 
-            default = 86400
-        },
+        exp = {type = "integer", minimum = 1, default = 86400},
         base64_secret = {
-            description = "Indicates if the secret is base64 encoded.",
             type = "boolean",
             default = false
         },
         lifetime_grace_period = {
-            description = "The grace period in seconds for the JWT token lifetime validation. "
-                            .. "Allows a buffer time for token expiration.",
             type = "integer",
             minimum = 0,
             default = 0
@@ -119,16 +93,8 @@ local consumer_schema = {
                 },
                 {
                     properties = {
-                        public_key = {
-                            description = "The public key used for verifying the JWT token when "
-                                            .. "using RS256 or ES256 algorithms.",
-                            type = "string"
-                        },
-                        private_key= {
-                            description = "The private key used for signing the JWT token when "
-                                            .. "using RS256 or ES256 algorithms.",
-                            type = "string"
-                        },
+                        public_key = {type = "string"},
+                        private_key= {type = "string"},
                         algorithm = {
                             enum = {"RS256", "ES256"},
                         },
@@ -154,12 +120,11 @@ local _M = {
 
 
 function _M.check_schema(conf, schema_type)
-    core.log.info("Input conf: ", core.json.delay_encode(conf))
+    core.log.info("input conf: ", core.json.delay_encode(conf))
 
     local ok, err
     if schema_type == core.schema.TYPE_CONSUMER then
         ok, err = core.schema.check(consumer_schema, conf)
-
     else
         return core.schema.check(schema, conf)
     end
@@ -170,7 +135,6 @@ function _M.check_schema(conf, schema_type)
 
     if conf.algorithm ~= "RS256" and conf.algorithm ~= "ES256" and not conf.secret then
         conf.secret = ngx_encode_base64(resty_random.bytes(32, true))
-
     elseif conf.base64_secret then
         if ngx_decode_base64(conf.secret) == nil then
             return false, "base64_secret required but the secret is not in base64 format"
@@ -191,7 +155,6 @@ function _M.check_schema(conf, schema_type)
     return true
 end
 
-
 local function remove_specified_cookie(src, key)
     local cookie_key_pattern = "([a-zA-Z0-9-_]*)"
     local cookie_val_pattern = "([a-zA-Z0-9-._]*)"
@@ -199,13 +162,13 @@ local function remove_specified_cookie(src, key)
 
     local it, err = ngx_re_gmatch(src, cookie_key_pattern .. "=" .. cookie_val_pattern, "jo")
     if not it then
-        core.log.error("Match origins failed: ", err)
+        core.log.error("match origins failed: ", err)
         return src
     end
     while true do
         local m, err = it()
         if err then
-            core.log.error("Iterate origins failed: ", err)
+            core.log.error("iterate origins failed: ", err)
             return src
         end
         if not m then
@@ -218,7 +181,6 @@ local function remove_specified_cookie(src, key)
 
     return table_concat(t, "; ")
 end
-
 
 local function fetch_jwt_token(conf, ctx)
     local token = core.request.header(ctx, conf.header)
@@ -262,7 +224,6 @@ local function fetch_jwt_token(conf, ctx)
     return val
 end
 
-
 local function get_secret(conf)
     local secret = conf.secret
 
@@ -280,22 +241,19 @@ local function get_rsa_or_ecdsa_keypair(conf)
 
     if public_key and private_key then
         return public_key, private_key
-
     elseif public_key and not private_key then
         return nil, nil, "missing private key"
-
     elseif not public_key and private_key then
         return nil, nil, "missing public key"
-
     else
         return nil, nil, "public and private keys are missing"
     end
 end
 
 
-local function get_real_payload(key, auth_conf, payload)
+local function get_real_payload(key, auth_conf, payload, key_claim_name)
     local real_payload = {
-        key = key,
+        [key_claim_name] = key,
         exp = ngx_time() + auth_conf.exp
     }
     if payload then
@@ -307,11 +265,11 @@ local function get_real_payload(key, auth_conf, payload)
 end
 
 
-local function sign_jwt_with_HS(key, consumer, payload)
+local function sign_jwt_with_HS(key, consumer, payload, key_claim_name)
     local auth_secret, err = get_secret(consumer.auth_conf)
     if not auth_secret then
-        core.log.error("Failed to sign JWT, err: ", err)
-        core.response.exit(503, "Failed to sign JWT")
+        core.log.error("failed to sign jwt, err: ", err)
+        core.response.exit(503, "failed to sign jwt")
     end
     local ok, jwt_token = pcall(jwt.sign, _M,
         auth_secret,
@@ -320,24 +278,24 @@ local function sign_jwt_with_HS(key, consumer, payload)
                 typ = "JWT",
                 alg = consumer.auth_conf.algorithm
             },
-            payload = get_real_payload(key, consumer.auth_conf, payload)
+            payload = get_real_payload(key, consumer.auth_conf, payload, key_claim_name)
         }
     )
     if not ok then
-        core.log.warn("Failed to sign JWT, err: ", jwt_token.reason)
-        core.response.exit(500, "Failed to sign JWT")
+        core.log.warn("failed to sign jwt, err: ", jwt_token.reason)
+        core.response.exit(500, "failed to sign jwt")
     end
     return jwt_token
 end
 
 
-local function sign_jwt_with_RS256_ES256(key, consumer, payload)
+local function sign_jwt_with_RS256_ES256(key, consumer, payload, key_claim_name)
     local public_key, private_key, err = get_rsa_or_ecdsa_keypair(
         consumer.auth_conf
     )
     if not public_key then
-        core.log.error("Failed to sign JWT, err: ", err)
-        core.response.exit(503, "Failed to sign JWT")
+        core.log.error("failed to sign jwt, err: ", err)
+        core.response.exit(503, "failed to sign jwt")
     end
 
     local ok, jwt_token = pcall(jwt.sign, _M,
@@ -350,16 +308,15 @@ local function sign_jwt_with_RS256_ES256(key, consumer, payload)
                     public_key,
                 }
             },
-            payload = get_real_payload(key, consumer.auth_conf, payload)
+            payload = get_real_payload(key, consumer.auth_conf, payload, key_claim_name)
         }
     )
     if not ok then
-        core.log.warn("Failed to sign JWT, err: ", jwt_token.reason)
-        core.response.exit(500, "Failed to sign JWT")
+        core.log.warn("failed to sign jwt, err: ", jwt_token.reason)
+        core.response.exit(500, "failed to sign jwt")
     end
     return jwt_token
 end
-
 
 -- introducing method_only flag (returns respective signing method) to save http API calls.
 local function algorithm_handler(consumer, method_only)
@@ -370,7 +327,6 @@ local function algorithm_handler(consumer, method_only)
         end
 
         return get_secret(consumer.auth_conf)
-
     elseif consumer.auth_conf.algorithm == "RS256" or consumer.auth_conf.algorithm == "ES256"  then
         if method_only then
             return sign_jwt_with_RS256_ES256
@@ -381,17 +337,16 @@ local function algorithm_handler(consumer, method_only)
     end
 end
 
-
 function _M.rewrite(conf, ctx)
     -- fetch token and hide credentials if necessary
     local jwt_token, err = fetch_jwt_token(conf, ctx)
     if not jwt_token then
-        core.log.info("Failed to fetch JWT token: ", err)
+        core.log.info("failed to fetch JWT token: ", err)
         return 401, {message = "Missing JWT token in request"}
     end
 
     local jwt_obj = jwt:load_jwt(jwt_token)
-    core.log.info("JWT object: ", core.json.delay_encode(jwt_obj))
+    core.log.info("jwt object: ", core.json.delay_encode(jwt_obj))
     if not jwt_obj.valid then
         core.log.warn("JWT token invalid: ", jwt_obj.reason)
         return 401, {message = "JWT token invalid"}
@@ -400,7 +355,7 @@ function _M.rewrite(conf, ctx)
     local key_claim_name = conf.key_claim_name
     local user_key = jwt_obj.payload and jwt_obj.payload[key_claim_name]
     if not user_key then
-        return 401, {message = "Missing " .. key_claim_name .. " claim in JWT token"}
+        return 401, {message = "missing " .. key_claim_name .. " claim in JWT token"}
     end
 
     local consumer_conf = consumer_mod.plugin(plugin_name)
@@ -412,29 +367,28 @@ function _M.rewrite(conf, ctx)
 
     local consumer = consumers[user_key]
     if not consumer then
-        -- This means that there's a mismatch between the JWT key claim and the Consumer key field
-        return 401, {message = "Invalid user " .. key_claim_name .. " in JWT token"}
+        return 401, {message = "Invalid user key in JWT token"}
     end
-    core.log.info("Consumer: ", core.json.delay_encode(consumer))
+    core.log.info("consumer: ", core.json.delay_encode(consumer))
 
     local auth_secret, err = algorithm_handler(consumer)
     if not auth_secret then
-        core.log.error("Failed to retrieve secrets, err: ", err)
-        return 503, {message = "Failed to verify JWT"}
+        core.log.error("failed to retrieve secrets, err: ", err)
+        return 503, {message = "failed to verify jwt"}
     end
     local claim_specs = jwt:get_default_validation_options(jwt_obj)
     claim_specs.lifetime_grace_period = consumer.auth_conf.lifetime_grace_period
 
     jwt_obj = jwt:verify_jwt_obj(auth_secret, jwt_obj, claim_specs)
-    core.log.info("JWT object: ", core.json.delay_encode(jwt_obj))
+    core.log.info("jwt object: ", core.json.delay_encode(jwt_obj))
 
     if not jwt_obj.verified then
-        core.log.warn("Failed to verify JWT: ", jwt_obj.reason)
-        return 401, {message = "Failed to verify JWT"}
+        core.log.warn("failed to verify jwt: ", jwt_obj.reason)
+        return 401, {message = "failed to verify jwt"}
     end
 
     consumer_mod.attach_consumer(ctx, consumer, consumer_conf)
-    core.log.info("Hit jwt-auth rewrite")
+    core.log.info("hit jwt-auth rewrite")
 end
 
 
@@ -446,6 +400,8 @@ local function gen_token()
 
     local key = args.key
     local payload = args.payload
+    local key_claim_name = args.key_claim_name or "key"
+
     if payload then
         payload = ngx.unescape_uri(payload)
     end
@@ -457,16 +413,16 @@ local function gen_token()
 
     local consumers = consumer_mod.consumers_kv(plugin_name, consumer_conf, "key")
 
-    core.log.info("Consumers: ", core.json.delay_encode(consumers))
+    core.log.info("consumers: ", core.json.delay_encode(consumers))
     local consumer = consumers[key]
     if not consumer then
         return core.response.exit(404)
     end
 
-    core.log.info("Consumer: ", core.json.delay_encode(consumer))
+    core.log.info("consumer: ", core.json.delay_encode(consumer))
 
     local sign_handler = algorithm_handler(consumer, true)
-    local jwt_token = sign_handler(key, consumer, payload)
+    local jwt_token = sign_handler(key, consumer, payload, key_claim_name)
     if jwt_token then
         return core.response.exit(200, jwt_token)
     end
