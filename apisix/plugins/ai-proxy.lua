cdd37db20a8e3c1dad7c2f7be8408350e731761b
Abhishek Choudhary <shreemaan.abhishek@gmail.com>
2024-09-12 08:43:50 +0545

    update priority

--- apisix/plugins/ai-proxy.lua
+++ apisix/plugins/ai-proxy.lua
@@ -27,7 +27,7 @@ local ngx_flush = ngx.flush
 local plugin_name = "ai-proxy"
 local _M = {
     version = 0.5,
-    priority = 1055,
+    priority = 999,
     name = plugin_name,
     schema = schema,
 }
