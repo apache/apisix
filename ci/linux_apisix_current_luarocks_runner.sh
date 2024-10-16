8a34021ef18a3ffd5a51fe8d5c16656f355e2100
Tej Magar <tejmagar20510@gmail.com>
2024-10-16 21:57:37 +0545

    fix: python CI failure
    Remove --break-system-packages flag from pip installation script.

--- ci/linux_apisix_current_luarocks_runner.sh
+++ ci/linux_apisix_current_luarocks_runner.sh
@@ -64,7 +64,7 @@ script() {
     set_coredns
 
     # install test dependencies
-    sudo pip install requests --break-system-packages
+    sudo pip install requests
 
     # dismiss "maximum number of open file descriptors too small" warning
     ulimit -n 10240
