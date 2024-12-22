8900fa1cd31a9cdcf2eaec376fcce9b53f331ef4
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2024-12-23 02:12:55 +0530

    fix test

--- apisix/control/router.lua
+++ apisix/control/router.lua
@@ -81,11 +81,13 @@ do
                         if type(body) == "table" and ngx.header["Content-Type"] == nil then
                             core.response.set_header("Content-Type", "application/json")
                         end
-                        if type(body) then
+                        if type(body) == "table" then
                             body.checker = nil
                             body.checker_idx = nil
                             body.checker_upstream = nil
-                            body.clean_handlers = nil
+                            if body.clean_handlers then
+                                body.clean_handlers = {}
+                            end
                         end
                         core.response.exit(code, body)
                     end
