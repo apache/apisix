7fbaa9e5f1fdfdedec8ed6d39a2c729c7997cd85
Nic <qianyong@api7.ai>
2024-12-26 16:30:43 +0800

    fix: deepcopy should copy same table exactly only once (#11861)
    Signed-off-by: Nic <qianyong@api7.ai>

--- apisix/init.lua
+++ apisix/init.lua
@@ -246,15 +246,7 @@ local function parse_domain_in_route(route)
     -- don't modify the modifiedIndex to avoid plugin cache miss because of DNS resolve result
     -- has changed
 
-    local parent = route.value.upstream.parent
-    if parent then
-        route.value.upstream.parent = nil
-    end
-    route.dns_value = core.table.deepcopy(route.value)
-    if parent then
-        route.value.upstream.parent = parent
-        route.dns_value.upstream.parent = parent
-    end
+    route.dns_value = core.table.deepcopy(route.value, { shallows = { "self.upstream.parent"}})
     route.dns_value.upstream.nodes = new_nodes
     core.log.info("parse route which contain domain: ",
                   core.json.delay_encode(route, true))
