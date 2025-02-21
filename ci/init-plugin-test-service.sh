a8845cce817e6fbec2510f0d71a5b216cbeaab7e
Zeping Bai <bzp2010@apache.org>
2025-02-21 17:22:23 +0800

    test: rollback

--- ci/init-plugin-test-service.sh
+++ ci/init-plugin-test-service.sh
@@ -51,7 +51,6 @@ after() {
     # configure keycloak
     docker exec apisix_keycloak bash /tmp/kcadm_configure_cas.sh
     docker exec apisix_keycloak bash /tmp/kcadm_configure_university.sh
-    docker exec apisix_keycloak bash /tmp/kcadm_configure_basic.sh
 
     # configure clickhouse
     echo 'CREATE TABLE default.test (`host` String, `client_ip` String, `route_id` String, `service_id` String, `@timestamp` String, PRIMARY KEY(`@timestamp`)) ENGINE = MergeTree()' | curl 'http://localhost:8123/' --data-binary @-
