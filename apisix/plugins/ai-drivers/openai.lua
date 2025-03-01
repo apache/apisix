88164f47ead10965c4c9e9a8a3368ce20f6b022c
Shreemaan Abhishek <shreemaan.abhishek@gmail.com>
2025-03-01 15:00:35 +0545

    feat(ai-proxy): support proxying openai compatible LLMs (#12004)

--- apisix/plugins/ai-drivers/openai.lua
+++ apisix/plugins/ai-drivers/openai.lua
@@ -15,7 +15,7 @@
 -- limitations under the License.
 --
 
-return require("apisix.plugins.ai-drivers.openai-compatible").new(
+return require("apisix.plugins.ai-drivers.openai-base").new(
     {
         host = "api.openai.com",
         path = "/v1/chat/completions",
