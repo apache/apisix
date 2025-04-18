bc58d24e890d64b4e44f234d11e12f5a1919a410
Zeping Bai <bzp2010@apache.org>
2025-04-18 10:18:07 +0800

    chore: clean centos7 test infrasturcture (#12157)

--- ci/common.sh
+++ ci/common.sh
@@ -96,9 +96,7 @@ install_grpcurl () {
 
 install_vault_cli () {
     VAULT_VERSION="1.9.0"
-    # the certificate can't be verified in CentOS7, see
-    # https://blog.devgenius.io/lets-encrypt-change-affects-openssl-1-0-x-and-centos-7-49bd66016af3
-    wget -q --no-check-certificate https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip
+    wget -q https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip
     unzip vault_${VAULT_VERSION}_linux_amd64.zip && mv ./vault /usr/local/bin
 }
 
