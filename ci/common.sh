24283084d28b21a25918fbd067ea0c12a2e04742
Zeping Bai <bzp2010@apache.org>
2025-05-17 00:51:13 +0800

    fix lint

--- ci/common.sh
+++ ci/common.sh
@@ -169,7 +169,7 @@ linux_get_dependencies () {
     apt remove -y curl
     apt-get install -y libyaml-dev
     wget https://github.com/mikefarah/yq/releases/download/3.4.1/yq_linux_amd64 -O /usr/bin/yq && sudo chmod +x /usr/bin/yq
-    
+
     # install curl with http3 support
     install_curl
 }
