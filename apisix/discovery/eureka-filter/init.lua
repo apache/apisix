c4af8c17e6f8d2cc3ba25d80204df84d99cd1b91
Fake <fake@ponyft.com>
2025-05-24 12:03:26 +0800

    删除中文注释

--- apisix/discovery/eureka-filter/init.lua
+++ apisix/discovery/eureka-filter/init.lua
@@ -6,25 +6,20 @@ local _M = {
     version = 0.1
 }
 
--- 初始化函数，复用 eureka 模块的初始化
 function _M.init_worker()
     eureka.init_worker()
 end
 
--- 获取服务节点并进行过滤
 function _M.nodes(service_name, discovery_args)
-    -- 获取所有节点
     local nodes = eureka.nodes(service_name)
     if not nodes then
         return nil
     end
 
-    -- 如果没有指定区域，返回所有节点
     if not discovery_args or not discovery_args.zone then
         return nodes
     end
 
-    -- 按区域过滤节点
     local zone = discovery_args.zone
     local filtered_nodes = {}
 
@@ -36,13 +31,12 @@ function _M.nodes(service_name, discovery_args)
 
     if #filtered_nodes == 0 then
         log.warn("no nodes found for service [", service_name, "] in zone [", zone, "]")
-        return nodes  -- 如果没有找到节点，返回所有节点作为降级策略
+        return nodes
     end
 
     return filtered_nodes
 end
 
--- 复用 eureka 模块的数据导出
 function _M.dump_data()
     return eureka.dump_data()
 end
