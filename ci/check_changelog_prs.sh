94a5daa13a41fb09c86934fa6c18bf6539932699
LiteSun <sunyi@apache.org>
2025-06-11 08:54:36 +0800

    f

--- ci/check_changelog_prs.sh
+++ ci/check_changelog_prs.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 set -euo pipefail
 
 old_tag=$1
 new_tag=$2
+
+# Function to compare versions
+version_gt() {
+    test "$(printf '%s\n' "$@" | sort -V | head -n1)" != "$1"
+}
+
+# Check if versions are above 3.8.0
+if ! version_gt "${old_tag#v}" "3.8.0"; then
+    echo "Skipping check for versions below or equal to 3.8.0"
+    exit 0
+fi
 
 # Configure PR types to ignore
 IGNORE_TYPES=(
