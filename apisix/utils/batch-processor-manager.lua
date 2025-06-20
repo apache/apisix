d1f91a22ac676e53f9e4ecc4e03841ba8e5bf42b
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2025-06-20 10:26:04 +0530

    improve logs

--- apisix/utils/batch-processor-manager.lua
+++ apisix/utils/batch-processor-manager.lua
@@ -95,10 +95,15 @@ local function total_processed_entries(self)
 end
 
 function _M:add_entry(conf, entry, max_pending_entries)
-    if max_pending_entries and
-       self.total_pushed_entries - total_processed_entries(self) > max_pending_entries then
-        core.log.error("max pending entries limit exceeded. discarding entry")
+    if max_pending_entries then
+        local total_processed_entries = total_processed_entries(self)
+        if self.total_pushed_entries - total_processed_entries > max_pending_entries then
+        core.log.error("max pending entries limit exceeded. discarding entry.",
+                       " total_pushed_entries: ", self.total_pushed_entries,
+                       " total_processed_entries: ", total_processed_entries
+                       " max_pending_entries: ", max_pending_entries)
         return
+       end
     end
     check_stale(self)
 
@@ -114,10 +119,15 @@ end
 
 
 function _M:add_entry_to_new_processor(conf, entry, ctx, func, max_pending_entries)
-    if max_pending_entries and
-       self.total_pushed_entries - total_processed_entries(self) > max_pending_entries then
-        core.log.error("max pending entries limit exceeded. discarding entry")
+    if max_pending_entries then
+        local total_processed_entries = total_processed_entries(self)
+        if self.total_pushed_entries - total_processed_entries > max_pending_entries then
+        core.log.error("max pending entries limit exceeded. discarding entry.",
+                       " total_pushed_entries: ", self.total_pushed_entries,
+                       " total_processed_entries: ", total_processed_entries
+                       " max_pending_entries: ", max_pending_entries)
         return
+       end
     end
     check_stale(self)
 
