e551bbcf2cf9dc0c132b5d4cbc5e45d5cb4d13be
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2025-07-16 17:23:42 +0530

    fix lint

--- apisix/healthcheck_manager.lua
+++ apisix/healthcheck_manager.lua
@@ -14,6 +14,10 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 --
+local require = require
+local ipairs   = ipairs
+local pairs    = pairs
+local tostring = tostring
 local core = require("apisix.core")
 local config_local   = require("apisix.core.config_local")
 local healthcheck
