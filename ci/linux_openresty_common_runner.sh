53de201e952135c2d80348921b99bbd07f048ddc
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2025-07-28 07:44:54 +0530

    fix: skipped failing bailedout tests in CI (#12462)

--- ci/linux_openresty_common_runner.sh
+++ ci/linux_openresty_common_runner.sh
@@ -99,6 +99,7 @@ script() {
 
     # APISIX_ENABLE_LUACOV=1 PERL5LIB=.:$PERL5LIB prove -Itest-nginx/lib -r t
     FLUSH_ETCD=1 TEST_EVENTS_MODULE=$TEST_EVENTS_MODULE prove --timer -Itest-nginx/lib -I./ -r $TEST_FILE_SUB_DIR | tee /tmp/test.result
+    fail_on_bailout /tmp/test.result
     rerun_flaky_tests /tmp/test.result
 }
 
