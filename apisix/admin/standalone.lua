63f7cbee552be3a9bf7806b9ec6a014bfba27a59
Zeping Bai <bzp2010@apache.org>
2025-08-15 10:24:48 +0800

    clean

--- apisix/admin/standalone.lua
+++ apisix/admin/standalone.lua
@@ -312,8 +312,6 @@ local function get(ctx)
         return core.response.exit(500, {error_msg = err})
     end
 
-    ngx.log(ngx.ERR, require('cjson').encode(config))
-
     core.response.set_header(METADATA_LAST_MODIFIED, config[METADATA_LAST_MODIFIED])
     core.response.set_header(METADATA_DIGEST, config[METADATA_DIGEST])
     return core.response.exit(200, resp)
