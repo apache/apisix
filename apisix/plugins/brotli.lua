ca09e48379b759846d0b67e9f6fc389b178b31e4
yuweizzz <yuwei764969238@gmail.com>
2025-12-31 16:41:44 +0800

    Downgrade etag when in needed

--- apisix/plugins/brotli.lua
+++ apisix/plugins/brotli.lua
@@ -152,6 +152,30 @@ local function check_accept_encoding(ctx)
 end
 
 
+local function weak_etag_header()
+    local etag = ngx.header.etag 
+    if not etag then
+        return
+    end
+
+    local regex = [[^(W/)?"(.*)"$]]
+    local matched, err = ngx.re.match(etag, regex, "jo")
+    if not matched then
+        -- not standard etag, no quote
+        ngx.header.etag = nil
+        return
+    end
+
+    if not matched[1] then
+        -- strong etag, downgrade it
+        ngx.header.etag = [[W/"]] .. matched[2] .. [["]]
+    else
+        -- weak etag, keep it
+        return
+    end
+end
+
+
 function _M.header_filter(conf, ctx)
     if not is_loaded then
         core.log.error("please check the brotli library")
@@ -224,6 +248,7 @@ function _M.header_filter(conf, ctx)
     ctx.compressor = compressor
     ngx.header.content_length = nil
     ngx.header.content_encoding = 'br'
+    weak_etag_header()
 end
 
 
