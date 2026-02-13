e17e8ee9f8d859c3a40e6cac136636377e1ec671
Shreemaan Abhishek <shreemaan.abhishek@gmail.com>
2026-02-13 07:29:11 +0545

    fix: use apisix_request_id only in http subsystem log format (#13006)

--- apisix/cli/config.lua
+++ apisix/cli/config.lua
@@ -111,7 +111,7 @@ local _M = {
       enable_access_log = false,
       access_log = "logs/access_stream.log",
       -- luacheck: push max code line length 300
-      access_log_format = "$remote_addr [$time_local] $protocol $status $bytes_sent $bytes_received $session_time $apisix_request_id",
+      access_log_format = "$remote_addr [$time_local] $protocol $status $bytes_sent $bytes_received $session_time",
       -- luacheck: pop
       access_log_format_escape = "default",
       lua_shared_dict = {
@@ -136,7 +136,7 @@ local _M = {
       access_log_buffer = 16384,
       -- luacheck: push max code line length 300
       access_log_format =
-      '$remote_addr - $remote_user [$time_local] $http_host "$request" $status $body_bytes_sent $request_time "$http_referer" "$http_user_agent" $upstream_addr $upstream_status $upstream_response_time "$upstream_scheme://$upstream_host$upstream_uri"',
+      '$remote_addr - $remote_user [$time_local] $http_host "$request" $status $body_bytes_sent $request_time "$http_referer" "$http_user_agent" $upstream_addr $upstream_status $upstream_response_time "$upstream_scheme://$upstream_host$upstream_uri" "$apisix_request_id"',
       -- luacheck: pop
       access_log_format_escape = "default",
       keepalive_timeout = "60s",
