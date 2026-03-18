b19294111a457bab479bae73a194c8f78aeae484
Julien Rouvier <julien.rouvier@instant-system.com>
2026-03-18 08:58:37 +0100

    implement datastream support on elasticsearch-logger plugin

--- apisix/plugins/elasticsearch-logger.lua
+++ apisix/plugins/elasticsearch-logger.lua
@@ -48,8 +48,12 @@ local schema = {
             type = "object",
             properties = {
                 index = { type = "string"},
+                datastream = { type = "string"},
             },
-            required = {"index"}
+            oneOf = {
+                {required = {"index"}},
+                {required = {"datastream"}},
+            },
         },
         log_format = {type = "object"},
         auth = {
@@ -203,11 +207,25 @@ end
 
 local function get_logger_entry(conf, ctx)
     local entry = log_util.get_log_entry(plugin_name, conf, ctx)
-    local body = {
-        index = {
-            _index = conf.field.index
+    local body
+    if conf.field.index then
+        body = {
+            index = {
+                _index = conf.field.index
+            }
         }
-    }
+    else if conf.field.datastream then
+        body = {
+            create = {
+                _index = conf.field.datastream
+            }
+        }
+    else
+        core.log.error("Unsupported field configuration")
+        return nil
+    end
+    end
+
     -- for older version type is required
     if conf._version == "6" or conf._version == "5" then
         body.index._type = "_doc"
