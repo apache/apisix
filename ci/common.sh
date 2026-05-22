01207a4d60a38ed212f620aabe08d1b5c6203455
Nic <qianyong@api7.ai>
2026-05-22 12:03:51 +0800

    ci: remove OpenResty version selector

--- ci/common.sh
+++ ci/common.sh
@@ -96,11 +96,7 @@ install_apisix_runtime() {
     export runtime_version=${APISIX_RUNTIME}
     wget "https://raw.githubusercontent.com/api7/apisix-build-tools/apisix-runtime/${APISIX_RUNTIME}/build-apisix-runtime.sh"
     chmod +x build-apisix-runtime.sh
-    if [[ "${OPENRESTY_VERSION:-}" == "default" || "${OPENRESTY_VERSION:-}" == "source" ]]; then
-        OPENRESTY_VERSION="" ./build-apisix-runtime.sh latest
-    else
-        ./build-apisix-runtime.sh latest
-    fi
+    ./build-apisix-runtime.sh latest
 }
 
 install_grpcurl () {
