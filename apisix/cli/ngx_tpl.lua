15a438183bfbba4581ade6f7d263e3636ce38578
Zeping Bai <bzp2010@apache.org>
2026-09-02 17:41:28 +0800

    feat: improve API-driven standalone update reliability (#13904)

--- apisix/cli/ngx_tpl.lua
+++ apisix/cli/ngx_tpl.lua
@@ -73,6 +73,7 @@ lua {
     {% end %}
     {% if standalone_with_admin_api then %}
     lua_shared_dict standalone-config {* meta.lua_shared_dict["standalone-config"] *};
+    lua_shared_dict standalone-status {* meta.lua_shared_dict["standalone-status"] *};
     {% end %}
     {% if status then %}
     lua_shared_dict status-report {* meta.lua_shared_dict["status-report"] *};
